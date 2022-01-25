b2694cd5872e794da427f885a0a06142a7bd8748
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-25 10:52:11 +0800

    initialize

---
+++ packages/watchmen-model/src/watchmen/model/admin/pipeline_action_write.py
@@ -0,0 +1,31 @@
+from typing import List, Union
+
+from watchmen.model.admin.pipeline_action import AggregateArithmeticHolder, FindBy, PipelineAction, ToFactor, ToTopic, \
+	WriteTopicActionType
+from watchmen.model.common import FactorId, Parameter
+
+
+class MappingFactor(AggregateArithmeticHolder):
+	source: Parameter = None
+	factorId: FactorId = None
+
+
+class MappingRow(PipelineAction):
+	mapping: List[MappingFactor] = []
+
+
+class WriteTopicAction(ToTopic, PipelineAction):
+	type: WriteTopicActionType = None
+
+
+class InsertRowAction(WriteTopicAction, MappingRow):
+	type: WriteTopicActionType.INSERT_ROW = WriteTopicActionType.INSERT_ROW
+
+
+class MergeRowAction(WriteTopicAction, MappingRow, FindBy):
+	type: Union[WriteTopicActionType.MERGE_ROW, WriteTopicActionType.INSERT_OR_MERGE_ROW] = None
+
+
+class WriteFactorAction(ToFactor, WriteTopicAction, FindBy, AggregateArithmeticHolder):
+	type: WriteTopicActionType.WRITE_FACTOR = WriteTopicActionType.WRITE_FACTOR
+	source: Parameter = None
---
+++ packages/watchmen-model/src/watchmen/model/admin/pipeline_graphic.py
@@ -0,0 +1,24 @@
+from typing import List
+
+from pydantic import BaseModel
+
+from watchmen.model.common import GraphicPosition, GraphicRect, PipelinesGraphicId, TenantId, TopicId, Tuple, UserId
+
+
+class TopicRect(BaseModel):
+	coordinate: GraphicPosition = None
+	frame: GraphicRect = None
+	name: GraphicPosition = None
+
+
+class TopicGraphic(BaseModel):
+	topicId: TopicId = None
+	rect: TopicRect = None
+
+
+class PipelinesGraphic(Tuple):
+	pipelineGraphId: PipelinesGraphicId = None
+	name: str = None
+	topics: List[TopicGraphic] = []
+	userId: UserId = None
+	tenantId: TenantId = None
