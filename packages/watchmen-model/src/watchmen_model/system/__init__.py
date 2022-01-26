041f3ff5d2faa447e9f50640c01ec57d56a29aa4
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-26 12:03:34 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/system/__init__.py
+++ packages/watchmen-model/src/watchmen_model/system/__init__.py
@@ -5,6 +5,6 @@ import tenant
 Tenant = tenant.Tenant
 DataSourceParam = data_source.DataSourceParam
 DataSourceType = data_source.DataSourceType
-DataSource = data_source.DataSourceType
+DataSource = data_source.DataSource
 ExternalWriterType = external_writer.ExternalWriterType
 ExternalWriter = external_writer.ExternalWriter
--- packages/watchmen-storage-mysql/src/watchmen_storage_mysql/mysql_data_source.py
+++ packages/watchmen-storage-mysql/src/watchmen_storage_mysql/mysql_data_source.py
@@ -1,7 +1,45 @@
+from sqlalchemy import create_engine
+
 from watchmen_storage import DataSourceHelper
-from watchmen_storage.storage_spi import StorageSPI
+from watchmen_storage_mysql.storage_msyql import StorageMySQL
+from watchmen_utilities import serialize_to_json
+
+
+def redress_url(value: str) -> str:
+	if value is None:
+		return ''
+	else:
+		return value.strip()
 
 
 class MySQLDataSourceHelper(DataSourceHelper):
-	def acquire_storage(self) -> StorageSPI:
-		pass
+	@staticmethod
+	def acquire_storage_by_url(url: str) -> StorageMySQL:
+		engine = create_engine(
+			url,
+			echo=False,
+			future=True,
+			pool_recycle=3600,
+			json_serializer=serialize_to_json,
+			encoding='utf-8'
+		)
+		return StorageMySQL(engine)
+
+	@staticmethod
+	def acquire_storage_by_params(
+			username: str, password: str, host: str, port: str, name: str
+	) -> StorageMySQL:
+		url = f'mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8'
+		return MySQLDataSourceHelper.acquire_storage_by_url(url)
+
+	def acquire_storage(self) -> StorageMySQL:
+		data_source = self.data_source
+		url = redress_url(data_source.url)
+		if len(url) != 0:
+			return MySQLDataSourceHelper.acquire_storage_by_url(url)
+		else:
+			return self.acquire_storage_by_params(
+				data_source.username, data_source.password,
+				data_source.host, data_source.port,
+				data_source.name
+			)
