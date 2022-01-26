cc6673db1cec7e6b017365957ce726b80724026b
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-26 13:52:47 +0800

    initialize

--- packages/watchmen-storage-mysql/src/watchmen_storage_mysql/mysql_data_source.py
+++ packages/watchmen-storage-mysql/src/watchmen_storage_mysql/mysql_data_source.py
@@ -1,5 +1,7 @@
 from sqlalchemy import create_engine
+from sqlalchemy.engine import Engine
 
+from watchmen_model.system import DataSource
 from watchmen_storage import DataSourceHelper
 from watchmen_storage_mysql.storage_msyql import StorageMySQL
 from watchmen_utilities import serialize_to_json
@@ -20,9 +22,25 @@ def redress_url_by_pymysql(url: str) -> str:
 
 
 class MySQLDataSourceHelper(DataSourceHelper):
+	def __init__(self, data_source: DataSource):
+		super().__init__(data_source)
+		self.engine = self.acquire_engine()
+
+	def acquire_engine(self) -> Engine:
+		data_source = self.data_source
+		url = redress_url(data_source.url)
+		if len(url) != 0:
+			return MySQLDataSourceHelper.acquire_engine_by_url(url)
+		else:
+			return self.acquire_engine_by_params(
+				data_source.username, data_source.password,
+				data_source.host, data_source.port,
+				data_source.name
+			)
+
 	@staticmethod
-	def acquire_storage_by_url(url: str) -> StorageMySQL:
-		engine = create_engine(
+	def acquire_engine_by_url(url: str) -> Engine:
+		return create_engine(
 			redress_url_by_pymysql(url),
 			echo=False,
 			future=True,
@@ -30,23 +48,13 @@ class MySQLDataSourceHelper(DataSourceHelper):
 			json_serializer=serialize_to_json,
 			encoding='utf-8'
 		)
-		return StorageMySQL(engine)
 
 	@staticmethod
-	def acquire_storage_by_params(
+	def acquire_engine_by_params(
 			username: str, password: str, host: str, port: str, name: str
-	) -> StorageMySQL:
+	) -> Engine:
 		url = f'mysql+pymysql://{username}:{password}@{host}:{port}/{name}?charset=utf8'
-		return MySQLDataSourceHelper.acquire_storage_by_url(url)
+		return MySQLDataSourceHelper.acquire_engine_by_url(url)
 
 	def acquire_storage(self) -> StorageMySQL:
-		data_source = self.data_source
-		url = redress_url(data_source.url)
-		if len(url) != 0:
-			return MySQLDataSourceHelper.acquire_storage_by_url(url)
-		else:
-			return self.acquire_storage_by_params(
-				data_source.username, data_source.password,
-				data_source.host, data_source.port,
-				data_source.name
-			)
+		return StorageMySQL(self.engine)
