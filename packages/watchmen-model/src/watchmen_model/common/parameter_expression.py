bc7c3d13c5cc15de475eb0cf5f14bfe2e7349b68
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-27 17:58:17 +0800

    initialize

---
+++ packages/watchmen-model/src/watchmen_model/common/parameter_expression.py
@@ -0,0 +1,23 @@
+from enum import Enum
+
+from .parameter import Parameter
+from .parameter_condition import ParameterCondition
+
+
+class ParameterExpressionOperator(str, Enum):
+	EMPTY = 'empty',
+	NOT_EMPTY = 'not-empty',
+	EQUALS = 'equals',
+	NOT_EQUALS = 'not-equals',
+	LESS = 'less',
+	LESS_EQUALS = 'less-equals',
+	MORE = 'more',
+	MORE_EQUALS = 'more-equals',
+	IN = 'in',
+	NOT_IN = 'not-in',
+
+
+class ParameterExpression(ParameterCondition):
+	left: Parameter = None
+	operator: ParameterExpressionOperator = ParameterExpressionOperator.EQUALS
+	right: Parameter = None
