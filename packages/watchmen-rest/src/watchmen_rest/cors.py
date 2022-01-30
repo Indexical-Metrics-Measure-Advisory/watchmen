afaa6d4318705713507e460cdcc1d0e2be126ead
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-30 16:47:25 +0800

    initialize

--- packages/watchmen-rest/src/watchmen_rest/cors.py
+++ packages/watchmen-rest/src/watchmen_rest/cors.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from .rest_settings import RestSettings
 
-def install_cors(app: FastAPI) -> None:
+
+def install_cors(app: FastAPI, settings: RestSettings) -> None:
 	app.add_middleware(
 		CORSMiddleware,
-		allow_origins=['*'],
-		allow_credentials=True,
-		allow_methods=["*"],
-		allow_headers=["*"],
+		allow_origins=settings.CORS_ALLOWED_ORIGINS,
+		allow_credentials=settings.CORS_ALLOW_CREDENTIALS,
+		allow_methods=settings.CORS_ALLOWED_METHODS,
+		allow_headers=settings.CORS_ALLOWED_HEADERS,
 	)
--- packages/watchmen-rest/src/watchmen_rest/prometheus.py
+++ packages/watchmen-rest/src/watchmen_rest/prometheus.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from starlette_prometheus import metrics, PrometheusMiddleware
 
+from .rest_settings import RestSettings
 
-def install_prometheus(app: FastAPI) -> None:
+
+def install_prometheus(app: FastAPI, settings: RestSettings) -> None:
 	app.add_middleware(PrometheusMiddleware)
-	app.add_route("/metrics", metrics)
+	app.add_route(settings.PROMETHEUS_CONTEXT, metrics)
