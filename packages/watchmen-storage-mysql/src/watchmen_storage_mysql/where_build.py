09d191d510ea34b301a163c4ffb87aad2483dc6c
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-01-31 23:02:19 +0800

    initialize

--- packages/watchmen-storage-mysql/src/watchmen_storage_mysql/where_build.py
+++ packages/watchmen-storage-mysql/src/watchmen_storage_mysql/where_build.py
@@ -1,4 +1,4 @@
-from sqlalchemy import and_, column, or_
+from sqlalchemy import and_, or_, Table
 
 from watchmen_storage import EntityCriteria, EntityCriteriaExpression, EntityCriteriaJoint, \
 	EntityCriteriaJointConjunction, EntityCriteriaOperator, EntityCriteriaStatement, NoCriteriaForUpdateException, \
@@ -8,72 +8,74 @@ from watchmen_utilities import ArrayHelper
 from .types import SQLAlchemyStatement
 
 
-def build_criteria_expression(expression: EntityCriteriaExpression):
+def build_criteria_expression(table: Table, expression: EntityCriteriaExpression):
+	column = table.c[expression.name]
 	op = expression.operator
 	if op == EntityCriteriaOperator.IS_EMPTY:
-		return column(expression.name).is_(None)
+		return column.is_(None)
 	elif op == EntityCriteriaOperator.IS_NOT_EMPTY:
-		return column(expression.name).is_not(None)
+		return column.is_not(None)
 	elif op == EntityCriteriaOperator.EQUALS:
-		return column(expression.name) == expression.value
+		return column == expression.value
 	elif op == EntityCriteriaOperator.NOT_EQUALS:
-		return column(expression.name) != expression.value
+		return column != expression.value
 	elif op == EntityCriteriaOperator.LESS_THAN:
-		return column(expression.name) < expression.value
+		return column < expression.value
 	elif op == EntityCriteriaOperator.LESS_THAN_OR_EQUALS:
-		return column(expression.name) <= expression.value
+		return column <= expression.value
 	elif op == EntityCriteriaOperator.GREATER_THAN:
-		return column(expression.name) > expression.value
+		return column > expression.value
 	elif op == EntityCriteriaOperator.GREATER_THAN_OR_EQUALS:
-		return column(expression.name) >= expression.value
+		return column >= expression.value
 	elif op == EntityCriteriaOperator.IN:
-		return column(expression.name).in_(expression.value)
+		return column.in_(expression.value)
 	elif op == EntityCriteriaOperator.NOT_IN:
-		return column(expression.name).not_in(expression.value)
+		return column.not_in(expression.value)
 	elif op == EntityCriteriaOperator.LIKE:
-		return column(expression.name).ilike(f'%{expression.value}%')
+		return column.ilike(f'%{expression.value}%')
 	elif op == EntityCriteriaOperator.NOT_LIKE:
-		return column(expression.name).not_ilike(f'%{expression.value}%')
+		return column.not_ilike(f'%{expression.value}%')
 	else:
 		raise UnsupportedCriteriaExpressionOperatorException(f'Unsupported criteria expression operator[{op}].')
 
 
-def build_criteria_joint(joint: EntityCriteriaJoint):
+def build_criteria_joint(table: Table, joint: EntityCriteriaJoint):
 	conjunction = joint.conjunction
 	if conjunction == EntityCriteriaJointConjunction.AND:
-		return and_(*ArrayHelper(joint.children).map(build_criteria_statement).to_list())
+		return and_(*ArrayHelper(joint.children).map(lambda x: build_criteria_statement(table, x)).to_list())
 	elif conjunction == EntityCriteriaJointConjunction.OR:
-		return or_(*ArrayHelper(joint.children).map(build_criteria_statement).to_list())
+		return or_(*ArrayHelper(joint.children).map(lambda x: build_criteria_statement(table, x)).to_list())
 	else:
 		raise UnsupportedCriteriaJointConjunctionException(
 			f'Unsupported criteria joint conjunction[{conjunction}].')
 
 
-def build_criteria_statement(statement: EntityCriteriaStatement):
+def build_criteria_statement(table: Table, statement: EntityCriteriaStatement):
 	if isinstance(statement, EntityCriteriaExpression):
-		return build_criteria_expression(statement)
+		return build_criteria_expression(table, statement)
 	elif isinstance(statement, EntityCriteriaJoint):
-		return build_criteria_joint(statement)
+		return build_criteria_joint(table, statement)
 	else:
 		raise UnsupportedCriteriaException(f'Unsupported criteria[{statement}].')
 
 
-def build_criteria(criteria: EntityCriteria):
+def build_criteria(table: Table, criteria: EntityCriteria):
 	if criteria is None or len(criteria) == 0:
 		return None
 
 	if len(criteria) == 1:
-		return build_criteria_statement(criteria[0])
+		return build_criteria_statement(table, criteria[0])
 	else:
-		return build_criteria_statement(EntityCriteriaJoint(children=criteria))
+		return build_criteria_statement(table, EntityCriteriaJoint(children=criteria))
 
 
 def build_criteria_for_statement(
+		table: Table,
 		statement: SQLAlchemyStatement,
 		criteria: EntityCriteria,
 		raise_exception_on_missed: bool = False
 ) -> SQLAlchemyStatement:
-	where = build_criteria(criteria)
+	where = build_criteria(table, criteria)
 	if where is not None:
 		return statement.where(where)
 	elif raise_exception_on_missed:
