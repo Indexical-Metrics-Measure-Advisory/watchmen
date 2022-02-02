33761861146671d9145e89eebfe55ab435356bf6
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-02 21:58:12 +0800

    initialize

--- packages/watchmen-meta-service/src/watchmen_meta_service/system/__init__.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/system/__init__.py
@@ -1,3 +1,4 @@
 from .data_source import DataSourceService
+from .external_writers import ExternalWriterService
 from .pat import PatService
 from .tenant import TenantService
--- packages/watchmen-rest-doll/src/watchmen_rest_doll/main.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/main.py
@@ -1,7 +1,7 @@
 from .admin import user
 from .auth import authenticator
 from .doll import doll
-from .system import data_source, pat, tenant
+from .system import data_source, external_writer, pat, tenant
 
 app = doll.construct()
 
@@ -9,4 +9,5 @@ app.include_router(authenticator.router)
 app.include_router(pat.router)
 app.include_router(tenant.router)
 app.include_router(data_source.router)
+app.include_router(external_writer.router)
 app.include_router(user.router)
