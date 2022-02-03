6666b7e00edab25cabac49be4d476bcc93273b63
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-03 16:18:12 +0800

    initialize

--- packages/watchmen-utilities/src/watchmen_utilities/array_helper.py
+++ packages/watchmen-utilities/src/watchmen_utilities/array_helper.py
@@ -5,30 +5,81 @@ from typing import Any, Callable, Optional
 ArrayPredicate = Callable[[Any], bool]
 ArrayTransform = Callable[[Any], Any]
 ArrayAction = Callable[[Any], None]
+ArrayCompare = Callable[[Any, Any], bool]
+
+
+def equals(a: Any, b: Any) -> bool:
+	return a == b
 
 
 class ArrayHelper:
-	def __init__(self, a_list: list):
-		self.a_list = a_list
+	def __init__(self, a_list: Optional[list]):
+		if a_list is None:
+			self.a_list = []
+		else:
+			self.a_list = a_list
 
 	def to_list(self):
 		return self.a_list
 
-	def each(self, func: ArrayAction) -> None:
-		for a_element in self.a_list:
-			func(a_element)
+	def each(self, func: ArrayAction) -> ArrayHelper:
+		"""
+		apply given function to each element, and return myself
+		"""
+		for an_element in self.a_list:
+			func(an_element)
+		return self
 
 	def filter(self, func: ArrayPredicate) -> ArrayHelper:
+		"""
+		pick elements which satisfies the given predicate function
+		"""
 		new_list: list = []
-		for a_element in self.a_list:
-			if func(a_element):
-				new_list.append(a_element)
+		for an_element in self.a_list:
+			if func(an_element):
+				new_list.append(an_element)
 		return ArrayHelper(new_list)
 
 	def map(self, func: ArrayTransform) -> ArrayHelper:
+		"""
+		transform each element by given transform function
+		"""
 		new_list: list = []
-		for a_element in self.a_list:
-			new_list.append(func(a_element))
+		for an_element in self.a_list:
+			new_list.append(func(an_element))
+		return ArrayHelper(new_list)
+
+	def distinct(self, func: ArrayCompare = equals) -> ArrayHelper:
+		"""
+		remove elements duplicated which satisfies given compare function
+		"""
+		new_list: list = []
+		for an_element in self.a_list:
+			found: bool = False
+			for an_existing_element in new_list:
+				if func(an_element, an_existing_element):
+					found = True
+					break
+			if not found:
+				new_list.append(an_element)
+		return ArrayHelper(new_list)
+
+	def difference(self, another_list: Optional[list], func: ArrayCompare = equals) -> ArrayHelper:
+		"""
+		pick elements which included in self but not in another
+		"""
+		if another_list is None or len(another_list) == 0:
+			return self
+
+		new_list: list = []
+		for an_element in self.a_list:
+			found: bool = False
+			for an_another_element in another_list:
+				if func(an_element, an_another_element):
+					found = True
+					break
+			if not found:
+				new_list.append(an_element)
 		return ArrayHelper(new_list)
 
 	def first(
@@ -39,8 +90,8 @@ class ArrayHelper:
 		"""
 		first one of array which match the found function after transform_to function
 		"""
-		for a_element in self.a_list:
-			new_element = transform_to(a_element)
+		for an_element in self.a_list:
+			new_element = transform_to(an_element)
 			if found(new_element):
 				return new_element
 		return None
