f58c5f83f71d8fba6606f61bdff67722da8b7589
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 15:45:01 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/admin/space.py
+++ packages/watchmen-model/src/watchmen_model/admin/space.py
@@ -1,9 +1,9 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from pydantic import BaseModel
 
-from watchmen_model.common import DataModel, OptimisticLock, ParameterJoint, SpaceId, TenantBasedTuple, TopicId, \
-	UserGroupId
+from watchmen_model.common import construct_parameter_joint, DataModel, OptimisticLock, ParameterJoint, SpaceId, \
+	TenantBasedTuple, TopicId, UserGroupId
 from watchmen_utilities import ArrayHelper
 
 
@@ -12,11 +12,32 @@ class SpaceFilter(DataModel, BaseModel):
 	joint: ParameterJoint = None
 	enabled: bool = False
 
+	def __setattr__(self, name, value):
+		if name == 'joint':
+			super().__setattr__(name, construct_parameter_joint(value))
+		else:
+			super().__setattr__(name, value)
 
-def construct_filters(filters: Optional[list] = None) -> Optional[List[SpaceFilter]]:
+
+def construct_filter(a_filter: Optional[Union[dict, SpaceFilter]]) -> Optional[SpaceFilter]:
+	if a_filter is None:
+		return None
+	elif isinstance(a_filter, SpaceFilter):
+		return a_filter
+	else:
+		# noinspection PyArgumentList
+		return SpaceFilter(
+			topicId=a_filter.get('topicId'),
+			joint=a_filter.get('joint'),
+			enabled=a_filter.get('enabled')
+		)
+
+
+def construct_filters(filters: Optional[Union[list, str]] = None) -> Optional[List[SpaceFilter]]:
 	if filters is None:
 		return None
-	return ArrayHelper(filters).map(lambda x: SpaceFilter(**x)).to_list()
+	else:
+		return ArrayHelper(filters).map(lambda x: construct_filter(x)).to_list()
 
 
 class Space(TenantBasedTuple, OptimisticLock, BaseModel):
@@ -29,7 +50,6 @@ class Space(TenantBasedTuple, OptimisticLock, BaseModel):
 
 	def __setattr__(self, name, value):
 		if name == 'filters':
-			filters = construct_filters(value)
-			super().__setattr__(name, filters)
+			super().__setattr__(name, construct_filters(value))
 		else:
 			super().__setattr__(name, value)
--- packages/watchmen-rest-doll/src/watchmen_rest_doll/auth/__init__.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/auth/__init__.py
@@ -1,2 +1,2 @@
-from .authenticate_router import get_admin_principal, get_any_admin_principal, get_any_principal, get_console_principal, \
-	get_super_admin_principal
+from .authenticate_router import get_admin_principal, get_any_admin_principal, get_any_principal, \
+	get_console_principal, get_super_admin_principal
