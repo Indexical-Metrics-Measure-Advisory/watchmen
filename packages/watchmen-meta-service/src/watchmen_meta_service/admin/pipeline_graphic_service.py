f84c66e38a869842e28af439487b14026e400c38
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 18:04:28 +0800

    initialize

--- packages/watchmen-meta-service/src/watchmen_meta_service/admin/pipeline_graphic_service.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/admin/pipeline_graphic_service.py
@@ -1,11 +1,8 @@
-from typing import List, Optional
-
-from watchmen_auth import PrincipalService
-from watchmen_meta_service.common import StorageService, TupleService
+from watchmen_meta_service.common import UserBasedTupleService
 from watchmen_model.admin import PipelineGraphic
-from watchmen_model.common import PipelineGraphicId, TenantId, UserId
-from watchmen_storage import EntityCriteriaExpression, EntityFinder, EntityHelper, EntityIdHelper, EntityRow, \
-	EntityShaper, SnowflakeGenerator, TransactionalStorageSPI
+from watchmen_model.common import PipelineGraphicId
+from watchmen_storage import EntityRow, \
+	EntityShaper
 
 
 class PipelineGraphicShaper(EntityShaper):
@@ -32,76 +29,19 @@ PIPELINE_GRAPHIC_ENTITY_NAME = 'pipeline_graphics'
 PIPELINE_GRAPHIC_ENTITY_SHAPER = PipelineGraphicShaper()
 
 
-class PipelineGraphicService(StorageService):
-	def __init__(
-			self,
-			storage: TransactionalStorageSPI,
-			snowflake_generator: SnowflakeGenerator,
-			principal_service: PrincipalService,
-
-	):
-		super().__init__(storage)
-		self.with_snowflake_generator(snowflake_generator)
-		self.with_principal_service(principal_service)
-
-	# noinspection PyMethodMayBeStatic
+class PipelineGraphicService(UserBasedTupleService):
 	def get_entity_name(self) -> str:
 		return PIPELINE_GRAPHIC_ENTITY_NAME
 
-	# noinspection PyMethodMayBeStatic
-	def get_id_column_name(self) -> str:
-		return 'pipeline_graphic_id'
-
-	# noinspection PyMethodMayBeStatic
 	def get_entity_shaper(self) -> EntityShaper:
 		return PIPELINE_GRAPHIC_ENTITY_SHAPER
 
-	def get_entity_helper(self) -> EntityHelper:
-		return EntityHelper(name=self.get_entity_name(), shaper=self.get_entity_shaper())
+	def get_tuple_id_column_name(self) -> str:
+		return 'pipeline_graphic_id'
 
-	def get_entity_id_helper(self) -> EntityIdHelper:
-		return EntityIdHelper(
-			name=self.get_entity_name(),
-			shaper=self.get_entity_shaper(),
-			idColumnName=self.get_id_column_name()
-		)
+	def get_tuple_id(self, a_tuple: PipelineGraphic) -> PipelineGraphicId:
+		return a_tuple.pipelineGraphId
 
-	def generate_pipeline_graphic_id(self) -> PipelineGraphicId:
-		return str(self.snowflake_generator.next_id())
-
-	def redress_tuple_id(self, pipeline_graphic: PipelineGraphic) -> PipelineGraphic:
-		"""
-		return exactly the given tuple, replace by generated id if it is faked
-		"""
-		if TupleService.is_tuple_id_faked(pipeline_graphic.pipelineGraphId):
-			pipeline_graphic.pipelineGraphId = self.generate_pipeline_graphic_id()
-		return pipeline_graphic
-
-	def create(self, pipeline_graphic: PipelineGraphic) -> None:
-		return self.storage.insert_one(pipeline_graphic, self.get_entity_helper())
-
-	def update(self, pipeline_graphic: PipelineGraphic) -> None:
-		self.storage.update_one(pipeline_graphic, self.get_entity_id_helper())
-
-	def find_by_id(self, pipeline_graphic_id: PipelineGraphicId) -> Optional[PipelineGraphic]:
-		return self.storage.find_one(EntityFinder(
-			name=self.get_entity_name(),
-			shaper=self.get_entity_shaper(),
-			criteria=[
-				EntityCriteriaExpression(name=self.get_id_column_name(), value=pipeline_graphic_id),
-			]
-		))
-
-	def find_all_by_id(self, user_id: UserId, tenant_id: TenantId) -> List[PipelineGraphic]:
-		# noinspection PyTypeChecker
-		return self.storage.find(EntityFinder(
-			name=self.get_entity_name(),
-			shaper=self.get_entity_shaper(),
-			criteria=[
-				EntityCriteriaExpression(name='user_id', value=user_id),
-				EntityCriteriaExpression(name='tenant_id', value=tenant_id)
-			]
-		))
-
-	def delete_by_id(self, pipeline_graphic_id: PipelineGraphicId) -> None:
-		self.storage.delete_by_id(pipeline_graphic_id, self.get_entity_id_helper())
+	def set_tuple_id(self, a_tuple: PipelineGraphic, tuple_id: PipelineGraphicId) -> PipelineGraphic:
+		a_tuple.pipelineGraphId = tuple_id
+		return a_tuple
--- packages/watchmen-meta-service/src/watchmen_meta_service/common/__init__.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/common/__init__.py
@@ -1,2 +1,3 @@
 from .storage_service import StorageService
 from .tuple_service import AuditableShaper, OptimisticLockShaper, TupleNotFoundException, TupleService, TupleShaper
+from .user_based_tuple_service import UserBasedTupleService
