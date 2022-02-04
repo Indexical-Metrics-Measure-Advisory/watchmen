f1ca58ed155bff7480b63a41af649f77a04c4eaa
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 13:34:21 +0800

    initialize

--- packages/watchmen-meta-service/src/watchmen_meta_service/admin/space_service.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/admin/space_service.py
@@ -2,11 +2,26 @@ from typing import List, Optional
 
 from watchmen_meta_service.common import TupleService, TupleShaper
 from watchmen_model.admin import Space
+from watchmen_model.admin.space import SpaceFilter
 from watchmen_model.common import DataPage, Pageable, SpaceId, TenantId
 from watchmen_storage import EntityCriteriaExpression, EntityCriteriaOperator, EntityRow, EntityShaper
+from watchmen_utilities import ArrayHelper
 
 
 class SpaceShaper(EntityShaper):
+	@staticmethod
+	def serialize_filter(space_filter: SpaceFilter) -> dict:
+		if isinstance(space_filter, dict):
+			return space_filter
+		else:
+			return space_filter.dict()
+
+	@staticmethod
+	def serialize_filters(filters: Optional[List[SpaceFilter]]) -> Optional[list]:
+		if filters is None:
+			return None
+		return ArrayHelper(filters).map(lambda x: SpaceShaper.serialize_filter(x)).to_list()
+
 	def serialize(self, space: Space) -> EntityRow:
 		return TupleShaper.serialize_tenant_based(space, {
 			'space_id': space.spaceId,
@@ -14,7 +29,7 @@ class SpaceShaper(EntityShaper):
 			'description': space.description,
 			'topic_ids': space.topicIds,
 			'group_ids': space.groupIds,
-			'filters': space.filters,
+			'filters': SpaceShaper.serialize_filters(space.filters),
 		})
 
 	def deserialize(self, row: EntityRow) -> Space:
--- packages/watchmen-rest-doll/src/watchmen_rest_doll/main.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/main.py
@@ -1,8 +1,8 @@
-from .admin import enumeration, pipeline, space, topic, user, user_group
-from .auth import authenticator
+from .admin import enumeration_router, pipeline_router, space_router, topic_router, user_group_router, user_router
+from .auth import authenticate_router
 from .doll import doll
-from .gui import favorite, last_snapshot
-from .system import data_source, external_writer, pat, tenant
+from .gui import favorite_router, last_snapshot_router
+from .system import data_source_router, external_writer_router, pat_router, tenant_router
 
 app = doll.construct()
 
@@ -12,18 +12,18 @@ def startup():
 	doll.on_startup(app)
 
 
-app.include_router(authenticator.router)
-app.include_router(pat.router)
-app.include_router(tenant.router)
-app.include_router(data_source.router)
-app.include_router(external_writer.router)
+app.include_router(authenticate_router.router)
+app.include_router(pat_router.router)
+app.include_router(tenant_router.router)
+app.include_router(data_source_router.router)
+app.include_router(external_writer_router.router)
 
-app.include_router(user.router)
-app.include_router(user_group.router)
-app.include_router(space.router)
-app.include_router(enumeration.router)
-app.include_router(topic.router)
-app.include_router(pipeline.router)
+app.include_router(user_router.router)
+app.include_router(user_group_router.router)
+app.include_router(space_router.router)
+app.include_router(enumeration_router.router)
+app.include_router(topic_router.router)
+app.include_router(pipeline_router.router)
 
-app.include_router(favorite.router)
-app.include_router(last_snapshot.router)
+app.include_router(favorite_router.router)
+app.include_router(last_snapshot_router.router)
