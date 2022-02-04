c3ce5660b26cc1207eb846704765f1f98a84e96b
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 13:34:21 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/admin/space.py
+++ packages/watchmen-model/src/watchmen_model/admin/space.py
@@ -1,15 +1,22 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel
 
 from watchmen_model.common import DataModel, OptimisticLock, ParameterJoint, SpaceId, TenantBasedTuple, TopicId, \
 	UserGroupId
+from watchmen_utilities import ArrayHelper
 
 
 class SpaceFilter(DataModel, BaseModel):
 	topicId: TopicId = None
 	joint: ParameterJoint = None
 	enabled: bool = False
+
+
+def construct_filters(filters: Optional[list] = None) -> Optional[List[SpaceFilter]]:
+	if filters is None:
+		return None
+	return ArrayHelper(filters).map(lambda x: SpaceFilter(**x)).to_list()
 
 
 class Space(TenantBasedTuple, OptimisticLock, BaseModel):
@@ -19,3 +26,10 @@ class Space(TenantBasedTuple, OptimisticLock, BaseModel):
 	topicIds: List[TopicId] = None
 	groupIds: List[UserGroupId] = None
 	filters: List[SpaceFilter] = None
+
+	def __setattr__(self, name, value):
+		if name == 'filters':
+			filters = construct_filters(value)
+			super().__setattr__(name, filters)
+		else:
+			super().__setattr__(name, value)
