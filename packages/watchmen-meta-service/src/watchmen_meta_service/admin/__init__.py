51c71aab0d9ce0000b30c2e0ade1a6888dcdbc57
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 11:29:45 +0800

    initialize

--- packages/watchmen-meta-service/src/watchmen_meta_service/admin/__init__.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/admin/__init__.py
@@ -1,6 +1,6 @@
 from .enumeration_service import EnumItemService, EnumService
 from .pipeline_service import PipelineService
 from .space_service import SpaceService
-from .topic_service import TopicService
+from .topic_service import FactorService, TopicService
 from .user_group_service import UserGroupService
 from .user_service import UserService
