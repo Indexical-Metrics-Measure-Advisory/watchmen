7a9e594289178f8a4e3d571dea0fe560b53d4de9
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-04 15:59:32 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/common/parameter_and_condition.py
+++ packages/watchmen-model/src/watchmen_model/common/parameter_and_condition.py
@@ -20,6 +20,12 @@ class Parameter(DataModel, BaseModel):
 	conditional: bool = False
 	on: Optional[ParameterJoint] = None
 
+	def __setattr__(self, name, value):
+		if name == 'on':
+			super().__setattr__(name, construct_parameter_joint(value))
+		else:
+			super().__setattr__(name, value)
+
 
 class TopicFactorParameter(Parameter):
 	kind: ParameterKind.TOPIC = ParameterKind.TOPIC
@@ -54,6 +60,12 @@ class ComputedParameter(Parameter):
 	kind: ParameterKind.COMPUTED = ParameterKind.COMPUTED
 	type: ParameterComputeType = ParameterComputeType.NONE
 	parameters: List[Parameter] = []
+
+	def __setattr__(self, name, value):
+		if name == 'parameters':
+			super().__setattr__(name, construct_parameters(value))
+		else:
+			super().__setattr__(name, value)
 
 
 class ParameterCondition(DataModel, BaseModel):
@@ -94,6 +106,35 @@ class ParameterExpression(ParameterCondition, BaseModel):
 	operator: ParameterExpressionOperator = ParameterExpressionOperator.EQUALS
 	right: Parameter = None
 
+	def __setattr__(self, name, value):
+		if name == 'left' or name == 'right':
+			super().__setattr__(name, construct_parameter(value))
+		else:
+			super().__setattr__(name, value)
+
+
+def construct_parameter(parameter: Optional[Union[dict | Parameter]]) -> Optional[Parameter]:
+	if parameter is None:
+		return None
+	elif isinstance(parameter, Parameter):
+		return parameter
+	else:
+		kind = parameter.get('kind')
+		if kind == ParameterKind.TOPIC:
+			return TopicFactorParameter(**parameter)
+		elif kind == ParameterKind.CONSTANT:
+			return ConstantParameter(**parameter)
+		elif kind == ParameterKind.COMPUTED:
+			return ComputedParameter(**parameter)
+		else:
+			raise Exception(f'Parameter kind[{kind}] cannot be determined.')
+
+
+def construct_parameters(parameters: Optional[list]) -> Optional[List[Parameter]]:
+	if parameters is None:
+		return None
+	return ArrayHelper(parameters).map(lambda x: construct_parameter(x)).to_list()
+
 
 def construct_parameter_condition(condition: Optional[Union[dict, ParameterCondition]]) -> Optional[ParameterCondition]:
 	if condition is None:
@@ -113,7 +154,6 @@ def construct_parameter_condition(condition: Optional[Union[dict, ParameterCondi
 def construct_parameter_conditions(conditions: Optional[list]) -> Optional[List[ParameterCondition]]:
 	if conditions is None:
 		return None
-
 	return ArrayHelper(conditions).map(lambda x: construct_parameter_condition(x)).to_list()
 
 
