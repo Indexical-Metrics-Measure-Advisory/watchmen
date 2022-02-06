2c2f8ba214494180a638b53d8854722121bd011d
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-06 09:47:35 +0800

    initialize

--- packages/watchmen-rest-doll/src/watchmen_rest_doll/console/report_router.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/console/report_router.py
@@ -11,3 +11,23 @@ def get_report_service(principal_service: PrincipalService) -> ReportService:
 	return ReportService(ask_meta_storage(), ask_snowflake_generator(), principal_service)
 
 # TODO report routers
+# @router.post("/console_space/subject/report/save", tags=["console"], response_model=Report)
+# async def save_report(subject_id: str, report: Report, current_user: User = Depends(deps.get_current_user)):
+# 	report = add_tenant_id_to_model(report, current_user)
+# 	report.reportId = get_surrogate_key()
+# 	new_report = create_report(report)
+# 	subject = load_console_subject_by_id(subject_id, current_user)
+# 	subject.reportIds.append(new_report.reportId)
+# 	update_console_subject(subject)
+# 	return new_report
+# @router.post("/console_space/subject/report/update", tags=["console"], response_model=Report)
+# async def update_report(report: Report, current_user: User = Depends(deps.get_current_user)):
+# 	report = add_tenant_id_to_model(report, current_user)
+# 	save_subject_report(report)
+# 	return report
+# @router.get("/console_space/subject/report/delete", tags=["console"])
+# async def delete_report(report_id, current_user: User = Depends(deps.get_current_user)):
+# 	subject = load_console_subject_by_report_id(report_id, current_user)
+# 	subject.reportIds.remove(report_id)
+# 	update_console_subject(subject)
+# 	delete_report_by_id(report_id)
