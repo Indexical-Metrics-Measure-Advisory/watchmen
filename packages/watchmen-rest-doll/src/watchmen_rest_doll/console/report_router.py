ea060a58bdf7cf403c1dd959908c5f719aaddbcb
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-06 20:41:52 +0800

    initialize

--- packages/watchmen-rest-doll/src/watchmen_rest_doll/console/report_router.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/console/report_router.py
@@ -5,10 +5,11 @@ from fastapi import APIRouter, Depends, HTTPException
 from watchmen_auth import PrincipalService
 from watchmen_meta_service.console import ReportService, SubjectService
 from watchmen_model.admin import UserRole
+from watchmen_model.common import ReportId
 from watchmen_model.console import Report, Subject
-from watchmen_rest.util import raise_400, raise_403, raise_500
-from watchmen_rest_doll.auth import get_console_principal
-from watchmen_rest_doll.doll import ask_meta_storage, ask_snowflake_generator
+from watchmen_rest.util import raise_400, raise_403, raise_404, raise_500
+from watchmen_rest_doll.auth import get_console_principal, get_super_admin_principal
+from watchmen_rest_doll.doll import ask_meta_storage, ask_snowflake_generator, ask_tuple_delete_enabled
 from watchmen_rest_doll.util import is_blank
 from watchmen_utilities import get_current_time_in_seconds
 
@@ -79,24 +80,58 @@ async def save_report(
 
 	return report
 
-# TODO report routers
-# @router.post("/console_space/subject/report/save", tags=["console"], response_model=Report)
-# async def save_report(subject_id: str, report: Report, current_user: User = Depends(deps.get_current_user)):
-# 	report = add_tenant_id_to_model(report, current_user)
-# 	report.reportId = get_surrogate_key()
-# 	new_report = create_report(report)
-# 	subject = load_console_subject_by_id(subject_id, current_user)
-# 	subject.reportIds.append(new_report.reportId)
-# 	update_console_subject(subject)
-# 	return new_report
-# @router.post("/console_space/subject/report/update", tags=["console"], response_model=Report)
-# async def update_report(report: Report, current_user: User = Depends(deps.get_current_user)):
-# 	report = add_tenant_id_to_model(report, current_user)
-# 	save_subject_report(report)
-# 	return report
-# @router.get("/console_space/subject/report/delete", tags=["console"])
-# async def delete_report(report_id, current_user: User = Depends(deps.get_current_user)):
-# 	subject = load_console_subject_by_report_id(report_id, current_user)
-# 	subject.reportIds.remove(report_id)
-# 	update_console_subject(subject)
-# 	delete_report_by_id(report_id)
+
+@router.get('/report/delete', tags=[UserRole.CONSOLE, UserRole.ADMIN], response_model=None)
+async def delete_report(
+		report_id: Optional[ReportId], principal_service: PrincipalService = Depends(get_console_principal)
+) -> None:
+	if is_blank(report_id):
+		raise_400('Report id is required.')
+
+	report_service = get_report_service(principal_service)
+
+	report_service.begin_transaction()
+	try:
+		# noinspection PyTypeChecker
+		existing_report: Optional[Report] = report_service.find_by_id(report_id)
+		if existing_report is None:
+			raise_404()
+		if existing_report.tenantId != principal_service.get_tenant_id():
+			raise_403()
+		if not principal_service.is_tenant_admin() and existing_report.userId != principal_service.get_user_id():
+			raise_403()
+		report_service.delete(report_id)
+	except HTTPException as e:
+		report_service.rollback_transaction()
+		raise e
+	except Exception as e:
+		report_service.rollback_transaction()
+		raise_500(e)
+
+
+@router.delete('/report', tags=[UserRole.SUPER_ADMIN], response_model=Report)
+async def delete_user_by_id(
+		report_id: Optional[ReportId] = None,
+		principal_service: PrincipalService = Depends(get_super_admin_principal)
+) -> Optional[Report]:
+	if not ask_tuple_delete_enabled():
+		raise_404('Not Found')
+
+	if is_blank(report_id):
+		raise_400('Report id is required.')
+
+	report_service = get_report_service(principal_service)
+	report_service.begin_transaction()
+	try:
+		# noinspection PyTypeChecker
+		report: Report = report_service.delete(report_id)
+		if report is None:
+			raise_404()
+		report_service.commit_transaction()
+		return report
+	except HTTPException as e:
+		report_service.rollback_transaction()
+		raise e
+	except Exception as e:
+		report_service.rollback_transaction()
+		raise_500(e)
