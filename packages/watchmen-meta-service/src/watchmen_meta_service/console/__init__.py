11438b85d5e189022f27112f96e721a65d035e03
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-06 09:42:10 +0800

    initialize

--- packages/watchmen-meta-service/src/watchmen_meta_service/console/__init__.py
+++ packages/watchmen-meta-service/src/watchmen_meta_service/console/__init__.py
@@ -1,4 +1,5 @@
 from .connected_space_graphic_service import ConnectedSpaceGraphicService
 from .connected_space_service import ConnectedSpaceService
+from .dashboard_service import DashboardService
 from .report_service import ReportService
 from .subject_service import SubjectService
--- packages/watchmen-model/src/watchmen_model/console/dashboard.py
+++ packages/watchmen-model/src/watchmen_model/console/dashboard.py
@@ -1,21 +1,23 @@
 from typing import List
 
-from watchmen_model.common import DashboardId, DataModel, GraphicRect, LastVisit, ReportId, UserBasedTuple
+from pydantic import BaseModel
+
+from watchmen_model.common import Auditable, DashboardId, DataModel, GraphicRect, LastVisit, ReportId, UserBasedTuple
 from .report import ReportFunnel
 
 
-class DashboardReport(DataModel):
+class DashboardReport(DataModel, BaseModel):
 	reportId: ReportId = None
 	funnels: List[ReportFunnel] = None
 	rect: GraphicRect = None
 
 
-class DashboardParagraph(DataModel):
+class DashboardParagraph(DataModel, BaseModel):
 	content: str = None
 	rect: GraphicRect = None
 
 
-class Dashboard(UserBasedTuple, LastVisit):
+class Dashboard(UserBasedTuple, Auditable, LastVisit, BaseModel):
 	dashboardId: DashboardId = None
 	name: str = None
 	reports: List[DashboardReport] = None
