86da9f0ef42317a32eaee85acd35a8f838417ab0
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-08 22:21:41 +0800

    initialize

--- packages/watchmen-reactor-surface/src/watchmen_reactor_surface/cache/cache_router.py
+++ packages/watchmen-reactor-surface/src/watchmen_reactor_surface/cache/cache_router.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends
+
 from watchmen_auth import PrincipalService
 from watchmen_model.admin import UserRole
 from watchmen_reactor_service.cache import CacheService
@@ -10,3 +11,19 @@ router = APIRouter()
 @router.get('/cache/clear/all', tags=[UserRole.SUPER_ADMIN], response_model=None)
 async def clear_all_cache(principal_service: PrincipalService = Depends(get_super_admin_principal)) -> None:
 	CacheService.clear_all()
+
+
+@router.get('/cache/clear/topic/all', tags=[UserRole.SUPER_ADMIN], response_model=None)
+async def clear_all_topic_cache(principal_service: PrincipalService = Depends(get_super_admin_principal)) -> None:
+	CacheService.topic().clear()
+
+
+@router.get('/cache/clear/pipeline/all', tags=[UserRole.SUPER_ADMIN], response_model=None)
+async def clear_all_pipelines_cache(principal_service: PrincipalService = Depends(get_super_admin_principal)) -> None:
+	CacheService.pipeline().clear()
+
+
+@router.get('/cache/clear/datasource/all', tags=[UserRole.SUPER_ADMIN], response_model=None)
+async def clear_all_data_sources_cache(
+		principal_service: PrincipalService = Depends(get_super_admin_principal)) -> None:
+	CacheService.data_source().clear()
