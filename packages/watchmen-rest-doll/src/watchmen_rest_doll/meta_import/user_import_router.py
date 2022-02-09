e717904f0091e91c99f5e86e2356a5b093f7738a
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-09 10:28:30 +0800

    initialize

---
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/meta_import/user_import_router.py
@@ -0,0 +1,45 @@
+from typing import List
+
+from fastapi import APIRouter, Depends
+from starlette.responses import Response
+
+from watchmen_rest_doll.admin import ask_save_user_action
+from watchmen_rest_doll.auth import get_any_admin_principal
+from watchmen_rest_doll.doll import ask_meta_storage, ask_snowflake_generator
+from watchmen_auth import PrincipalService
+from watchmen_meta.admin import UserService
+from watchmen_model.admin import User, UserRole
+from watchmen_rest_doll.util import trans
+from watchmen_utilities import ArrayHelper
+
+router = APIRouter()
+
+
+def get_user_service(principal_service: PrincipalService) -> UserService:
+	return UserService(ask_meta_storage(), ask_snowflake_generator(), principal_service)
+
+
+# @router.post("/import/admin/user", tags=["import"])
+# async def import_user(user: User):
+#     result = get_user(user.userId)
+#     if result is None:
+#         import_user_to_db(user)
+#     else:
+#         update_user_storage(user)
+
+@router.post('/user/import', tags=[UserRole.ADMIN, UserRole.SUPER_ADMIN], response_class=Response)
+async def import_user(
+		users: List[User], principal_service: PrincipalService = Depends(get_any_admin_principal)) -> None:
+	if users is None:
+		return
+	if len(users) == 0:
+		return
+
+	user_service = get_user_service(principal_service)
+
+	def action() -> None:
+		save = ask_save_user_action(user_service, principal_service)
+		# noinspection PyTypeChecker
+		ArrayHelper(users).each(lambda x: save(x))
+
+	trans(user_service, action)
