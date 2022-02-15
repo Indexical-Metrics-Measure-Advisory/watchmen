5dcf0a1d34826827322fe8e17f99569d0a1c2a12
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-15 11:23:44 +0800

    initialize

--- packages/watchmen-reactor/src/watchmen_reactor/storage/data_service.py
+++ packages/watchmen-reactor/src/watchmen_reactor/storage/data_service.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from datetime import datetime
 from logging import getLogger
 from typing import Any, Dict, Optional, Tuple
@@ -53,41 +54,57 @@ class TopicDataService:
 		has_id, _ = self.get_data_entity_helper().find_data_id(data)
 		return has_id
 
+	@abstractmethod
+	def try_to_wrap_data(self, data: Dict[str, Any]) -> Dict[str, Any]:
+		pass
+
+	@abstractmethod
+	def try_to_unwrap_topic_data(self, topic_data: Dict[str, Any]) -> Dict[str, Any]:
+		pass
+
 	def raise_on_topic(self) -> str:
 		topic = self.get_schema().get_topic()
 		return f'topic[id={topic.topicId}, name={topic.name}]'
 
 	# noinspection PyMethodMayBeStatic
 	def raise_exception(self, message: str, e: Optional[Exception] = None) -> None:
-		if e is not None:
-			logger.error(e, exc_info=True, stack_info=True)
+		# if e is not None:
+		# 	logger.error(e, exc_info=True, stack_info=True)
 		raise ReactorException(message)
 
 	def trigger_by_insert(
 			self, data: Dict[str, Any]
 	) -> Tuple[Optional[Dict[str, Any]], Optional[Dict[str, Any]], PipelineTriggerType]:
+		"""
+		data is pure data
+		"""
 		data_entity_helper = self.get_data_entity_helper()
 		storage = self.get_storage()
 		try:
+			topic_data = self.try_to_wrap_data(data)
 			data_entity_helper.assign_fix_columns_on_create(
-				data=data,
+				data=topic_data,
 				snowflake_generator=self.get_snowflake_generator(), principal_service=self.get_principal_service(),
 				now=self.now()
 			)
 			storage.connect()
-			storage.insert_one(data, data_entity_helper.get_entity_helper())
+			storage.insert_one(topic_data, data_entity_helper.get_entity_helper())
 			return None, data, PipelineTriggerType.INSERT
 		except Exception as e:
 			self.raise_exception(f'Failed to create data[{data}] into {self.raise_on_topic()}.', e)
 
 	def find_previous_data_by_id(self, id_: int, raise_on_not_found: bool = False) -> Optional[Dict[str, Any]]:
+		"""
+		return topic data
+		"""
 		data_entity_helper = self.get_data_entity_helper()
 		storage = self.get_storage()
 		storage.connect()
-		previous: Optional[Dict[str, Any]] = storage.find_by_id(id_, data_entity_helper.get_entity_id_helper())
-		if previous is None and raise_on_not_found:
+		previous_topic_data: Optional[Dict[str, Any]] = \
+			storage.find_by_id(id_, data_entity_helper.get_entity_id_helper())
+		if previous_topic_data is None and raise_on_not_found:
 			self.raise_exception(f'Data not found by data[id={id_}] from {self.raise_on_topic()}.')
-		return previous
+		return previous_topic_data
 
 	def trigger_by_merge(
 			self, data: Dict[str, Any]
@@ -100,18 +117,19 @@ class TopicDataService:
 		if not has_id:
 			self.raise_exception(f'Id not found from data[{data}] on merge into {self.raise_on_topic()}.')
 		try:
-			previous = self.find_previous_data_by_id(id_, True)
+			previous_topic_data = self.find_previous_data_by_id(id_, True)
+			topic_data = self.try_to_wrap_data(data)
 			# copy insert time from previous
-			insert_time = data_entity_helper.find_insert_time(previous)
-			data_entity_helper.assign_insert_time(data, insert_time)
-			version = data_entity_helper.find_version(previous)
+			insert_time = data_entity_helper.find_insert_time(previous_topic_data)
+			data_entity_helper.assign_insert_time(topic_data, insert_time)
+			version = data_entity_helper.find_version(previous_topic_data)
 			data_entity_helper.assign_fix_columns_on_update(
-				data=data, principal_service=self.get_principal_service(), now=self.now(), version=version + 1)
-			updated_count = storage.update_one(data, data_entity_helper.get_entity_id_helper())
+				data=topic_data, principal_service=self.get_principal_service(), now=self.now(), version=version + 1)
+			updated_count = storage.update_one(topic_data, data_entity_helper.get_entity_id_helper())
 			if updated_count == 0:
 				raise ReactorException(
 					f'Data not found by data[{data}] on merge into topic[id={topic.topicId}, name={topic.name}].')
-			return previous, data, PipelineTriggerType.MERGE
+			return self.try_to_unwrap_topic_data(previous_topic_data), data, PipelineTriggerType.MERGE
 		except Exception as e:
 			self.raise_exception(f'Failed to merge data[id={id_}] into {self.raise_on_topic()}.', e)
 
@@ -132,10 +150,10 @@ class TopicDataService:
 		if not has_id:
 			self.raise_exception(f'Id not found in data[{data}] on delete from {self.raise_on_topic()}.')
 		try:
-			previous = self.find_previous_data_by_id(id_, True)
+			previous_topic_data = self.find_previous_data_by_id(id_, True)
 			deleted_count = storage.delete_by_id(id_, self.get_data_entity_helper().get_entity_id_helper())
 			if deleted_count == 0:
 				self.raise_exception(f'Data not found by data[{data}] on delete from {self.raise_on_topic()}.')
-			return previous, None, PipelineTriggerType.DELETE
+			return self.try_to_unwrap_topic_data(previous_topic_data), None, PipelineTriggerType.DELETE
 		except Exception as e:
 			self.raise_exception(f'Failed to delete data[id={id_}] on {self.raise_on_topic()}.', e)
