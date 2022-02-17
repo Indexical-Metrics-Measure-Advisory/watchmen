ce8e4e686770b5da38a3960ee23fd4687ffb300a
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-17 18:45:10 +0800

    initialize

--- packages/watchmen-reactor/src/watchmen_reactor/pipeline/runtime/__init__.py
+++ packages/watchmen-reactor/src/watchmen_reactor/pipeline/runtime/__init__.py
@@ -1,6 +1,7 @@
 from .ask_action_defined_as import parse_action_defined_as
-from .ask_prerequisite import PrerequisiteTest, parse_prerequisite, parse_prerequisite_defined_as, PrerequisiteDefinedAs
 from .ask_constant import ConstantValue, parse_constant
+from .ask_prerequisite import parse_prerequisite, parse_prerequisite_defined_as, PrerequisiteDefinedAs, PrerequisiteTest
 from .create_queue_pipeline import CreateQueuePipeline
 from .time_utils import now, spent_ms
+from .utils import get_value_from_pipeline_variables
 from .variables import PipelineVariables
