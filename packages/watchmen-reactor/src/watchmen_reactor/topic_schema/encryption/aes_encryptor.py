79ebb78e09685ddacffba6f88009bfa783b4f482
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-21 20:15:54 +0800

    initialize

--- packages/watchmen-reactor/src/watchmen_reactor/topic_schema/encryption/aes_encryptor.py
+++ packages/watchmen-reactor/src/watchmen_reactor/topic_schema/encryption/aes_encryptor.py
@@ -1,16 +1,14 @@
 from base64 import b64decode, b64encode
 from typing import Any, Union
 
-from Crypto.Cipher import AES
-
 from watchmen_model.admin import FactorEncryptMethod
 from .encryptor import Encryptor
 
 
 class AESEncryptor(Encryptor):
 	def __init__(self, key: str, iv: str):
-		self.key = key
-		self.iv = iv
+		self.key = key.encode('utf-8')
+		self.iv = iv.encode('utf-8')
 
 	def accept(self, method: Union[FactorEncryptMethod, str]) -> bool:
 		return method == FactorEncryptMethod.AES256_PKCS5_PADDING
@@ -25,11 +23,14 @@ class AESEncryptor(Encryptor):
 
 	# noinspection PyMethodMayBeStatic
 	def ask_aes(self):
+		# lazy load
+		# noinspection PyPackageRequirements
+		from Crypto.Cipher import AES
 		return AES.new(self.key, AES.MODE_CFB, self.iv)
 
 	def do_encrypt(self, value: Any) -> Any:
 		value = str(value)
-		return b64encode(self.ask_aes().encrypt(value)).decode()
+		return b64encode(self.ask_aes().encrypt(value.encode('utf-8'))).decode()
 
 	def do_decrypt(self, value: str) -> str:
 		# remove prefix and decrypt
