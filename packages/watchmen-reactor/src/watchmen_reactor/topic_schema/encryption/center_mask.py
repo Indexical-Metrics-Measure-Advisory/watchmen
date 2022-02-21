5a6c8fb1065778d037fa9551e6f88e758d4b597e
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-21 20:15:54 +0800

    initialize

--- packages/watchmen-reactor/src/watchmen_reactor/topic_schema/encryption/center_mask.py
+++ packages/watchmen-reactor/src/watchmen_reactor/topic_schema/encryption/center_mask.py
@@ -1,27 +1,34 @@
 from typing import Any, Union
 
 from watchmen_model.admin import FactorEncryptMethod
+from watchmen_reactor.common import ReactorException
 from .encryptor import Encryptor
 
 
-# elif method == FactorEncryptMethod.MASK_CENTER_5:
 # elif method == FactorEncryptMethod.MASK_LAST_3:
 # elif method == FactorEncryptMethod.MASK_LAST_6:
 # elif method == FactorEncryptMethod.MASK_DAY:
 # elif method == FactorEncryptMethod.MASK_MONTH:
 # elif method == FactorEncryptMethod.MASK_MONTH_DAY:
 
-class Center3Masker(Encryptor):
+class CenterMasker(Encryptor):
+	def __init__(self, digits: int):
+		self.digits = digits
+		if digits == 3:
+			self.method = FactorEncryptMethod.MASK_CENTER_3
+		elif digits == 5:
+			self.method = FactorEncryptMethod.MASK_CENTER_5
+		else:
+			raise ReactorException(f'Only 3 or 5 digits center mask is supported, current is [{digits}].')
+
 	def accept(self, method: Union[FactorEncryptMethod, str]) -> bool:
-		return method == FactorEncryptMethod.MASK_CENTER_3
+		return method == self.method
 
 	def is_encrypted(self, value: Any) -> bool:
-		if value is None:
-			return False
-		elif isinstance(value, str) and value.startswith('*****@'):
-			return True
-		else:
-			return False
+		"""
+		always returns false
+		"""
+		return False
 
 	def do_encrypt(self, value: Any) -> Any:
 		value = str(value)
@@ -32,5 +39,5 @@ class Center3Masker(Encryptor):
 			return value
 
 	def do_decrypt(self, value: str) -> str:
-		# center 3 mask cannot be decrypted
+		# center mask cannot be decrypted
 		return value
