8dff1631a3dbf4eecdc9fe51347ae162773182ae
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-24 21:55:26 +0800

    initialize

--- packages/watchmen-storage-mysql/src/watchmen_storage_mysql/where_build.py
+++ packages/watchmen-storage-mysql/src/watchmen_storage_mysql/where_build.py
@@ -1,33 +1,100 @@
-from sqlalchemy import and_, or_, Table
+from typing import Tuple
 
-from watchmen_storage import ColumnNameLiteral, ComputedLiteral, EntityCriteria, EntityCriteriaExpression, \
-	EntityCriteriaJoint, EntityCriteriaJointConjunction, EntityCriteriaOperator, EntityCriteriaStatement, Literal, \
-	NoCriteriaForUpdateException, UnsupportedCriteriaException
-from watchmen_utilities import ArrayHelper
+from sqlalchemy import and_, case, func, or_, Table
+
+from watchmen_storage import ColumnNameLiteral, ComputedLiteral, ComputedLiteralOperator, EntityCriteria, \
+	EntityCriteriaExpression, EntityCriteriaJoint, EntityCriteriaJointConjunction, EntityCriteriaOperator, \
+	EntityCriteriaStatement, Literal, NoCriteriaForUpdateException, UnsupportedComputationException, \
+	UnsupportedCriteriaException
+from watchmen_utilities import ArrayHelper, DateTimeConstants
 from .types import SQLAlchemyStatement
 
 
-def build_literal(literal: Literal, table: Table):
+def build_literal(table: Table, literal: Literal):
 	if isinstance(literal, ColumnNameLiteral):
 		return table.c[literal.columnName]
 	elif isinstance(literal, ComputedLiteral):
-		# TODO build compute literal
-		raise
+		operator = literal.operator
+		if operator == ComputedLiteralOperator.ADD:
+			return ArrayHelper(literal.elements) \
+				.map(lambda x: build_literal(table, x)) \
+				.reduce(lambda prev, current: prev + current, None)
+		elif operator == ComputedLiteralOperator.SUBTRACT:
+			return ArrayHelper(literal.elements) \
+				.map(lambda x: build_literal(table, x)) \
+				.reduce(lambda prev, current: prev - current, None)
+		elif operator == ComputedLiteralOperator.MULTIPLY:
+			return ArrayHelper(literal.elements) \
+				.map(lambda x: build_literal(table, x)) \
+				.reduce(lambda prev, current: prev * current, None)
+		elif operator == ComputedLiteralOperator.DIVIDE:
+			return ArrayHelper(literal.elements) \
+				.map(lambda x: build_literal(table, x)) \
+				.reduce(lambda prev, current: prev / current, None)
+		elif operator == ComputedLiteralOperator.MODULUS:
+			return ArrayHelper(literal.elements) \
+				.map(lambda x: build_literal(table, x)) \
+				.reduce(lambda prev, current: prev % current, None)
+		elif operator == ComputedLiteralOperator.YEAR_OF:
+			return func.year(build_literal(table, literal.elements[0]))
+		elif operator == ComputedLiteralOperator.HALF_YEAR_OF:
+			return case(
+				(func.month(build_literal(table, literal.elements[0])) <= 6, DateTimeConstants.HALF_YEAR_FIRST.value),
+				_else=DateTimeConstants.HALF_YEAR_SECOND.value
+			)
+		elif operator == ComputedLiteralOperator.QUARTER_OF:
+			return func.quarter(build_literal(table, literal.elements[0]))
+		elif operator == ComputedLiteralOperator.MONTH_OF:
+			return func.month(build_literal(table, literal.elements[0]))
+		elif operator == ComputedLiteralOperator.WEEK_OF_YEAR:
+			return func.week(build_literal(table, literal.elements[0]), 0)
+		elif operator == ComputedLiteralOperator.WEEK_OF_MONTH:
+			# weekofmonth is a customized function, which can be found in meta-scripts folder
+			# make sure each topic storage have this function
+			return func.weekofmonth(build_literal(table, literal.elements[0]))
+		elif operator == ComputedLiteralOperator.DAY_OF_MONTH:
+			return func.day(build_literal(table, literal.elements[0]))
+		elif operator == ComputedLiteralOperator.DAY_OF_WEEK:
+			# weekday in mysql is 0: Monday - 6: Sunday, here need 1: Sunday - 7: Saturday
+			return (func.weekday(build_literal(table, literal.elements[0])) + 1) % 7 + 1
+		elif operator == ComputedLiteralOperator.CASE_THEN:
+			elements = literal.elements
+			cases = ArrayHelper(elements).filter(lambda x: isinstance(x, Tuple)) \
+				.map(lambda x: (build_criteria_statement(table, x[0]), build_literal(table, x[1]))) \
+				.to_list()
+			anyway = ArrayHelper(elements).find(lambda x: isinstance(x, Literal))
+			if anyway is None:
+				return case(*cases)
+			else:
+				return case(*cases, build_literal(table, anyway))
+		elif operator == ComputedLiteralOperator.CONCAT:
+			return func.concat(*ArrayHelper(literal.elements).map(lambda x: build_literal(table, x)).to_list())
+		elif operator == ComputedLiteralOperator.YEAR_DIFF:
+			# yeardiff is a customized function, which can be found in meta-scripts folder
+			# make sure each topic storage have this function
+			return func.yeardiff(build_literal(table, literal.elements[0]), build_literal(table, literal.elements[1]))
+		elif operator == ComputedLiteralOperator.MONTH_DIFF:
+			# monthdiff is a customized function, which can be found in meta-scripts folder
+			# make sure each topic storage have this function
+			return func.monthdiff(build_literal(table, literal.elements[0]), build_literal(table, literal.elements[1]))
+		elif operator == ComputedLiteralOperator.DAY_DIFF:
+			return func.datediff(build_literal(table, literal.elements[0]), build_literal(table, literal.elements[1]))
+		else:
+			raise UnsupportedComputationException(f'Unsupported computation operator[{operator}].')
 	else:
 		# a value, return itself
 		return literal
 
 
 def build_criteria_expression(table: Table, expression: EntityCriteriaExpression):
-	# TODO current supports (name op value), others need to be supported
-	built_left = build_literal(expression.left, table)
+	built_left = build_literal(table, expression.left)
 	op = expression.operator
 	if op == EntityCriteriaOperator.IS_EMPTY:
 		return or_(built_left.is_(None), built_left == '')
 	elif op == EntityCriteriaOperator.IS_NOT_EMPTY:
 		return and_(built_left.is_not(None), built_left != '')
 
-	built_right = build_literal(expression.right, table)
+	built_right = build_literal(table, expression.right)
 	if op == EntityCriteriaOperator.EQUALS:
 		return built_left == built_right
 	elif op == EntityCriteriaOperator.NOT_EQUALS:
