ba6b83b30abaddda901a24f36556c3a33fce2b3f
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-24 11:03:03 +0800

    initialize

--- packages/watchmen-reactor/test/watchmen_reactor_test/test_ask_from_storage.py
+++ packages/watchmen-reactor/test/watchmen_reactor_test/test_ask_from_storage.py
@@ -2,8 +2,11 @@ from unittest import TestCase
 
 from watchmen_auth import PrincipalService
 from watchmen_model.admin import User, UserRole
-from watchmen_model.common import ParameterJoint, ParameterJointType
-from watchmen_reactor.pipeline.runtime import parse_condition_for_storage
+from watchmen_model.common import ComputedParameter, ConstantParameter, ParameterComputeType, ParameterExpression, \
+	ParameterExpressionOperator, ParameterJoint, \
+	ParameterJointType
+from watchmen_reactor.pipeline.runtime import parse_condition_for_storage, PipelineVariables
+from watchmen_reactor.pipeline.runtime.ask_from_storage import ParsedStorageJoint
 
 
 def create_fake_principal_service() -> PrincipalService:
@@ -17,4 +20,31 @@ class AskFromStorage(TestCase):
 		parsed_joint = parse_condition_for_storage(joint, [], create_fake_principal_service(), True)
 		print(parsed_joint)
 
+	# noinspection PyMethodMayBeStatic
+	def test_no_storage_filter(self):
+		joint = ParameterJoint(
+			jointType=ParameterJointType.AND,
+			filters=[
+				ParameterExpression(left=ConstantParameter(value='abc'), operator=ParameterExpressionOperator.EMPTY),
+				ParameterExpression(
+					left=ConstantParameter(value='2022/01'), operator=ParameterExpressionOperator.NOT_EMPTY),
+				ParameterExpression(
+					left=ConstantParameter(value='2'), operator=ParameterExpressionOperator.EQUALS,
+					right=ComputedParameter(
+						type=ParameterComputeType.ADD,
+						parameters=[
+							ConstantParameter(value='0'),
+							ConstantParameter(value='2')
+						]
+					)
+				)
+			]
+		)
 
+		principal_service = create_fake_principal_service()
+		# noinspection PyTypeChecker
+		parsed_joint: ParsedStorageJoint = parse_condition_for_storage(joint, [], principal_service, True)
+		print(parsed_joint)
+		variables = PipelineVariables(None, {})
+		result = parsed_joint.run(variables, principal_service)
+		print(result.to_dict())
