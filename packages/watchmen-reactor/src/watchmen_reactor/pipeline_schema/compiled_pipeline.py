2e49355cde7add0b21ef642c5cfe404ff3dd6e64
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-25 15:35:30 +0800

    initialize

--- packages/watchmen-reactor/src/watchmen_reactor/pipeline_schema/compiled_pipeline.py
+++ packages/watchmen-reactor/src/watchmen_reactor/pipeline_schema/compiled_pipeline.py
@@ -12,12 +12,12 @@ from watchmen_data_kernel.storage_bridge import now, parse_prerequisite_defined_
 	PipelineVariables, spent_ms
 from watchmen_data_kernel.topic_schema import TopicSchema
 from watchmen_meta.common import ask_snowflake_generator
-from watchmen_model.admin import Pipeline
+from watchmen_model.admin import Pipeline, PipelineTriggerType
 from watchmen_model.reactor import MonitorLogStatus, PipelineMonitorLog, PipelineTriggerTraceId
+from watchmen_reactor.common import ReactorException
 from watchmen_reactor.pipeline_schema_interface import CompiledPipeline, PipelineContext, TopicStorages
 from watchmen_utilities import ArrayHelper
 from .compiled_stage import compile_stages, CompiledStage
-from .queued_pipeline_contexts import QueuedPipelineContexts
 
 logger = getLogger(__name__)
 
@@ -121,3 +121,54 @@ class RuntimeCompiledPipeline(CompiledPipeline):
 			return stage.run(
 				variables=variables, new_pipeline=new_pipeline, monitor_log=monitor_log,
 				storages=storages, principal_service=principal_service)
+
+
+class QueuedPipelineContexts:
+	contexts: List[PipelineContext] = []
+
+	# noinspection PyMethodMayBeStatic,DuplicatedCode
+	def should_run(self, trigger_type: PipelineTriggerType, pipeline: Pipeline) -> bool:
+		if not pipeline.enabled:
+			return False
+
+		if trigger_type == PipelineTriggerType.DELETE:
+			return pipeline.type == PipelineTriggerType.DELETE
+		elif trigger_type == PipelineTriggerType.INSERT:
+			return pipeline.type == PipelineTriggerType.INSERT or pipeline.type == PipelineTriggerType.INSERT_OR_MERGE
+		elif trigger_type == PipelineTriggerType.MERGE:
+			return pipeline.type == PipelineTriggerType.MERGE or pipeline.type == PipelineTriggerType.INSERT_OR_MERGE
+		elif trigger_type == PipelineTriggerType.INSERT_OR_MERGE:
+			return pipeline.type == PipelineTriggerType.INSERT_OR_MERGE
+		else:
+			raise ReactorException(f'Pipeline trigger type[{trigger_type}] is not supported.')
+
+	def append(
+			self,
+			schema: TopicSchema, trigger: TopicTrigger, trace_id: PipelineTriggerTraceId,
+			principal_service: PrincipalService
+	) -> List[PipelineContext]:
+		topic = schema.get_topic()
+		pipelines = get_pipeline_service(principal_service).find_by_topic_id(topic.topicId)
+		if len(pipelines) == 0:
+			logger.warning(f'No pipeline needs to be triggered by topic[id={topic.topicId}, name={topic.name}].')
+			return []
+
+		pipelines = ArrayHelper(pipelines) \
+			.filter(lambda x: self.should_run(trigger.triggerType, x)).to_list()
+		if len(pipelines) == 0:
+			logger.warning(f'No pipeline needs to be triggered by topic[id={topic.topicId}, name={topic.name}].')
+			return []
+
+		# to avoid the loop dependency
+		from .pipeline_context import RuntimePipelineContext
+		return ArrayHelper(pipelines).map(lambda x: RuntimePipelineContext(
+			pipeline=x,
+			trigger_topic_schema=schema,
+			previous_data=trigger.previous,
+			current_data=trigger.current,
+			principal_service=principal_service,
+			trace_id=trace_id
+		)).each(lambda x: self.contexts.append(x)).to_list()
+
+	def to_list(self):
+		return self.contexts
