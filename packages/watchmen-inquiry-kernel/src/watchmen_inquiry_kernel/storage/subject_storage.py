f25f1cbbf097152e5db33cf49f9799e4640073a2
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-01 00:44:46 +0800

    initialize

--- packages/watchmen-inquiry-kernel/src/watchmen_inquiry_kernel/storage/subject_storage.py
+++ packages/watchmen-inquiry-kernel/src/watchmen_inquiry_kernel/storage/subject_storage.py
@@ -1,8 +1,16 @@
 from watchmen_auth import PrincipalService
 from watchmen_data_kernel.service import ask_topic_storage
+from watchmen_data_kernel.storage_bridge import parse_condition_for_storage, parse_parameter_for_storage, \
+	PipelineVariables
 from watchmen_inquiry_kernel.common.settings import ask_use_storage_directly
 from watchmen_inquiry_kernel.subject_schema import SubjectSchema
 from watchmen_model.common import DataPage, Pageable
+from watchmen_storage import FreePager
+from watchmen_utilities import ArrayHelper
+
+
+def ask_empty_variables() -> PipelineVariables:
+	return PipelineVariables(None, None)
 
 
 class SubjectStorage:
@@ -12,8 +20,23 @@ class SubjectStorage:
 
 	def page_by_storage_directly(self, pageable: Pageable) -> DataPage:
 		storage = ask_topic_storage(self.schema.get_primary_topic_schema(), self.principalService)
-		# TODO build pager
-		return storage.free_page()
+		# build pager
+		subject = self.schema.get_subject()
+		dataset = subject.dataset
+		available_schemas = self.schema.get_available_schemas()
+
+		columns = ArrayHelper(dataset.columns) \
+			.map(lambda x: parse_parameter_for_storage(x, available_schemas, self.principalService, False)) \
+			.to_list()
+		criteria = parse_condition_for_storage(dataset.filters, available_schemas, self.principalService, False)
+
+		return storage.free_page(FreePager(
+			columns=columns,
+			# TODO build joins
+			joins=[],
+			criteria=[criteria.run(ask_empty_variables(), self.principalService)],
+			pageable=pageable
+		))
 
 	def page(self, pageable: Pageable) -> DataPage:
 		if self.schema.from_one_data_source() and ask_use_storage_directly():
