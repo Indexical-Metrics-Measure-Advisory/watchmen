98fe1ffc8ded9b5e0a1bd6fbf5d5322efba68da5
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-02-28 11:56:16 +0800

    initialize

--- packages/watchmen-data-surface/src/watchmen_data_surface/main.py
+++ packages/watchmen-data-surface/src/watchmen_data_surface/main.py
@@ -3,9 +3,11 @@ from typing import List
 from fastapi import APIRouter
 
 from .cache import cache_router
+from .data import topic_data_router
 
 
 def get_data_surface_routers() -> List[APIRouter]:
 	return [
-		cache_router.router
+		cache_router.router,
+		topic_data_router.router
 	]
