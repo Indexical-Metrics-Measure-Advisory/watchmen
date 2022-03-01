a274d594f43dde4645c53d322f4fc0fe8ed2b542
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-01 19:48:29 +0800

    initialize

--- packages/watchmen-auth/src/watchmen_auth/__init__.py
+++ packages/watchmen-auth/src/watchmen_auth/__init__.py
@@ -1,4 +1,4 @@
 from .auth_helper import authorize, authorize_token
-from .authentication import AuthenticationDetails, AuthenticationManager, AuthenticationProvider, AuthenticationType
+from .authentication import AuthenticationDetails, AuthenticationManager, AuthenticationProvider, AuthenticationScheme
 from .authorization import AuthFailOn401, AuthFailOn403, Authorization
 from .principal_service import PrincipalService
