5592345dea5ec32b4a456eeec8d1beaf63ef2bfe
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-03 23:49:52 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/common/data_result_set.py
+++ packages/watchmen-model/src/watchmen_model/common/data_result_set.py
@@ -1,5 +1,12 @@
 from typing import List, Union
 
+from pydantic import BaseModel
+
 DataResultSetCell = Union[str, int, float, bool, None]
 DataResultSetRow = List[DataResultSetCell]
 DataResultSet = List[DataResultSetRow]
+
+
+class DataResult(BaseModel):
+	columns: List[str]
+	data: DataResultSet
