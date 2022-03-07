a3a6cf0780630b40e90c5c453f17d09c8fc8fb51
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-07 16:17:25 +0800

    initialize

---
+++ packages/watchmen-model/src/watchmen_model/dqc/topic_monitor_rule_pipelines.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from watchmen_model.admin import Pipeline, Topic
+
+
+def ask_topic_monitor_rule_pipelines(topics: List[Topic]) -> List[Pipeline]:
+	# TODO define all topic monitor rule pipelines
+	return []
