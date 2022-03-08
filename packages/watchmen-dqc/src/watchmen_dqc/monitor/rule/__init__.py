f20b2b4391ad20ef9d7beba63de4390ed8fa5f14
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-08 12:25:06 +0800

    initialize

--- packages/watchmen-dqc/src/watchmen_dqc/monitor/rule/__init__.py
+++ packages/watchmen-dqc/src/watchmen_dqc/monitor/rule/__init__.py
@@ -1,4 +1,5 @@
 from .date_range import compute_date_range
+from .disabled_rules import disabled_rules
 from .rows_count_mismatch_with_another import rows_count_mismatch_with_another
 from .rows_not_exists import rows_not_exists
 from .rules_controller import run_all_rules
