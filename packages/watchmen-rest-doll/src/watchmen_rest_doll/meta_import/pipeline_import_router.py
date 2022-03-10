3c35d95bebd575ea27f1aad2f483b4d310546ede
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-10 17:56:56 +0800

    initialize

--- packages/watchmen-rest-doll/src/watchmen_rest_doll/meta_import/pipeline_import_router.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/meta_import/pipeline_import_router.py
@@ -6,7 +6,7 @@ from starlette.responses import Response
 from watchmen_auth import PrincipalService
 from watchmen_meta.admin import PipelineService
 from watchmen_meta.common import ask_meta_storage, ask_snowflake_generator
-from watchmen_model.admin import Pipeline, UserRole
+from watchmen_model.admin import Pipeline, PipelineActionType, UserRole, WriteToExternalAction
 from watchmen_rest import get_any_admin_principal
 from watchmen_rest_doll.admin import ask_save_pipeline_action
 from watchmen_rest_doll.util import trans
@@ -32,6 +32,13 @@ async def import_dashboards(
 
 	def action() -> None:
 		validate_tenant_based_tuples(pipelines, get_user_service(pipeline_service), principal_service)
+		for pipeline in pipelines:
+			for stage in ArrayHelper(pipeline.stages).to_list():
+				for unit in ArrayHelper(stage.units).to_list():
+					for an_action in ArrayHelper(unit.action).to_list():
+						if an_action.type == PipelineActionType.WRITE_TO_EXTERNAL:
+							if isinstance(an_action, WriteToExternalAction):
+								an_action.externalWriterId = None
 		save = ask_save_pipeline_action(pipeline_service, principal_service)
 		# noinspection PyTypeChecker
 		ArrayHelper(pipelines).each(lambda x: save(x))
