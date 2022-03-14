03925465fe11e11b7068ae60c1c472be7043cfa3
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-14 14:56:14 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/admin/enumeration.py
+++ packages/watchmen-model/src/watchmen_model/admin/enumeration.py
@@ -1,8 +1,9 @@
-from typing import List
+from typing import Dict, List, Optional, Union
 
 from pydantic import BaseModel
 
 from watchmen_model.common import EnumId, EnumItemId, OptimisticLock, Storable, TenantBasedTuple, TenantId
+from watchmen_utilities import ArrayHelper
 
 
 class EnumItem(Storable, BaseModel):
@@ -15,9 +16,31 @@ class EnumItem(Storable, BaseModel):
 	tenantId: TenantId = None
 
 
+def construct_item(item: Union[EnumItem, Dict]) -> Optional[EnumItem]:
+	if item is None:
+		return None
+	elif isinstance(item, EnumItem):
+		return item
+	else:
+		return EnumItem(**item)
+
+
+def construct_items(items: Optional[List[Union[EnumItem, Dict]]]) -> Optional[List[EnumItem]]:
+	if items is None:
+		return None
+	else:
+		return ArrayHelper(items).map(lambda x: construct_item(x)).to_list()
+
+
 class Enum(TenantBasedTuple, OptimisticLock, BaseModel):
 	enumId: EnumId = None
 	name: str = None
 	description: str = None
 	parentEnumId: EnumId = None
 	items: List[EnumItem] = []
+
+	def __setattr__(self, name, value):
+		if name == 'items':
+			super().__setattr__(name, construct_items(value))
+		else:
+			super().__setattr__(name, value)
