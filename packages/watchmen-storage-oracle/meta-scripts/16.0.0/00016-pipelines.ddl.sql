8822988180a8104761ed3fe4bacfaa21d845f940
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-15 23:30:44 +0800

    initialize

--- packages/watchmen-storage-oracle/meta-scripts/16.0.0/00016-pipelines.ddl.sql
+++ packages/watchmen-storage-oracle/meta-scripts/16.0.0/00016-pipelines.ddl.sql
@@ -1,18 +1,20 @@
 CREATE TABLE pipelines
 (
-    pipeline_id      VARCHAR2(50) NOT NULL,
-    topic_id         VARCHAR2(50) NOT NULL,
-    name             VARCHAR2(50) NOT NULL,
-    type             VARCHAR2(20) NOT NULL,
-    stages           CLOB,
-    enabled          NUMBER(1),
-    validated        NUMBER(1),
-    tenant_id        VARCHAR2(50) NOT NULL,
-    created_at       DATE         NOT NULL,
-    created_by       VARCHAR2(50) NOT NULL,
-    last_modified_at DATE         NOT NULL,
-    last_modified_by VARCHAR2(50) NOT NULL,
-    version          NUMBER(20),
+    pipeline_id          VARCHAR2(50) NOT NULL,
+    topic_id             VARCHAR2(50) NOT NULL,
+    name                 VARCHAR2(50) NOT NULL,
+    type                 VARCHAR2(20) NOT NULL,
+    prerequisite_enabled NUMBER(1),
+    prerequisite_on      CLOB,
+    stages               CLOB,
+    enabled              NUMBER(1),
+    validated            NUMBER(1),
+    tenant_id            VARCHAR2(50) NOT NULL,
+    created_at           DATE         NOT NULL,
+    created_by           VARCHAR2(50) NOT NULL,
+    last_modified_at     DATE         NOT NULL,
+    last_modified_by     VARCHAR2(50) NOT NULL,
+    version              NUMBER(20),
     CONSTRAINT pk_pipelines PRIMARY KEY (pipeline_id)
 );
 CREATE INDEX i_pipelines_1 ON pipelines (name);
