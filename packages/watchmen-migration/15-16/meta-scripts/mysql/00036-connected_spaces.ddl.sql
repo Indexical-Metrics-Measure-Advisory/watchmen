9c47f1022abd63c0ef4945e907717dae8f775095
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-15 19:06:59 +0800

    initialize

--- packages/watchmen-migration/15-16/meta-scripts/mysql/00036-connected_spaces.ddl.sql
+++ packages/watchmen-migration/15-16/meta-scripts/mysql/00036-connected_spaces.ddl.sql
@@ -8,7 +8,7 @@ ALTER TABLE connected_spaces DROP subjects;
 ALTER TABLE connected_spaces DROP groupids;
 ALTER TABLE connected_spaces CHANGE userid user_id VARCHAR(50) NOT NULL;
 ALTER TABLE connected_spaces CHANGE tenantid tenant_id VARCHAR(50) NOT NULL;
-ALTER TABLE connected_spaces ADD last_visit_time DATETIME NOT NULL;
+ALTER TABLE connected_spaces CHANGE lastvisittime last_visit_time DATETIME NOT NULL;
 ALTER TABLE connected_spaces DROP createtime;
 ALTER TABLE connected_spaces DROP lastmodified;
 ALTER TABLE connected_spaces ADD created_at DATETIME NOT NULL;
