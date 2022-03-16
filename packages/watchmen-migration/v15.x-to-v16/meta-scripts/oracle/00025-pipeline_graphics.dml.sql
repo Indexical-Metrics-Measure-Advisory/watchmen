1da542571dc6499e2f1f93db33975d910ca80b31
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-16 14:00:08 +0800

    initialize

--- packages/watchmen-migration/v15.x-to-v16/meta-scripts/oracle/00025-pipeline_graphics.dml.sql
+++
@@ -1,1 +0,0 @@
-UPDATE pipeline_graphics SET created_at = SYSDATE, last_modified_at = SYSDATE;
--- packages/watchmen-migration/v15.x-to-v16/meta-scripts/oracle/00026-snowflake_competitive_workers.ddl.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE snowflake_workerid RENAME TO snowflake_competitive_workers;
-ALTER TABLE snowflake_competitive_workers DROP CONSTRAINT snowflake_workerid_pk;
