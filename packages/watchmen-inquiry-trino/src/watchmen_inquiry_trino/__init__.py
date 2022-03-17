208aae8d79832e7fe20436f153dd5ff8c709c9bb
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-17 14:50:29 +0800

    initialize

---
+++ packages/watchmen-inquiry-trino/src/watchmen_inquiry_trino/__init__.py
@@ -0,0 +1,3 @@
+from .exception import InquiryTrinoException
+from .trino_storage_helper import ask_trino_topic_storage
+from .trino_storage_spi import TrinoStorageSPI
---
+++ packages/watchmen-inquiry-trino/src/watchmen_inquiry_trino/exception.py
@@ -0,0 +1,2 @@
+class InquiryTrinoException(Exception):
+	pass
