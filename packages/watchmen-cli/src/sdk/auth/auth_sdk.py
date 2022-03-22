007e0cca973c497e8fdb07117bac5747335a26f7
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-22 15:53:28 +0800

    initialize

--- packages/watchmen-cli/src/sdk/auth/auth_sdk.py
+++ packages/watchmen-cli/src/sdk/auth/auth_sdk.py
@@ -2,9 +2,8 @@ import requests
 
 
 def login(site):
-    login_data = {"username": site["username"], "password": site["password"], "grant_type": "password"}
-    headers = {"Content-Type": "application/x-www-form-urlencoded"}
-    response = requests.post(site["host"] + "login/access-token", data=login_data,
-                             headers=headers)
-    auth_token = response.json()["access_token"]
-    return auth_token
+	login_data = {"username": site["username"], "password": site["password"], "grant_type": "password"}
+	headers = {"Content-Type": "application/x-www-form-urlencoded"}
+	response = requests.post(site["host"] + "login/access-token", data=login_data, headers=headers)
+	auth_token = response.json()["access_token"]
+	return auth_token
