964bb0e80d564e3c3f055f80fdbc2f6afaa81da1
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-27 22:00:42 +0800

    initialize

--- packages/watchmen-storage/src/watchmen_storage/types_detect.py
+++ packages/watchmen-storage/src/watchmen_storage/types_detect.py
@@ -11,15 +11,34 @@ class DetectedType(str, Enum):
 	DATE = 'date',
 	DATETIME = 'datetime',
 	TIME = 'time',
-	ANY = 'any'
+	ANY = 'any',
+	UNCONCERNED = 'unconcerned'
 
 
-def ask_possible_type_types(expression: EntityCriteriaExpression) -> List[DetectedType]:
+# noinspection DuplicatedCode
+def ask_possible_left_types(expression: EntityCriteriaExpression) -> List[DetectedType]:
 	operator = expression.operator
 	if operator == EntityCriteriaOperator.IS_EMPTY or operator == EntityCriteriaOperator.IS_NOT_EMPTY:
 		return [DetectedType.ANY]
 	elif operator == EntityCriteriaOperator.IS_BLANK or operator == EntityCriteriaOperator.IS_NOT_BLANK:
 		return [DetectedType.STRING]
+	elif operator == EntityCriteriaOperator.LESS_THAN or operator == EntityCriteriaOperator.LESS_THAN_OR_EQUALS:
+		return [DetectedType.NUMBER, DetectedType.DATE, DetectedType.TIME, DetectedType.DATETIME]
+	elif operator == EntityCriteriaOperator.GREATER_THAN or operator == EntityCriteriaOperator.GREATER_THAN_OR_EQUALS:
+		return [DetectedType.NUMBER, DetectedType.DATE, DetectedType.TIME, DetectedType.DATETIME]
+	elif operator == EntityCriteriaOperator.LIKE or operator == EntityCriteriaOperator.NOT_LIKE:
+		return [DetectedType.STRING]
+	else:
+		return [DetectedType.ANY]
+
+
+# noinspection DuplicatedCode
+def ask_possible_right_types(expression: EntityCriteriaExpression) -> List[DetectedType]:
+	operator = expression.operator
+	if operator == EntityCriteriaOperator.IS_EMPTY or operator == EntityCriteriaOperator.IS_NOT_EMPTY:
+		return [DetectedType.UNCONCERNED]
+	elif operator == EntityCriteriaOperator.IS_BLANK or operator == EntityCriteriaOperator.IS_NOT_BLANK:
+		return [DetectedType.UNCONCERNED]
 	elif operator == EntityCriteriaOperator.LESS_THAN or operator == EntityCriteriaOperator.LESS_THAN_OR_EQUALS:
 		return [DetectedType.NUMBER, DetectedType.DATE, DetectedType.TIME, DetectedType.DATETIME]
 	elif operator == EntityCriteriaOperator.GREATER_THAN or operator == EntityCriteriaOperator.GREATER_THAN_OR_EQUALS:
