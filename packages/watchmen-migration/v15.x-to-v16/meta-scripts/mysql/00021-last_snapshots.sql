12cabf62842952fc18fc6c1b40d29b5f67d4d1a5
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-29 16:42:28 +0800

    initialize

--- packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00021-last_snapshots.sql
+++ packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00021-last_snapshots.sql
@@ -8,7 +8,7 @@ ALTER TABLE last_snapshots CHANGE userid user_id VARCHAR(50) NOT NULL;
 ALTER TABLE last_snapshots CHANGE tenantid tenant_id VARCHAR(50) NOT NULL;
 ALTER TABLE last_snapshots DROP createtime;
 ALTER TABLE last_snapshots DROP lastmodified;
-ALTER TABLE last_snapshots ADD last_visit_time DATETIME NOT NULL;
+ALTER TABLE last_snapshots ADD last_visit_time DATETIME DEFAULT NOW() NOT NULL;
 CREATE INDEX tenant_id ON last_snapshots (tenant_id);
 -- noinspection SqlWithoutWhere
 UPDATE last_snapshots SET last_visit_time = NOW();
