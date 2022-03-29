93788cf3050278b44a576b09b9ac1f46ac31814b
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-29 16:42:28 +0800

    initialize

--- packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00020-favorites.sql
+++ packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00020-favorites.sql
@@ -6,7 +6,7 @@ ALTER TABLE favorites CHANGE userid user_id VARCHAR(50) NOT NULL;
 ALTER TABLE favorites CHANGE tenantid tenant_id VARCHAR(50) NOT NULL;
 ALTER TABLE favorites DROP createtime;
 ALTER TABLE favorites DROP lastmodified;
-ALTER TABLE favorites ADD last_visit_time DATETIME NOT NULL;
+ALTER TABLE favorites ADD last_visit_time DATETIME DEFAULT NOW() NOT NULL;
 CREATE INDEX tenant_id ON favorites (tenant_id);
 -- noinspection SqlWithoutWhere
 UPDATE favorites SET last_visit_time = NOW();
