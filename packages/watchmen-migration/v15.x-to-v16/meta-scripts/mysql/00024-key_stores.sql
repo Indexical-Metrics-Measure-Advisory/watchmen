037e72a0d573510f7b40a27b2a63170ae1eff2bb
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-29 16:40:05 +0800

    initialize

--- packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00024-key_stores.sql
+++ packages/watchmen-migration/v15.x-to-v16/meta-scripts/mysql/00024-key_stores.sql
@@ -7,6 +7,6 @@ ALTER TABLE key_stores CHANGE keyType key_type VARCHAR(20) NOT NULL;
 ALTER TABLE key_stores MODIFY params JSON NOT NULL;
 ALTER TABLE key_stores DROP createtime;
 ALTER TABLE key_stores DROP lastmodified;
-ALTER TABLE key_stores ADD created_at DATETIME NOT NULL;
-ALTER TABLE key_stores ADD created_by VARCHAR(50) NOT NULL;
+ALTER TABLE key_stores ADD created_at DATETIME DEFAULT NOW() NOT NULL;
+ALTER TABLE key_stores ADD created_by VARCHAR(50) DEFAULT '-1' NOT NULL;
 ALTER TABLE key_stores ADD PRIMARY KEY (tenant_id, key_type);
