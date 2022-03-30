7173dcadfe9e835141293d9ea14371e996454b09
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-03-30 20:09:47 +0800

    initialize

--- packages/watchmen-storage-mysql/meta-scripts/16.0.0/00014-enum_items.ddl.sql
+++ packages/watchmen-storage-mysql/meta-scripts/16.0.0/00014-enum_items.ddl.sql
@@ -2,7 +2,7 @@ CREATE TABLE enum_items
 (
     item_id      VARCHAR(50) NOT NULL,
     code         VARCHAR(50) NOT NULL,
-    label        VARCHAR(100),
+    label        VARCHAR(255),
     parent_code  VARCHAR(50),
     replace_code VARCHAR(50),
     enum_id      VARCHAR(50),
