67abcff57459f51c7adce5585b1a97bad28d7982
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-01 17:06:00 +0800

    initialize

--- packages/watchmen-model/src/watchmen_model/chart/chart_settings.py
+++ packages/watchmen-model/src/watchmen_model/chart/chart_settings.py
@@ -1,6 +1,6 @@
 from pydantic import BaseModel
 
-from .chart_basic_structure import ChartTruncationHolder
+from .chart_basic_structure import ChartTruncation, ChartTruncationHolder
 from .chart_basic_style import ChartBorder
 from .chart_enums import PredefinedChartColorSeries
 from .chart_types import ChartColor
@@ -11,3 +11,9 @@ class ChartSettings(ChartTruncationHolder, BaseModel):
 	backgroundColor: ChartColor = None
 
 	colorSeries: PredefinedChartColorSeries = PredefinedChartColorSeries.REGULAR
+
+	def __setattr__(self, name, value):
+		if name == 'truncation':
+			super().__setattr__(name, ChartTruncation(**value))
+		else:
+			super().__setattr__(name, value)
