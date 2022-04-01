216de1f50574f35b8987cd66ff072d59afd84a5c
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-01 15:51:45 +0800

    initialize

--- packages/watchmen-inquiry-kernel/src/watchmen_inquiry_kernel/schema/subject_schema.py
+++ packages/watchmen-inquiry-kernel/src/watchmen_inquiry_kernel/schema/subject_schema.py
@@ -209,7 +209,7 @@ def find_topic_schemas(
 		columns: List[SubjectDatasetColumn], filters: Optional[ParameterJoint],
 		principal_service: PrincipalService) -> List[TopicSchema]:
 	schemas = find_topic_schemas_by_columns(columns, principal_service)
-	if filters.filters is not None and len(filters.filters) != 0:
+	if filters is not None and filters.filters is not None and len(filters.filters) != 0:
 		schemas = ArrayHelper(schemas).grab(*find_topic_schemas_by_filters(filters, principal_service)).to_list()
 
 	distinct_map: Dict[TopicId, TopicSchema] = {}
