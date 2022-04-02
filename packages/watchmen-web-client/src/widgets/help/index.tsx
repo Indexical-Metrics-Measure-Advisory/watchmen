f559f72fa355e8e9d48bee0e85a04d5292a9e287
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-02 17:51:54 +0800

    initialize 16

--- packages/watchmen-web-client/src/widgets/help/index.tsx
+++ packages/watchmen-web-client/src/widgets/help/index.tsx
@@ -1,20 +1,82 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import hotkeys from 'hotkeys-js';
-import {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useRef, useState} from 'react';
 import {ICON_HELP} from '../basic/constants';
+import {ButtonInk} from '../basic/types';
 import {useEventBus} from '../events/event-bus';
 import {EventTypes} from '../events/types';
-import {HelpContainer, HelpIcon, HelpLabel} from './widgets';
+import {Lang} from '../langs';
+import {
+	HelpContainer,
+	HelpDialogBody,
+	HelpDialogButtons,
+	HelpDialogCloseButton,
+	HelpDialogSearchInput,
+	HelpDialogSearchResults,
+	HelpDialogVersionLabel,
+	HelpIcon,
+	HelpLabel
+} from './widgets';
 
 // enable hotkeys in anything
 hotkeys.filter = () => {
 	return true;
 };
 
+export enum HELP_KEYS {
+	ADMIN_ENUM = 'admin/enumeration'
+}
+
 const isMacLike = /(Mac|iPhone|iPod|iPad)/i.test(navigator.userAgent);
 
+const HelpDialog = (props: { text?: string }) => {
+	const {text} = props;
+
+	const {fire} = useEventBus();
+	const inputRef = useRef<HTMLInputElement>(null);
+	const [searchText, setSearchText] = useState(text ?? '');
+	useEffect(() => {
+		if (inputRef != null && inputRef.current != null) {
+			inputRef.current.focus();
+		}
+	});
+
+	const onKeyChanged = (event: ChangeEvent<HTMLInputElement>) => {
+		const value = event.target.value;
+		if (value === searchText) {
+			return;
+		}
+
+		setSearchText(value);
+	};
+	const onCloseClicked = () => {
+		fire(EventTypes.HIDE_DIALOG);
+	};
+
+	return <HelpDialogBody>
+		<HelpDialogSearchInput value={searchText} onChange={onKeyChanged} ref={inputRef}/>
+		<HelpDialogSearchResults>
+
+		</HelpDialogSearchResults>
+		<HelpDialogButtons>
+			<HelpDialogVersionLabel>Watchmen Web Client, Ver. {process.env.REACT_APP_VERSION}</HelpDialogVersionLabel>
+			<HelpDialogCloseButton ink={ButtonInk.PRIMARY} onClick={onCloseClicked}>
+				{Lang.ACTIONS.CLOSE}
+			</HelpDialogCloseButton>
+		</HelpDialogButtons>
+	</HelpDialogBody>;
+};
+const showHelpDialog = (fire: Function, key: string) => {
+	fire(EventTypes.SHOW_DIALOG, <HelpDialog text={key}/>,
+		{
+			marginTop: '10vh',
+			marginLeft: '20%',
+			width: '60%',
+			maxHeight: '80vh'
+		});
+};
 export const HelpButton = () => {
-	const {on, off} = useEventBus();
+	const {on, off, fire} = useEventBus();
 	const [state, setState] = useState({key: '', visible: false});
 	useEffect(() => {
 		const onShowHelp = (key: string) => {
@@ -27,7 +89,7 @@ export const HelpButton = () => {
 	}, [on, off]);
 
 	const onHelpClicked = () => {
-
+		showHelpDialog(fire, state.key);
 	};
 	const onAnimationEnd = () => {
 		setState(state => {
@@ -50,7 +112,7 @@ export const useHelp = (key: string) => {
 	const {fire} = useEventBus();
 	useEffect(() => {
 		const showHelp = () => {
-			fire(EventTypes.SHOW_HELP, key);
+			showHelpDialog(fire, key);
 			return false;
 		};
 		hotkeys('⌃+k,⌘+k', showHelp);
@@ -64,7 +126,3 @@ export const useHelp = (key: string) => {
 		}, 10000);
 	});
 };
-
-export enum HELP_KEYS {
-	ADMIN_ENUM = 'admin/enumeration'
-}
