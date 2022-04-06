4aa4438458e87d780863a8a6a553ed01970d2f0a
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-06 21:59:35 +0800

    initialize

--- packages/watchmen-dqc/src/watchmen_dqc/monitor/monitor_data_service.py
+++ packages/watchmen-dqc/src/watchmen_dqc/monitor/monitor_data_service.py
@@ -72,7 +72,7 @@ class MonitorDataService:
 			parsed, end_date = is_date(criteria.endDate, ask_datetime_formats())
 			if parsed:
 				if isinstance(end_date, datetime):
-					end_date = end_date.replace(microsecond=0, tzinfo=None)
+					end_date = end_date.replace(hour=23, minute=59, second=59, microsecond=999999, tzinfo=None)
 				else:
 					end_date = datetime(
 						year=end_date.year, month=end_date.month, day=end_date.day,
