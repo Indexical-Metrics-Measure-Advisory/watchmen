65aa92534a2347ca561a93d51d37c75403591c60
skywalkerX69 <skywalkerX69@outlook.com>
2022-04-07 14:59:26 +0800

    package

--- .github/actions/change-version-action/entrypoint.sh
+++ .github/actions/change-version-action/entrypoint.sh
@@ -3,13 +3,11 @@
 path=$GITHUB_WORKSPACE$INPUT_FILEPATH
 
 if [ $INPUT_FILETYPE=="toml" ]; then
-  cd /
-  python change_poetry_toml.py $INPUT_VERSION $path
+  python /change_poetry_toml.py $INPUT_VERSION $path
 fi
 
 if [ $INPUT_FILETYPE=="json" ]; then
-  cd /
-  python change_package_json.py $INPUT_VERSION $path
+  python /change_package_json.py $INPUT_VERSION $path
 fi
 
 cd $path
