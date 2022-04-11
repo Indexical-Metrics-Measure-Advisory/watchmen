b2e3227fea9530adffa4ad0982a0cb4303bb8793
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-11 22:21:41 +0800

    package

--- .github/actions/compute-pre-release-branch/index.js
+++ .github/actions/compute-pre-release-branch/index.js
@@ -4,8 +4,8 @@ const fs = require('fs');
 try {
     const targetVersion = core.getInput('version');
     const [major, minor, patch] = targetVersion.split('.');
-    console.log(targetVersion);
-    console.log(major, minor, patch);
+    // console.log(targetVersion);
+    // console.log(major, minor, patch);
     core.exportVariable('PRE_RELEASE_BRANCH_NAME', `pre-release/v-${major}.0`)
 } catch (error) {
     core.setFailed(error.message);
