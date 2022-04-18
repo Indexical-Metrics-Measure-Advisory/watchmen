fe1b7d1508d1977d114f3dc8a99145a675cfb13b
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-19 00:17:22 +0800

    16.1

--- packages/watchmen-web-client/src/indicator-workbench/indicator/edit/measure-methods/index.tsx
+++ packages/watchmen-web-client/src/indicator-workbench/indicator/edit/measure-methods/index.tsx
@@ -1,108 +1,58 @@
-import {Factor} from '@/services/data/tuples/factor-types';
-import {IndicatorAggregateArithmetic, IndicatorMeasure, MeasureMethod} from '@/services/data/tuples/indicator-types';
+import {IndicatorAggregateArithmetic, MeasureMethod} from '@/services/data/tuples/indicator-types';
 import {
 	detectMeasures,
+	findTopicAndFactor,
 	isCategoryMeasure,
 	isGeoMeasure,
 	isIndividualMeasure,
 	isOrganizationMeasure,
 	isTimePeriodMeasure
 } from '@/services/data/tuples/indicator-utils';
-import {EnumForIndicator} from '@/services/data/tuples/query-indicator-types';
-import {ICON_INDICATOR_MEASURE_METHOD} from '@/widgets/basic/constants';
-import {MeasureMethodLabel} from '@/widgets/basic/measure-method-label';
 import {Lang} from '@/widgets/langs';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {Fragment, useRef} from 'react';
+import {useRef} from 'react';
 import {EmphaticSinkingLabel, Step, StepBody, StepTitle} from '../../../step-widgets';
-import {MeasureMethodSort} from '../../../utils/sort';
+import {IndicatorsData} from '../../indicators-event-bus-types';
 import {IndicatorDeclarationStep} from '../../types';
-import {MeasureFactor} from '../measure-factor';
 import {Construct, useConstructed} from '../use-constructed';
 import {useStep} from '../use-step';
-import {
-	AggregateItem,
-	AggregateItemsBlock,
-	AggregateItemsTitle,
-	MeasureFactors,
-	MeasureItemsBlock,
-	MeasureItemsContainer,
-	MeasureItemsTitle,
-	MeasureMethodItem
-} from './widgets';
+import {AggregateItems} from './aggregate-items';
+import {MeasureFactorItems} from './measure-factor-items';
+import {AvailableMeasureColumn, AvailableMeasureFactor} from './types';
+import {MeasureItemsContainer} from './widgets';
 
-interface AvailableMeasureFactor extends IndicatorMeasure {
-	factorName?: string;
-	factor?: Factor;
-}
+type MeasureFilter = (func: (measure: MeasureMethod) => boolean) => Array<AvailableMeasureFactor> | Array<AvailableMeasureColumn>;
 
-const MeasureItems = (props: { label: string; measureFactors: Array<AvailableMeasureFactor>; enums: Array<EnumForIndicator> }) => {
-	const {label, measureFactors, enums} = props;
-
-	if (measureFactors.length === 0) {
-		return null;
+const prepareMeasures = (data?: IndicatorsData): MeasureFilter => {
+	if (data == null) {
+		// noinspection JSUnusedLocalSymbols
+		return (func: (measure: MeasureMethod) => boolean) => [];
+	} else if (data.topic != null) {
+		const measures = detectMeasures(data.topic);
+		return (func: (measure: MeasureMethod) => boolean): Array<AvailableMeasureFactor> => {
+			const {factors = []} = data?.topic || {};
+			return measures.filter(({method}) => func(method))
+				.map(({factorId, method}) => {
+					// eslint-disable-next-line
+					const factor = factors.find(factor => factor.factorId == factorId);
+					return {factorId, factorName: factor?.name, factor, method};
+				});
+		};
+	} else if (data.subject != null) {
+		const measures = detectMeasures(data.subject);
+		return (func: (measure: MeasureMethod) => boolean): Array<AvailableMeasureColumn> => {
+			const {columns = []} = data.subject?.dataset || {};
+			return measures.filter(({method}) => func(method))
+				.map(({factorId, method}) => {
+					// eslint-disable-next-line
+					const column = columns.find(column => column.columnId == factorId);
+					const {factor} = findTopicAndFactor(column!, data.subject!);
+					return {factorId, columnAlias: column?.alias, column, method, enumId: factor?.enumId};
+				});
+		};
+	} else {
+		// noinspection JSUnusedLocalSymbols
+		return (func: (measure: MeasureMethod) => boolean) => [];
 	}
-
-	const legal = (amf: AvailableMeasureFactor): amf is Required<AvailableMeasureFactor> => amf.factor != null;
-	const mfs: Array<Required<AvailableMeasureFactor>> = measureFactors.filter(legal);
-	if (mfs.length === 0) {
-		return null;
-	}
-
-	const methodGroups = mfs.reduce((map, measureFactor) => {
-		const {method, factor} = measureFactor;
-		let group = map[method];
-		if (group == null) {
-			group = [];
-			map[method] = group;
-		}
-		group.push(factor);
-		return map;
-	}, {} as Record<MeasureMethod, Array<Factor>>);
-
-	return <>
-		<MeasureItemsTitle>{label}</MeasureItemsTitle>
-		<MeasureItemsBlock>
-			{Object.keys(methodGroups).sort((m1, m2) => {
-				return MeasureMethodSort[m1 as MeasureMethod] - MeasureMethodSort[m2 as MeasureMethod];
-			}).map(method => {
-				const factors = methodGroups[method as MeasureMethod];
-				return <Fragment key={method}>
-					<MeasureMethodItem>
-						<FontAwesomeIcon icon={ICON_INDICATOR_MEASURE_METHOD}/>
-						<MeasureMethodLabel measureMethod={method as MeasureMethod}/>
-					</MeasureMethodItem>
-					<MeasureFactors>
-						{factors.map(factor => {
-							// eslint-disable-next-line
-							const enumeration = enums.find(enumeration => enumeration.enumId == factor.enumId);
-							return <MeasureFactor factor={factor} enum={enumeration}
-							                      key={factor.factorId}/>;
-						})}
-					</MeasureFactors>
-				</Fragment>;
-			})}
-		</MeasureItemsBlock>
-	</>;
-};
-
-const AggregateItems = (props: { label: string; aggregates: Array<IndicatorAggregateArithmetic> }) => {
-	const {label, aggregates} = props;
-
-	if (aggregates.length === 0) {
-		return null;
-	}
-
-	return <>
-		<AggregateItemsTitle>{label}</AggregateItemsTitle>
-		<AggregateItemsBlock>
-			{aggregates.map(aggregate => {
-				return <AggregateItem key={aggregate}>
-					{aggregate.replace(/-/g, ' ')}
-				</AggregateItem>;
-			})}
-		</AggregateItemsBlock>
-	</>;
 };
 
 export const MeasureMethods = () => {
@@ -119,16 +69,7 @@ export const MeasureMethods = () => {
 		return null;
 	}
 
-	const measures = detectMeasures(data?.topic);
-	const filterMeasures = (func: (measure: MeasureMethod) => boolean): Array<AvailableMeasureFactor> => {
-		const {factors = []} = data?.topic || {};
-		return measures.filter(({method}) => func(method))
-			.map(({factorId, method}) => {
-				// eslint-disable-next-line
-				const factor = factors.find(factor => factor.factorId == factorId);
-				return {factorId, factorName: factor?.name, factor, method};
-			});
-	};
+	const filterMeasures = prepareMeasures(data);
 
 	const geoMeasures = {
 		key: 'geo',
@@ -169,8 +110,8 @@ export const MeasureMethods = () => {
 			<MeasureItemsContainer>
 				{[geoMeasures, timePeriodMeasures, individualMeasures, organizationMeasures, categoryMeasures]
 					.map(({key, label, measures}) => {
-						return <MeasureItems label={label} measureFactors={measures} enums={data?.enums || []}
-						                     key={key}/>;
+						return <MeasureFactorItems label={label} measureFactors={measures} enums={data?.enums || []}
+						                           key={key}/>;
 					})}
 				<AggregateItems label={Lang.INDICATOR_WORKBENCH.INDICATOR.AGGREGATE} aggregates={aggregates}/>
 			</MeasureItemsContainer>
