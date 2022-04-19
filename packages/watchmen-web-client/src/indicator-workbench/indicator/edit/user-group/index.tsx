ab882e42cea5c29bc70f9d613178c8794cd875bc
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-19 22:54:03 +0800

    16.1

---
+++ packages/watchmen-web-client/src/indicator-workbench/indicator/edit/user-group/index.tsx
@@ -0,0 +1,35 @@
+import {useStep} from '@/indicator-workbench/indicator/edit/use-step';
+import {useEventBus} from '@/widgets/events/event-bus';
+import {Lang} from '@/widgets/langs';
+import {useRef} from 'react';
+import {EmphaticSinkingLabel, Step, StepBody, StepTitle} from '../../../step-widgets';
+import {useIndicatorsEventBus} from '../../indicators-event-bus';
+import {IndicatorDeclarationStep} from '../../types';
+import {Construct, useConstructed} from '../use-constructed';
+
+export const UserGroupAssigner = () => {
+	const ref = useRef<HTMLDivElement>(null);
+	const {fire: fireGlobal} = useEventBus();
+	const {fire} = useIndicatorsEventBus();
+	const {constructed, setConstructed, visible, setVisible} = useConstructed(ref);
+	const {data, done} = useStep({
+		step: IndicatorDeclarationStep.USER_GROUPS,
+		active: () => setConstructed(Construct.ACTIVE),
+		done: () => setConstructed(Construct.DONE),
+		dropped: () => setVisible(false)
+	});
+
+	if (constructed === Construct.WAIT) {
+		return null;
+	}
+
+	return <Step index={IndicatorDeclarationStep.USER_GROUPS} visible={visible} ref={ref}>
+		<StepTitle visible={visible}>
+			<EmphaticSinkingLabel>
+				{Lang.INDICATOR_WORKBENCH.INDICATOR.USER_GROUP}
+			</EmphaticSinkingLabel>
+		</StepTitle>
+		<StepBody visible={visible}>
+		</StepBody>
+	</Step>;
+};
