8fa0feda70672fe38c4e7d69991bd162abe61e38
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-25 14:07:33 +0800

    16.1

--- packages/watchmen-web-client/src/services/data/data-import/import-data-types.ts
+++ packages/watchmen-web-client/src/services/data/data-import/import-data-types.ts
@@ -1,3 +1,6 @@
+import {MonitorRuleId} from '@/services/data/data-quality/rule-types';
+import {ReportId} from '@/services/data/tuples/report-types';
+import {SubjectId} from '@/services/data/tuples/subject-types';
 import {ConnectedSpaceId} from '../tuples/connected-space-types';
 import {PipelineId} from '../tuples/pipeline-types';
 import {SpaceId} from '../tuples/space-types';
@@ -5,8 +8,11 @@ import {TopicId} from '../tuples/topic-types';
 
 export interface ImportDataResponse {
 	passed: boolean;
-	topics?: Array<{ topicId: TopicId; name: string; reason: string }>;
-	pipelines?: Array<{ pipelineId: PipelineId; name: string; reason: string }>;
-	spaces?: Array<{ spaceId: SpaceId; name: string; reason: string }>;
-	connectedSpaces?: Array<{ connectId: ConnectedSpaceId; name: string; reason: string }>;
+	topics?: Array<{ topicId: TopicId; name: string; reason: string; passed: boolean }>;
+	pipelines?: Array<{ pipelineId: PipelineId; name: string; reason: string; passed: boolean }>;
+	spaces?: Array<{ spaceId: SpaceId; name: string; reason: string; passed: boolean }>;
+	connectedSpaces?: Array<{ connectId: ConnectedSpaceId; name: string; reason: string; passed: boolean }>;
+	subjects?: Array<{ subjectId: SubjectId; name: string; reason: string; passed: boolean }>;
+	reports?: Array<{ reportId: ReportId; name: string; reason: string; passed: boolean }>;
+	monitorRules?: Array<{ monitorRuleId: MonitorRuleId; name: string; reason: string; passed: boolean }>;
 }
