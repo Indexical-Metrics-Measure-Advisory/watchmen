5a94529c7d53a49f4f7318f2e7ab6ff53c6f8586
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-04-27 19:42:07 +0800

    16.1

--- packages/watchmen-web-client/src/services/data/utils.ts
+++ packages/watchmen-web-client/src/services/data/utils.ts
@@ -2,7 +2,7 @@ import dayjs, {Dayjs} from 'dayjs';
 
 export const isMockService = (): boolean => process.env.REACT_APP_SERVICE_MOCK_FLAG === 'true';
 export const getServiceHost = (): string => {
-	if (window.location.hostname === 'localhost') {
+	if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
 		return process.env.REACT_APP_SERVICE_URL!;
 	} else {
 		return window.location.protocol + '//' + window.location.host + '/watchmen/';
