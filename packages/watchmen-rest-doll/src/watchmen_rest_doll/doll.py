fa1b6402d8f3706431308d9f1275928e2eae6c8c
luke0623 <luke0623@outlook.com>
2022-04-27 10:10:47 +0800

    saml integration

--- packages/watchmen-rest-doll/src/watchmen_rest_doll/doll.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/doll.py
@@ -1,13 +1,16 @@
-from typing import Callable, Optional, Tuple
+from typing import Callable, Optional, Tuple, Dict
 
 from fastapi import FastAPI
-
+from logging import getLogger
 from watchmen_meta.auth import build_find_user_by_name, build_find_user_by_pat
 from watchmen_model.admin import User
 from watchmen_pipeline_surface import pipeline_surface
 from watchmen_rest import RestApp
+from watchmen_rest_doll.sso.sso import install_saml
+
 from .settings import DollSettings
 
+logger = getLogger(f'app.{__name__}')
 
 class DollApp(RestApp):
 	def get_settings(self) -> DollSettings:
@@ -38,6 +41,13 @@ class DollApp(RestApp):
 	def post_construct(self, app: FastAPI) -> None:
 		pass
 
+	def init_sso(self, app: FastAPI) -> None:
+		if self.is_sso_on():
+			if self.settings.SSO_PROVIDER =="saml2":
+				install_saml(app, self.settings)
+			else:
+				logger.error(f'SSO provider {self.settings.SSO_PROVIDER} is not supported.')
+
 	# noinspection PyMethodMayBeStatic
 	def init_pipeline_surface(self) -> None:
 		pipeline_surface.init_connectors()
@@ -67,3 +77,29 @@ def ask_create_pipeline_monitor_topics_on_tenant_create() -> bool:
 
 def ask_create_dqc_topics_on_tenant_create() -> bool:
 	return doll.ask_create_dqc_topics_on_tenant_create()
+
+
+def ask_saml_settings() -> Dict:
+    saml_settings = {
+        'strict': settings.SAML_STRICT,
+        'debug': settings.SAML_DEBUG,
+        'sp': {
+            'entityId': settings.SAML_SP_ENTITY_ID,
+            'assertionConsumerService': {
+                "url": settings.SAML_SP_ASSERT_URL,
+                "binding": settings.SAML_SP_ASSERT_BINDING
+            },
+            "x509cert": settings.SAML_SP_X509CERT,
+
+        },
+        "idp": {
+            "entityId": settings.SAML_IDP_ENTITY_ID,
+            "singleSignOnService": {
+                "url": settings.SAML_IDP_SSO_URL,
+                "binding": settings.SAML_IDP_SSO_BINDING
+            },
+            "x509cert": settings.SAML_IDP_X509CERT
+        },
+    }
+
+    return saml_settings
---
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/sso/sso.py
@@ -0,0 +1,16 @@
+
+from fastapi import FastAPI
+
+from watchmen_rest.settings import RestSettings
+from watchmen_rest_doll.sso.saml import auth_saml_router
+
+
+def install_saml(app: FastAPI, settings: RestSettings) -> None:
+    app.include_router(auth_saml_router.router)
+
+
+
+
+
+
+
