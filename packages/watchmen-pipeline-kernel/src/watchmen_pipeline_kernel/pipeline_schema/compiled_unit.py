653e9c0d57d9c4de885ab3b195e6218975e3e5a3
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-05-25 19:38:35 +0800

    remove the compatibility of none and non-list value, for loop variable value.
    - for none, simply run nothing,
    - for non-list value, flag this unit as error

--- packages/watchmen-pipeline-kernel/src/watchmen_pipeline_kernel/pipeline_schema/compiled_unit.py
+++ packages/watchmen-pipeline-kernel/src/watchmen_pipeline_kernel/pipeline_schema/compiled_unit.py
@@ -3,10 +3,10 @@ from logging import getLogger
 from typing import Any, List
 
 from watchmen_auth import PrincipalService
-from watchmen_data_kernel.storage_bridge import PipelineVariables
+from watchmen_data_kernel.storage_bridge import now, PipelineVariables
 from watchmen_model.admin import Pipeline, PipelineStage, PipelineUnit
-from watchmen_model.pipeline_kernel import MonitorLogStage
-from watchmen_pipeline_kernel.common import ask_parallel_actions_in_loop_unit
+from watchmen_model.pipeline_kernel import MonitorLogStage, MonitorLogStatus, MonitorLogUnit
+from watchmen_pipeline_kernel.common import ask_parallel_actions_in_loop_unit, PipelineKernelException
 from watchmen_pipeline_kernel.pipeline_schema_interface import CreateQueuePipeline, TopicStorages
 from watchmen_utilities import ArrayHelper
 from .compiled_single_unit import CompiledSingleUnit
@@ -40,9 +40,17 @@ class CompiledUnit(CompiledSingleUnit):
 			loop_variable_value = variables.find(loop_variable_name)
 			if loop_variable_value is None:
 				# no value or it is none, run once
-				return self.do_run(
-					variables=clone_variables(loop_variable_value), new_pipeline=new_pipeline,
-					stage_monitor_log=stage_monitor_log, storages=storages, principal_service=principal_service)
+				# return self.do_run(
+				# 	variables=clone_variables(loop_variable_value), new_pipeline=new_pipeline,
+				# 	stage_monitor_log=stage_monitor_log, storages=storages, principal_service=principal_service)
+				unit_monitor_log = MonitorLogUnit(
+					unitId=self.unit.unitId, name=self.unit.name,
+					status=MonitorLogStatus.DONE, startTime=now(), spentInMills=0, error=None,
+					loopVariableName=self.loopVariableName, loopVariableValue=loop_variable_value,
+					actions=[]
+				)
+				stage_monitor_log.units.append(unit_monitor_log)
+				return True
 			elif isinstance(loop_variable_value, list):
 				# a list, run for each element
 
@@ -63,10 +71,20 @@ class CompiledUnit(CompiledSingleUnit):
 					return ArrayHelper(loop_variable_value).every(run_element_in_loop)
 			else:
 				# not a list, run once
-				return self.do_run(
-					variables=clone_variables(loop_variable_value), new_pipeline=new_pipeline,
-					stage_monitor_log=stage_monitor_log,
-					storages=storages, principal_service=principal_service)
+				# return self.do_run(
+				# 	variables=clone_variables(loop_variable_value), new_pipeline=new_pipeline,
+				# 	stage_monitor_log=stage_monitor_log,
+				# 	storages=storages, principal_service=principal_service)
+				# not a list, raise exception
+				unit_monitor_log = MonitorLogUnit(
+					unitId=self.unit.unitId, name=self.unit.name,
+					status=MonitorLogStatus.ERROR, startTime=now(), spentInMills=0,
+					loopVariableName=self.loopVariableName, loopVariableValue=loop_variable_value,
+					actions=[],
+					error=f'Value of loop variable[{loop_variable_name}] must be a list, current is [{loop_variable_value}].'
+				)
+				stage_monitor_log.units.append(unit_monitor_log)
+				return False
 		else:
 			# no loop declared
 			return self.do_run(
