218b28b1063ab3c5aca215168282cb98b9a751da
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-05-29 16:19:13 +0800

    16.2

--- packages/watchmen-web-client/src/admin/toolbox/topic-snapshot/index.tsx
+++ packages/watchmen-web-client/src/admin/toolbox/topic-snapshot/index.tsx
@@ -1,3 +1,40 @@
+import {useAdminCacheEventBus} from '@/admin/cache/cache-event-bus';
+import {AdminCacheEventTypes} from '@/admin/cache/cache-event-bus-types';
+import {Router} from '@/routes/types';
+import {Topic} from '@/services/data/tuples/topic-types';
+import {AdminCacheData} from '@/services/local-persist/types';
+import {VerticalMarginOneUnit} from '@/widgets/basic/margin';
+import {FixWidthPage} from '@/widgets/basic/page';
+import {PageHeader} from '@/widgets/basic/page-header';
+import React, {useEffect, useState} from 'react';
+import {useHistory} from 'react-router-dom';
+
 export const TopicSnapshot = () => {
-	return <></>;
+	const history = useHistory();
+	const {fire: fireCache} = useAdminCacheEventBus();
+	const [data, setData] = useState<{ topics: Array<Topic> }>({topics: []});
+	useEffect(() => {
+		const askData = () => {
+			fireCache(AdminCacheEventTypes.ASK_DATA_LOADED, (loaded) => {
+				if (loaded) {
+					fireCache(AdminCacheEventTypes.ASK_DATA, (data?: AdminCacheData) => {
+						setData({
+							topics: data?.topics || []
+						});
+					});
+				} else {
+					setTimeout(() => askData(), 100);
+				}
+			});
+		};
+		askData();
+	}, [fireCache]);
+
+	const onBackClicked = () => history.push(Router.ADMIN_TOOLBOX);
+
+	return <FixWidthPage>
+		<PageHeader title="Topic Snapshot" onBackClicked={onBackClicked}/>
+		<VerticalMarginOneUnit/>
+	</FixWidthPage>;
+
 };
