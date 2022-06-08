2f4a100dbdba525f8ee6df6cc6b7ad1fcf2014a8
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-06-09 01:03:32 +0800

    16.2

--- packages/watchmen-web-client/src/services/data/mock/tuples/mock-objective-analysis.ts
+++ packages/watchmen-web-client/src/services/data/mock/tuples/mock-objective-analysis.ts
@@ -4,25 +4,34 @@ import {getCurrentTime} from '@/services/data/utils';
 export const listMockObjectiveAnalysis = async (): Promise<Array<ObjectiveAnalysis>> => {
 	return new Promise<Array<ObjectiveAnalysis>>((resolve) => {
 		setTimeout(() => {
-			resolve([{
-				analysisId: '1',
-				title: 'hello worldasdfjhaklsdfhlkashdjflkahsdfkjhaslkdfhlkashjdfklahsjdflkjhalsdfhalkshdflh',
-				lastVisitTime: getCurrentTime(),
-				createdAt: getCurrentTime(),
-				lastModifiedAt: getCurrentTime()
-			}, {
-				analysisId: '2',
-				title: 'hello world',
-				lastVisitTime: getCurrentTime(),
-				createdAt: getCurrentTime(),
-				lastModifiedAt: getCurrentTime()
-			}, {
-				analysisId: '3',
-				title: 'hello world',
-				lastVisitTime: getCurrentTime(),
-				createdAt: getCurrentTime(),
-				lastModifiedAt: getCurrentTime()
-			}]);
+			resolve(new Array(100).fill(1).map((_, index) => {
+				return {
+					analysisId: `${index + 1}`,
+					title: `Hello world ${index + 1}`,
+					lastVisitTime: getCurrentTime(),
+					createdAt: getCurrentTime(),
+					lastModifiedAt: getCurrentTime()
+				};
+			}));
+			// resolve([{
+			// 	analysisId: '1',
+			// 	title: 'hello worldasdfjhaklsdfhlkashdjflkahsdfkjhaslkdfhlkashjdfklahsjdflkjhalsdfhalkshdflh',
+			// 	lastVisitTime: getCurrentTime(),
+			// 	createdAt: getCurrentTime(),
+			// 	lastModifiedAt: getCurrentTime()
+			// }, {
+			// 	analysisId: '2',
+			// 	title: 'hello world',
+			// 	lastVisitTime: getCurrentTime(),
+			// 	createdAt: getCurrentTime(),
+			// 	lastModifiedAt: getCurrentTime()
+			// }, {
+			// 	analysisId: '3',
+			// 	title: 'hello world',
+			// 	lastVisitTime: getCurrentTime(),
+			// 	createdAt: getCurrentTime(),
+			// 	lastModifiedAt: getCurrentTime()
+			// }]);
 		}, 500);
 	});
 };
