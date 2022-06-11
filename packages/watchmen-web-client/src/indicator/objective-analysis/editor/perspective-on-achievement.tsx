d0becf38e5646cf347828d5c20f7c05babfc170d
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-06-11 19:00:43 +0800

    16.2

--- packages/watchmen-web-client/src/indicator/objective-analysis/editor/perspective-on-achievement.tsx
+++ packages/watchmen-web-client/src/indicator/objective-analysis/editor/perspective-on-achievement.tsx
@@ -1,13 +1,17 @@
-import {AchievementId} from '@/services/data/tuples/achievement-types';
+import {AchievementEditPageBody} from '@/indicator/achievement/edit/body';
+import {AchievementSaver} from '@/indicator/achievement/edit/saver';
+import {fetchAchievement} from '@/services/data/tuples/achievement';
+import {Achievement, AchievementId} from '@/services/data/tuples/achievement-types';
 import {ObjectiveAnalysis, ObjectiveAnalysisPerspective} from '@/services/data/tuples/objective-analysis-types';
 import {RoundDwarfButton} from '@/widgets/basic/button';
 import {ICON_OBJECTIVE_ANALYSIS_PERSPECTIVE} from '@/widgets/basic/constants';
 import {ButtonInk} from '@/widgets/basic/types';
+import {useForceUpdate} from '@/widgets/basic/utils';
 import {useEventBus} from '@/widgets/events/event-bus';
 import {EventTypes} from '@/widgets/events/types';
 import {Lang} from '@/widgets/langs';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {AchievementEventBusProvider} from '../../achievement/achievement-event-bus';
 import {AchievementStateHolder} from '../../achievement/state';
 import {useObjectiveAnalysisEventBus} from '../objective-analysis-event-bus';
@@ -22,6 +26,19 @@ export const PerspectiveOnAchievement = (props: { analysis: ObjectiveAnalysis, p
 	const {fire: fireGlobal} = useEventBus();
 	const {fire} = useObjectiveAnalysisEventBus();
 	const {onDescriptionChanged, onDescriptionBlurred} = useDescription(perspective);
+	const [achievement, setAchievement] = useState<Achievement | null>(null);
+	useEffect(() => {
+		const achievementId = perspective.relationId;
+		if (achievementId != null && achievementId.length !== 0) {
+			fireGlobal(EventTypes.INVOKE_REMOTE_REQUEST, async () => {
+				const {achievement} = await fetchAchievement(achievementId);
+				return achievement;
+			}, (achievement: Achievement) => {
+				setAchievement(achievement);
+			});
+		}
+	}, [fireGlobal, perspective.relationId]);
+	const forceUpdate = useForceUpdate();
 
 	const onDeleteClicked = () => {
 		fireGlobal(EventTypes.SHOW_YES_NO_DIALOG,
@@ -32,11 +49,15 @@ export const PerspectiveOnAchievement = (props: { analysis: ObjectiveAnalysis, p
 			},
 			() => fireGlobal(EventTypes.HIDE_DIALOG));
 	};
-	const onAchievementPicked = (achievement: AchievementId) => {
-
+	const onAchievementPicked = (achievementId: AchievementId) => {
+		perspective.relationId = achievementId;
+		fire(ObjectiveAnalysisEventTypes.SAVE, analysis);
+		forceUpdate();
 	};
 	const onAchievementCleared = () => {
-
+		delete perspective.relationId;
+		fire(ObjectiveAnalysisEventTypes.SAVE, analysis);
+		setAchievement(null);
 	};
 
 	return <AchievementEventBusProvider>
@@ -53,8 +74,14 @@ export const PerspectiveOnAchievement = (props: { analysis: ObjectiveAnalysis, p
 					</RoundDwarfButton>
 				</PerspectiveButtons>
 			</PerspectiveDescriptorWrapper>
-			<CreateOrFindAchievement analysis={analysis} perspective={perspective}
+			<CreateOrFindAchievement analysis={analysis} perspective={perspective} achievement={achievement}
 			                         onPicked={onAchievementPicked} onCleared={onAchievementCleared}/>
+			{achievement != null
+				? <>
+					<AchievementEditPageBody achievement={achievement}/>
+					<AchievementSaver achievement={achievement}/>
+				</>
+				: null}
 		</PerspectiveContainer>
 	</AchievementEventBusProvider>;
 };
