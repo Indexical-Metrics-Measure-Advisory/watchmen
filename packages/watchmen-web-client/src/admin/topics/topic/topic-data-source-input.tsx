9acbabc4767b5b8de91874a464a3e96da3dcf779
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-06-21 22:30:29 +0800

    16.2

--- packages/watchmen-web-client/src/admin/topics/topic/topic-data-source-input.tsx
+++ packages/watchmen-web-client/src/admin/topics/topic/topic-data-source-input.tsx
@@ -1,24 +1,48 @@
 import {QueryDataSourceForHolder} from '@/services/data/tuples/query-data-source-types';
 import {Topic, TopicType} from '@/services/data/tuples/topic-types';
+import {isRawTopic, isS3Storage} from '@/services/data/tuples/topic-utils';
 import {DropdownOption} from '@/widgets/basic/types';
 import {useForceUpdate} from '@/widgets/basic/utils';
 import {TuplePropertyDropdown} from '@/widgets/tuple-workbench/tuple-editor';
-import React from 'react';
+import React, {useEffect} from 'react';
 import {useTopicEventBus} from '../topic-event-bus';
 import {TopicEventTypes} from '../topic-event-bus-types';
 
 export const TopicDataSourceInput = (props: { topic: Topic; dataSources: Array<QueryDataSourceForHolder> }) => {
 	const {topic, dataSources} = props;
 
-	const {fire} = useTopicEventBus();
+	const {on, off, fire} = useTopicEventBus();
 	const forceUpdate = useForceUpdate();
+	useEffect(() => {
+		const onTopicTypeChanged = () => {
+			if (!isRawTopic(topic)) {
+				const dataSource = dataSources.find(dataSource => dataSource.dataSourceId == topic.dataSourceId);
+				if (dataSource != null && isS3Storage(dataSource.dataSourceType)) {
+					delete topic.dataSourceId;
+					fire(TopicEventTypes.TOPIC_DATA_SOURCE_CHANGED, topic);
+					forceUpdate();
+				}
+			}
+		};
+		on(TopicEventTypes.TOPIC_TYPE_CHANGED, onTopicTypeChanged);
+		return () => {
+			off(TopicEventTypes.TOPIC_TYPE_CHANGED, onTopicTypeChanged);
+		};
+	}, [on, off, topic, forceUpdate]);
+
 	const onDataSourceChange = (option: DropdownOption) => {
 		topic.dataSourceId = option.value as TopicType;
 		fire(TopicEventTypes.TOPIC_DATA_SOURCE_CHANGED, topic);
 		forceUpdate();
 	};
 
-	const options: Array<DropdownOption> = dataSources.map(dataSource => {
+	const options: Array<DropdownOption> = dataSources.filter(dataSource => {
+		if (topic.type === TopicType.RAW) {
+			return true;
+		} else {
+			return !isS3Storage(dataSource.dataSourceType);
+		}
+	}).map(dataSource => {
 		return {value: dataSource.dataSourceId, label: dataSource.dataSourceCode};
 	});
 
