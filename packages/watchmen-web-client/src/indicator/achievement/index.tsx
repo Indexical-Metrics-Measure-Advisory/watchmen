fa3ec25948e6470782f6f965f0c2775e83b978d7
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-06-22 16:58:08 +0800

    16.2

--- packages/watchmen-web-client/src/indicator/achievement/index.tsx
+++ packages/watchmen-web-client/src/indicator/achievement/index.tsx
@@ -1,8 +1,11 @@
+import {HELP_KEYS, useHelp} from '@/widgets/help';
 import React from 'react';
 import {AchievementEventBusProvider} from './achievement-event-bus';
 import {AchievementRoute} from './route';
 
 const IndicatorAchievementIndex = () => {
+	useHelp(HELP_KEYS.INDICATOR_ACHIEVEMENT);
+
 	return <AchievementEventBusProvider>
 		<AchievementRoute/>
 	</AchievementEventBusProvider>;
--- packages/watchmen-web-client/src/indicator/inspection/index.tsx
+++ packages/watchmen-web-client/src/indicator/inspection/index.tsx
@@ -1,5 +1,6 @@
 import {FixWidthPage} from '@/widgets/basic/page';
 import {PageHeader} from '@/widgets/basic/page-header';
+import {HELP_KEYS, useHelp} from '@/widgets/help';
 import {Lang} from '@/widgets/langs';
 import React from 'react';
 import {Inspection} from './inspection';
@@ -7,6 +8,8 @@ import {InspectionEventBusProvider} from './inspection-event-bus';
 import {InspectionStateHolder} from './state';
 
 const IndicatorInspectionIndex = () => {
+	useHelp(HELP_KEYS.INDICATOR_INSPECTION);
+
 	return <InspectionEventBusProvider>
 		<InspectionStateHolder/>
 		<FixWidthPage>
