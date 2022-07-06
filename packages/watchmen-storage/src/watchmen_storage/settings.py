3b7663e7f053da6474963fd11c0f1009e602a653
skywalkerX69 <skywalkerX69@outlook.com>
2022-07-07 01:31:33 +0800

    disable compiled cache

--- packages/watchmen-storage/src/watchmen_storage/settings.py
+++ packages/watchmen-storage/src/watchmen_storage/settings.py
@@ -1,4 +1,7 @@
 from pydantic import BaseSettings
+from logging import getLogger
+
+logger = getLogger(__name__)
 
 
 class StorageSettings(BaseSettings):
@@ -6,8 +9,15 @@ class StorageSettings(BaseSettings):
 	DECIMAL_FRACTION_DIGITS: int = 8
 	DISABLE_COMPILED_CACHE: bool = False
 
+	class Config:
+		# secrets_dir = '/var/run'
+		env_file = '.env'
+		env_file_encoding = 'utf-8'
+		case_sensitive = True
+
 
 storage_settings = StorageSettings()
+logger.info(f'Storage settings[{storage_settings.dict()}].')
 
 
 def ask_decimal_integral_digits() -> int:
