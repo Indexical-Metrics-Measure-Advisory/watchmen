b0a2b5c314b3ff8f7b0b29462aea0ac083a3867d
skywalkerX69 <skywalkerX69@outlook.com>
2022-07-07 00:45:53 +0800

    disable compiled cache for sqlalchemy

--- packages/watchmen-storage/src/watchmen_storage/settings.py
+++ packages/watchmen-storage/src/watchmen_storage/settings.py
@@ -4,6 +4,7 @@ from pydantic import BaseSettings
 class StorageSettings(BaseSettings):
 	DECIMAL_INTEGRAL_DIGITS: int = 24
 	DECIMAL_FRACTION_DIGITS: int = 8
+	DISABLE_COMPILED_CACHE: bool = False
 
 
 storage_settings = StorageSettings()
@@ -15,3 +16,7 @@ def ask_decimal_integral_digits() -> int:
 
 def ask_decimal_fraction_digits() -> int:
 	return storage_settings.DECIMAL_FRACTION_DIGITS
+
+
+def ask_disable_compiled_cache() -> bool:
+	return storage_settings.DISABLE_COMPILED_CACHE
