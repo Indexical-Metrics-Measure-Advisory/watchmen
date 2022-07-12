01de6efac3c0ea56f239fd911ce4a9df43a5f60f
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-07-12 17:01:54 +0800

    16.3

--- packages/watchmen-web-client/src/indicator/objective-analysis/editor/achievement/widgets.ts
+++ packages/watchmen-web-client/src/indicator/objective-analysis/editor/achievement/widgets.ts
@@ -3,9 +3,5 @@ import styled from 'styled-components';
 export const AchievementEdit = styled.div`
 	display  : block;
 	position : relative;
-	&:hover {
-		> button[data-widget=objective-analysis-perspective-achievement-indicator-adjust-button] {
-			opacity : 1;
-		}
-	}
+	margin   : 0 calc(var(--margin) * -1);
 `;
