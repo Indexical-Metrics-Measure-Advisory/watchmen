e8ed4586efe086ae6aa902aec5c43598d72c42d2
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-07-12 14:23:21 +0800

    16.3

--- packages/watchmen-web-client/src/indicator/achievement/edit/body/index.tsx
+++ packages/watchmen-web-client/src/indicator/achievement/edit/body/index.tsx
@@ -2,10 +2,10 @@ import {Achievement} from '@/services/data/tuples/achievement-types';
 import {Indicator} from '@/services/data/tuples/indicator-types';
 import {useEffect, useState} from 'react';
 import {useAchievementEventBus} from '../../achievement-event-bus';
-import {AchievementEventTypes} from '../../achievement-event-bus-types';
+import {AchievementEventTypes, AchievementRenderMode} from '../../achievement-event-bus-types';
 import {AchievementEditEventBusProvider} from './achievement-edit-event-bus';
 import {Editor} from './editor';
-import {BodyContainer} from './widgets';
+import {Viewer} from './viewer';
 
 interface Indicators {
 	loaded: boolean;
@@ -15,21 +15,34 @@ interface Indicators {
 export const AchievementEditPageBody = (props: { achievement: Achievement }) => {
 	const {achievement} = props;
 
+	const [renderMode, setRenderMode] = useState(AchievementRenderMode.EDIT);
 	const [indicators, setIndicators] = useState<Indicators>({loaded: false, data: []});
-	const {fire} = useAchievementEventBus();
+	const {on, off, fire} = useAchievementEventBus();
 	useEffect(() => {
 		fire(AchievementEventTypes.ASK_INDICATORS, (indicators: Array<Indicator>) => {
 			setIndicators({loaded: true, data: indicators});
 		});
 	}, [fire, achievement]);
+	useEffect(() => {
+		const onSwitchRenderMode = (anAchievement: Achievement, mode: AchievementRenderMode) => {
+			if (anAchievement !== achievement) {
+				return;
+			}
+			setRenderMode(mode);
+		};
+		on(AchievementEventTypes.SWITCH_RENDER_MODE, onSwitchRenderMode);
+		return () => {
+			off(AchievementEventTypes.SWITCH_RENDER_MODE, onSwitchRenderMode);
+		};
+	}, [on, off, achievement]);
 
 	if (!indicators.loaded) {
 		return null;
 	}
 
 	return <AchievementEditEventBusProvider>
-		<BodyContainer>
-			<Editor achievement={achievement} indicators={indicators.data}/>
-		</BodyContainer>
+		{renderMode === AchievementRenderMode.EDIT
+			? <Editor achievement={achievement} indicators={indicators.data}/>
+			: <Viewer achievement={achievement} indicators={indicators.data}/>}
 	</AchievementEditEventBusProvider>;
 };
