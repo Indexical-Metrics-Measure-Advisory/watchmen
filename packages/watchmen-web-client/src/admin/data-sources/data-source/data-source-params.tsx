2465354c2171b3bb24d0825bb6dc9ec068c8e61a
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-08-01 18:34:32 +0800

    16.4: add plugin

--- packages/watchmen-web-client/src/admin/data-sources/data-source/data-source-params.tsx
+++ packages/watchmen-web-client/src/admin/data-sources/data-source/data-source-params.tsx
@@ -1,6 +1,10 @@
 import {DataSource, DataSourceParam} from '@/services/data/tuples/data-source-types';
+import {Button} from '@/widgets/basic/button';
+import {ICON_DELETE} from '@/widgets/basic/constants';
+import {ButtonInk} from '@/widgets/basic/types';
 import {useForceUpdate} from '@/widgets/basic/utils';
 import {TuplePropertyInput, TuplePropertyLabel} from '@/widgets/tuple-workbench/tuple-editor';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React, {ChangeEvent, Fragment} from 'react';
 import styled from 'styled-components';
 import {useDataSourceEventBus} from '../data-source-event-bus';
@@ -8,7 +12,7 @@ import {DataSourceEventTypes} from '../data-source-event-bus-types';
 
 const ExtraParams = styled.div`
 	display               : grid;
-	grid-template-columns : 60% 32px calc(40% - 32px);
+	grid-template-columns : 60% 32px calc(40% - 32px - 48px) 48px;
 	grid-auto-rows        : minmax(var(--grid-tall-row-height), auto);
 	grid-row-gap          : calc(var(--margin) / 4);
 	align-content         : center;
@@ -16,6 +20,12 @@ const ExtraParams = styled.div`
 		align-self   : center;
 		justify-self : center;
 		font-weight  : var(--font-bold);
+	}
+	> button {
+		align-self   : center;
+		justify-self : end;
+		height       : var(--height);
+		width        : var(--height);
 	}
 `;
 export const DataSourceParams = (props: { dataSource: DataSource }) => {
@@ -34,6 +44,11 @@ export const DataSourceParams = (props: { dataSource: DataSource }) => {
 			dataSource.params = [...(dataSource.params || []), param];
 		}
 	};
+	const onParamDelete = (param: DataSourceParam) => () =>  {
+		dataSource.params = (dataSource.params || []).filter(p => p !== param);
+		fire(DataSourceEventTypes.DATASOURCE_PARAM_CHANGED, dataSource);
+		forceUpdate();
+	};
 
 	const params = [...(dataSource.params || []), {name: '', value: ''}].filter(x => x);
 
@@ -47,6 +62,9 @@ export const DataSourceParams = (props: { dataSource: DataSource }) => {
 					<span>=</span>
 					<TuplePropertyInput value={param.value || ''} onChange={onParamNameChange(param, 'value')}
 					                    placeholder={'Parameter value'}/>
+					<Button ink={ButtonInk.DANGER} onClick={onParamDelete(param)}>
+						<FontAwesomeIcon icon={ICON_DELETE}/>
+					</Button>
 				</Fragment>;
 			})}
 		</ExtraParams>
--- packages/watchmen-web-client/src/admin/plugins/plugin/plugin-params.tsx
+++ packages/watchmen-web-client/src/admin/plugins/plugin/plugin-params.tsx
@@ -1,6 +1,10 @@
 import {Plugin} from '@/services/data/tuples/plugin-types';
+import {Button} from '@/widgets/basic/button';
+import {ICON_DELETE} from '@/widgets/basic/constants';
+import {ButtonInk} from '@/widgets/basic/types';
 import {useForceUpdate} from '@/widgets/basic/utils';
 import {TuplePropertyInput, TuplePropertyLabel} from '@/widgets/tuple-workbench/tuple-editor';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import React, {ChangeEvent, Fragment} from 'react';
 import styled from 'styled-components';
 import {usePluginEventBus} from '../plugin-event-bus';
@@ -8,7 +12,7 @@ import {PluginEventTypes} from '../plugin-event-bus-types';
 
 const ExtraParams = styled.div`
 	display               : grid;
-	grid-template-columns : 100%;
+	grid-template-columns : calc(100% - 48px) 48px;
 	grid-auto-rows        : minmax(var(--grid-tall-row-height), auto);
 	grid-row-gap          : calc(var(--margin) / 4);
 	align-content         : center;
@@ -16,6 +20,12 @@ const ExtraParams = styled.div`
 		align-self   : center;
 		justify-self : center;
 		font-weight  : var(--font-bold);
+	}
+	> button {
+		align-self   : center;
+		justify-self : end;
+		height       : var(--height);
+		width        : var(--height);
 	}
 `;
 export const PluginParams = (props: { plugin: Plugin }) => {
@@ -39,6 +49,11 @@ export const PluginParams = (props: { plugin: Plugin }) => {
 			}
 		}
 	};
+	const onParamDelete = (param: string, index: number) => () => {
+		plugin.params = (plugin.params || []).filter((_, idx) => idx !== index);
+		fire(PluginEventTypes.PLUGIN_PARAM_CHANGED, plugin);
+		forceUpdate();
+	};
 
 	const params = [...(plugin.params || []), ''];
 
@@ -49,6 +64,9 @@ export const PluginParams = (props: { plugin: Plugin }) => {
 				return <Fragment key={index}>
 					<TuplePropertyInput value={param || ''} onChange={onParamNameChange(param, index)}
 					                    placeholder={'Parameter name'}/>
+					<Button ink={ButtonInk.DANGER} onClick={onParamDelete(param, index)}>
+						<FontAwesomeIcon icon={ICON_DELETE}/>
+					</Button>
 				</Fragment>;
 			})}
 		</ExtraParams>
