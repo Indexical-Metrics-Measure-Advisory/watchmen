6b9d5c724bafcff4dadf6b551829aac6d6edecf6
skywalkerX69 <skywalkerX69@outlook.com>
2022-08-04 18:08:13 +0800

    support configuration of file name in s3 bucket

--- packages/watchmen-storage-s3/src/watchmen_storage_s3/object_defs_s3.py
+++ packages/watchmen-storage-s3/src/watchmen_storage_s3/object_defs_s3.py
@@ -1,8 +1,9 @@
 from typing import Dict
-
+from string import Template
 from watchmen_model.admin import Topic
 from watchmen_model.common import TopicId
 from watchmen_storage import UnexpectedStorageException
+from datetime import datetime
 
 object_directory: Dict[TopicId, str] = {}
 
@@ -16,3 +17,18 @@ def find_directory(name: str) -> str:
 	if directory is None:
 		raise UnexpectedStorageException(f'Table[{name}] definition not found.')
 	return directory
+
+
+def as_file_name(literal: str, directory: str, id_: str) -> str:
+	tmp = Template(literal)
+	now = datetime.now()
+	values = {'year': now.strftime("%Y"),
+	          'month': now.strftime("%m"),
+	          'day': now.strftime("%d"),
+	          'hour': now.strftime("%H"),
+	          'minute': now.strftime("%M"),
+	          'sec': now.strftime("%S"),
+	          'directory': directory,
+	          'id': id_
+	          }
+	return tmp.substitute(values)
