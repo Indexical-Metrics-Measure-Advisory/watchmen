7639433d1a60292c247cc476895de8ea69b42570
skywalkerX69 <skywalkerX69@outlook.com>
2022-08-16 09:43:50 +0800

    consumer

---
+++ packages/watchmen-collector-kernel/src/watchmen_collector_kernel/lock/__init__.py
@@ -0,0 +1,1 @@
+from .distributed_lock import DistributedLock
---
+++ packages/watchmen-collector-kernel/src/watchmen_collector_kernel/service/s3_consumer.py
@@ -0,0 +1,16 @@
+from .consumer import Consumer
+
+
+class S3Consumer(Consumer):
+
+	def __init__(self):
+		pass
+
+	def process(self):
+		pass
+
+	def pre_process(self) -> bool:
+		pass
+
+	def post_process(self):
+		pass
