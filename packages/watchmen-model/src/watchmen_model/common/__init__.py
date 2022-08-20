0ad00391028d2cdfe8eccf1d9d2f53a2bba7f19b
skywalkerX69 <skywalkerX69@outlook.com>
2022-08-20 16:41:44 +0800

    consumer

--- packages/watchmen-model/src/watchmen_model/common/__init__.py
+++ packages/watchmen-model/src/watchmen_model/common/__init__.py
@@ -11,4 +11,4 @@ from .tuple import TenantBasedTuple, Tuple, UserBasedTuple
 from .tuple_ids import AchievementId, AchievementPluginTaskId, BucketId, ConnectedSpaceId, DashboardId, DataSourceId, EnumId, \
 	EnumItemId, ExternalWriterId, FactorId, IndicatorId, InspectionId, ObjectiveAnalysisId, PatId, PipelineActionId, \
 	PipelineGraphicId, PipelineId, PipelineStageId, PipelineUnitId, PluginId, ReportFunnelId, ReportId, SpaceId, \
-	SubjectDatasetColumnId, SubjectId, TenantId, TopicId, UserGroupId, UserId， OssCollectorCompetitiveLockId
+	SubjectDatasetColumnId, SubjectId, TenantId, TopicId, UserGroupId, UserId, OssCollectorCompetitiveLockId
