75e1b290c7089524af0191eab9e01e1422ae3c9d
skywalkerX69 <skywalkerX69@outlook.com>
2022-08-25 10:32:01 +0800

    Update 00001-collector_competitive_lock.ddl.sql

--- packages/watchmen-storage-postgresql/meta-scripts/16.3.5/00001-collector_competitive_lock.ddl.sql
+++ packages/watchmen-storage-postgresql/meta-scripts/16.3.5/00001-collector_competitive_lock.ddl.sql
@@ -7,4 +7,4 @@ CREATE TABLE oss_collector_competitive_lock
     registered_at  TIMESTAMP   NOT NULL,
     CONSTRAINT pk_oss_collector_competitive_lock PRIMARY KEY (lock_id)
 );
-CREATE UNIQUE INDEX u_topics_1 ON oss_collector_competitive_lock (model_name, object_id);
+CREATE UNIQUE INDEX u_oss_collector_competitive_lock_1 ON oss_collector_competitive_lock (model_name, object_id);
