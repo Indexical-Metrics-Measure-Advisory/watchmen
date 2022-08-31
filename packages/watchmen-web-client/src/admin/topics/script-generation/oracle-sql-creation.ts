ff4bacbac3a317c19b22940b54c4dd719c8fa5df
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-08-31 14:55:57 +0800

    fixed: use standard func to generate column name

--- packages/watchmen-web-client/src/admin/topics/script-generation/oracle-sql-creation.ts
+++ packages/watchmen-web-client/src/admin/topics/script-generation/oracle-sql-creation.ts
@@ -25,13 +25,13 @@ const buildFactors = (topic: Topic) => {
 			...topic.factors.filter(factor => {
 				return factor.flatten === true;
 			}).map(factor => {
-				return `\t${factor.name.toUpperCase()} ${OracleFactorTypeMap[factor.type](factor.precision)},`;
+				return `\t${asFactorName(factor)} ${OracleFactorTypeMap[factor.type](factor.precision)},`;
 			}),
 			`\t${getRawTopicDataColumnName()} CLOB,`
 		].join('\n');
 	} else {
 		return topic.factors.filter(factor => factor.name.indexOf('.') === -1).map(factor => {
-			return `\t${factor.name.toUpperCase()} ${OracleFactorTypeMap[factor.type](factor.precision)},`;
+			return `\t${asFactorName(factor)} ${OracleFactorTypeMap[factor.type](factor.precision)},`;
 		}).join('\n');
 	}
 };
@@ -65,7 +65,7 @@ ${buildVersion(topic)}
 	${getUpdateTimeColumnName()} DATE,
 
 	-- primary key
-	PRIMARY KEY (${getIdColumnName()})
+	CONSTRAINT pk_${tableName} PRIMARY KEY (${getIdColumnName()})
 );
 
 -- unique index
