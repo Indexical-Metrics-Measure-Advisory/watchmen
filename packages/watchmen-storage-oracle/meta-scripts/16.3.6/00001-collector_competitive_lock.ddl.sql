5de4eb72e6fa74caf15fefe2641f220c1ba420d0
skywalkerX69 <skywalkerX69@outlook.com>
2022-09-26 01:07:19 +0800

    Update 00001-collector_competitive_lock.ddl.sql

--- packages/watchmen-storage-oracle/meta-scripts/16.3.6/00001-collector_competitive_lock.ddl.sql
+++ packages/watchmen-storage-oracle/meta-scripts/16.3.6/00001-collector_competitive_lock.ddl.sql
@@ -3,7 +3,6 @@ ALTER TABLE collector_competitive_lock
 ADD tenant_id VARCHAR2(50) NOT NULL;
 ALTER TABLE collector_competitive_lock
 ADD status NUMBER(1) NOT NULL;
-ALTER TABLE collector_competitive_lock
 DROP INDEX u_oss_collector_competitive_lock_1;
 ALTER TABLE collector_competitive_lock
 ADD CONSTRAINT unique_resource UNIQUE (resource_id);
