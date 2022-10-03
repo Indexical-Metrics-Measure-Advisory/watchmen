0264c56e950654dcf937733afc7a3b70cdfed29e
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-10-04 02:23:40 +0800

    add movedate

--- packages/watchmen-storage-mysql/data-scripts/16.0.0/00005-move_date.func.sql
+++ packages/watchmen-storage-mysql/data-scripts/16.0.0/00005-move_date.func.sql
@@ -28,148 +28,74 @@ BEGIN
                 SET year = value;
             ELSEIF command = '+' THEN
                 -- add year
-                SET year = year + value;
+                RETURN DATE_ADD(a_date, INTERVAL value YEAR);
             ELSEIF command = '-' THEN
                 -- subtract year
-                SET year = year - value;
+                RETURN DATE_SUB(a_date, INTERVAL value YEAR);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         ELSEIF location = 'M' THEN
             IF command = '' THEN
-                -- check month
-                IF value < 1 OR value > 12 THEN
-                    SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT =
-                            'Month must be between 1 and 12 when do set directly on date movement.';
-                ELSE
-                    -- simply set month to given value
-                    SET month = value;
-                END IF;
+                SET month = IF(value < 1, 1, IF(value > 12, 12, value));
             ELSEIF command = '+' THEN
-                SET movement_value = VALUE % 12;
-                IF month + movement_value > 12 THEN
-                    SET year = year + FLOOR(month / 12) + 1;
-                    SET month = month + movement_value - 12;
-                ELSE
-                    SET year = year + FLOOR(month / 12);
-                    SET month = month + movement_value;
-                END IF;
+                RETURN DATE_ADD(a_date, INTERVAL value MONTH);
             ELSEIF command = '-' THEN
-                SET movement_value = VALUE % 12;
-                IF month - movement_value < 1 THEN
-                    SET year = year - FLOOR(month / 12) - 1;
-                    SET month = month - movement_value + 12;
-                ELSE
-                    SET year = year - FLOOR(month / 12);
-                    SET month = month - movement_value;
-                END IF;
+                RETURN DATE_SUB(a_date, INTERVAL value MONTH);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         ELSEIF location = 'D' THEN
             IF value = 99 THEN
-                IF month = 1 OR month = 3 OR month = 5 OR month = 7 OR month = 8 OR month = 10 OR month = 12 THEN
-                    SET day_of_month = 31;
-                ELSEIF month = 4 OR month = 6 OR month = 9 OR month = 11 THEN
-                    SET day_of_month = 30;
-                ELSE
-                    SET day_of_month = 29;
-                END IF;
+                SET day_of_month = 31;
             ELSEIF command = '' THEN
-                IF value < 1 THEN
-                    SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT =
-                            'Day of month must be started from 1 when do set directly on date movement.';
-                END IF;
-                IF month = 1 OR month = 3 OR month = 5 OR month = 7 OR month = 8 OR month = 10 OR month = 12 THEN
-                    IF value > 31 THEN
-                        SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT =
-                                'Day of month must be ended by 31 when do set directly on date movement for Jan, Mar, May, Jul, Aug, Oct and Dec.';
-                    ELSE
-                        SET day_of_month = 31;
-                    END IF;
-                ELSEIF month = 4 OR month = 6 OR month = 9 OR month = 11 THEN
-                    IF value > 30 THEN
-                        SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT =
-                                'Day of month must be ended by 30 when do set directly on date movement for Apr, Jun, Sep and Nov.';
-                    ELSE
-                        SET day_of_month = 30;
-                    END IF;
-                ELSE
-                    IF value > 29 THEN
-                        SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT =
-                                'Day of month must be ended by 29 when do set directly on date movement for Feb.';
-                    ELSE
-                        SET day_of_month = 29;
-                    END IF;
-                END IF;
-                SET day_of_month = value;
+                SET day_of_month = IF(value < 1, 1, value);
             ELSEIF command = '+' THEN
                 RETURN DATE_ADD(a_date, INTERVAL value DAY);
             ELSEIF command = '-' THEN
                 RETURN DATE_SUB(a_date, INTERVAL value DAY);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         ELSEIF location = 'h' THEN
             IF command = '' THEN
-                IF value < 0 OR VALUE >= 60 THEN
-                    SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = 'Hour must be between 0 and 59 when do set directly.';
-                ELSE
-                    SET hour = value;
-                END IF;
+                SET hour = IF(value < 0, 0, IF(value > 23, 23, value));
             ELSEIF command = '+' THEN
                 RETURN DATE_ADD(a_date, INTERVAL value DAY_HOUR);
             ELSEIF command = '-' THEN
                 RETURN DATE_SUB(a_date, INTERVAL value DAY_HOUR);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         ELSEIF location = 'm' THEN
             IF command = '' THEN
-                IF value < 0 OR VALUE >= 60 THEN
-                    SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = 'Minute must be between 0 and 59 when do set directly.';
-                ELSE
-                    SET minute = value;
-                END IF;
+                SET minute = IF(value < 0, 0, IF(value > 59, 59, value));
             ELSEIF command = '+' THEN
                 RETURN DATE_ADD(a_date, INTERVAL value DAY_MINUTE);
             ELSEIF command = '-' THEN
                 RETURN DATE_SUB(a_date, INTERVAL value DAY_MINUTE);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         ELSEIF location = 's' THEN
             IF command = '' THEN
-                IF value < 0 OR VALUE >= 60 THEN
-                    SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = 'Second must be between 0 and 59 when do set directly.';
-                ELSE
-                    SET minute = value;
-                END IF;
+                SET second = IF(value < 0, 0, IF(value > 59, 59, value));
             ELSEIF command = '+' THEN
                 RETURN DATE_ADD(a_date, INTERVAL value DAY_SECOND);
             ELSEIF command = '-' THEN
                 RETURN DATE_SUB(a_date, INTERVAL value DAY_SECOND);
             ELSE
-                SET @msg = CONCAT('Date movement command[', command, '] is not supported.');
-                SIGNAL SQLSTATE 'HY000' SET MESSAGE_TEXT = @msg;
+                RETURN a_date;
             END IF;
         END IF;
     END IF;
 
-    IF month = 2 and day_of_month >= 29 THEN
-        -- set to 02/29, check leap year
-        SET last_day_of_month = DAY(LAST_DAY(STR_TO_DATE(DATE_FORMAT(a_date, '%Y-02-%d'), '%Y-%m-%d')));
-        IF last_day_of_month = 28 THEN
-            -- last day of feb is 28, set day of month to 28
-            SET day_of_month = 28;
-        END IF;
+    -- if day of month is greater than last day of month, reset it
+    SET last_day_of_month = DAY(LAST_DAY(STR_TO_DATE(
+            DATE_FORMAT(a_date, CONCAT('%Y-', IF(month < 10, CONCAT('0', month), month), '-%d')), '%Y-%m-%d')));
+    IF day_of_month > last_day_of_month THEN
+        SET day_of_month = last_day_of_month;
     END IF;
-
 
     RETURN STR_TO_DATE(
             CONCAT(
