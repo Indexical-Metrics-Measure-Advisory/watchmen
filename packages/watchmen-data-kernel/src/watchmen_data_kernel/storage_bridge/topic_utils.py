c62247d951e15c1fea047fc7430ae396be9f7081
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-10-05 21:53:20 +0800

    fixed: remove cache when topic is fake

--- packages/watchmen-data-kernel/src/watchmen_data_kernel/storage_bridge/topic_utils.py
+++ packages/watchmen-data-kernel/src/watchmen_data_kernel/storage_bridge/topic_utils.py
@@ -6,13 +6,19 @@ from watchmen_model.admin import is_raw_topic
 
 def ask_topic_data_entity_helper(schema: TopicSchema) -> TopicDataEntityHelper:
 	"""
-	ask topic data entity helper, from cache first
+	ask topic data entity helper, from cache first.
+	never use cache for fake topic
 	"""
-	data_entity_helper = CacheService.topic().get_entity_helper(schema.get_topic().topicId)
+	if schema.get_topic().topicId == '-1':
+		data_entity_helper = None
+	else:
+		data_entity_helper = CacheService.topic().get_entity_helper(schema.get_topic().topicId)
 	if data_entity_helper is None:
 		if is_raw_topic(schema.get_topic()):
 			data_entity_helper = RawTopicDataEntityHelper(schema)
 		else:
 			data_entity_helper = RegularTopicDataEntityHelper(schema)
-		CacheService.topic().put_entity_helper(data_entity_helper)
+
+		if schema.get_topic().topicId != '-1':
+			CacheService.topic().put_entity_helper(data_entity_helper)
 	return data_entity_helper
