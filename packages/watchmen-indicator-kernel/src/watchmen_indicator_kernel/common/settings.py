728e1b84b257cbe0289d22972db14a2868aa6c04
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-10-12 11:13:25 +0800

    remove useless settings item

--- packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/common/settings.py
+++ packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/common/settings.py
@@ -8,16 +8,11 @@ logger = getLogger(__name__)
 
 
 class IndicatorKernelSettings(BaseSettings):
-	META_STORAGE_TYPE: DataSourceType = DataSourceType.MYSQL
 	PLUGIN_HOST: str = None
 
 
 settings = IndicatorKernelSettings()
 logger.info(f'Indicator kernel settings[{settings.dict()}].')
-
-
-def ask_meta_storage_type() -> DataSourceType:
-	return settings.META_STORAGE_TYPE
 
 
 def ask_plugin_host() -> str:
