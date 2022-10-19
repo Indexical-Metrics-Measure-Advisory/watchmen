3771d3cda07eb71e0284c538412d8facecbcb6e3
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-10-19 17:31:43 +0800

    add achievement data service

--- packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/meta/objective_analysis_service.py
+++ packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/meta/objective_analysis_service.py
@@ -1,10 +1,11 @@
-from typing import List
+from typing import List, Optional
 
 from watchmen_meta.common import TupleService, TupleShaper
-from watchmen_model.common import ObjectiveAnalysisId, TenantId
+from watchmen_model.common import DataPage, ObjectiveAnalysisId, Pageable, TenantId
 from watchmen_model.indicator import ObjectiveAnalysis
-from watchmen_storage import ColumnNameLiteral, EntityCriteriaExpression, EntityRow, EntityShaper
-from watchmen_utilities import ArrayHelper, is_not_blank
+from watchmen_storage import ColumnNameLiteral, EntityCriteriaExpression, EntityCriteriaJoint, \
+	EntityCriteriaJointConjunction, EntityCriteriaOperator, EntityRow, EntityShaper
+from watchmen_utilities import ArrayHelper
 
 
 class ObjectiveAnalysisShaper(EntityShaper):
@@ -53,3 +54,20 @@ class ObjectiveAnalysisService(TupleService):
 			criteria.append(EntityCriteriaExpression(left=ColumnNameLiteral(columnName='tenant_id'), right=tenant_id))
 		# noinspection PyTypeChecker
 		return self.storage.find(self.get_entity_finder(criteria=criteria))
+
+	def find_page_by_text(self, text: Optional[str], tenant_id: Optional[TenantId], pageable: Pageable) -> DataPage:
+		criteria = []
+		if text is not None and len(text.strip()) != 0:
+			criteria.append(EntityCriteriaJoint(
+				conjunction=EntityCriteriaJointConjunction.OR,
+				children=[
+					EntityCriteriaExpression(
+						left=ColumnNameLiteral(columnName='title'), operator=EntityCriteriaOperator.LIKE, right=text),
+					EntityCriteriaExpression(
+						left=ColumnNameLiteral(columnName='description'), operator=EntityCriteriaOperator.LIKE,
+						right=text)
+				]
+			))
+		if tenant_id is not None and len(tenant_id.strip()) != 0:
+			criteria.append(EntityCriteriaExpression(left=ColumnNameLiteral(columnName='tenant_id'), right=tenant_id))
+		return self.storage.page(self.get_entity_pager(criteria=criteria, pageable=pageable))
