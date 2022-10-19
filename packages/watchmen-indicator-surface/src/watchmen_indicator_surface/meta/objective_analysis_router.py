4e1582dc7dd734146f50de0f4254d471bb19d616
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-10-19 17:31:43 +0800

    add achievement data service

--- packages/watchmen-indicator-surface/src/watchmen_indicator_surface/meta/objective_analysis_router.py
+++ packages/watchmen-indicator-surface/src/watchmen_indicator_surface/meta/objective_analysis_router.py
@@ -1,6 +1,6 @@
 from typing import Callable, List, Optional
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends
 from starlette.responses import Response
 
 from watchmen_auth import PrincipalService
@@ -9,9 +9,9 @@ from watchmen_indicator_surface.settings import ask_tuple_delete_enabled
 from watchmen_indicator_surface.util import trans, trans_readonly
 from watchmen_meta.common import ask_meta_storage, ask_snowflake_generator
 from watchmen_model.admin import UserRole
-from watchmen_model.common import ObjectiveAnalysisId, TenantId
+from watchmen_model.common import DataPage, ObjectiveAnalysisId, Pageable, TenantId
 from watchmen_model.indicator import ObjectiveAnalysis
-from watchmen_rest import get_console_principal, get_super_admin_principal
+from watchmen_rest import get_admin_principal, get_console_principal, get_super_admin_principal
 from watchmen_rest.util import raise_400, raise_403, raise_404
 from watchmen_utilities import is_blank
 
@@ -90,6 +90,28 @@ async def find_my_analysis(
 		tenant_id: TenantId = principal_service.get_tenant_id()
 		# noinspection PyTypeChecker
 		return analysis_service.find_all(tenant_id)
+
+	return trans_readonly(analysis_service, action)
+
+
+class QueryAnalysisDataPage(DataPage):
+	data: List[ObjectiveAnalysis]
+
+
+@router.post('/indicator/objective-analysis/name', tags=[UserRole.ADMIN], response_model=QueryAnalysisDataPage)
+async def find_analysis_page_by_name(
+		query_name: Optional[str], pageable: Pageable = Body(...),
+		principal_service: PrincipalService = Depends(get_admin_principal)) -> QueryAnalysisDataPage:
+	analysis_service = get_analysis_service(principal_service)
+
+	def action() -> QueryAnalysisDataPage:
+		tenant_id: TenantId = principal_service.get_tenant_id()
+		if is_blank(query_name):
+			# noinspection PyTypeChecker
+			return analysis_service.find_page_by_text(None, tenant_id, pageable)
+		else:
+			# noinspection PyTypeChecker
+			return analysis_service.find_page_by_text(query_name, tenant_id, pageable)
 
 	return trans_readonly(analysis_service, action)
 
