a6cace1f605f6c19be0ee817176baf1d3f2d045c
skywalkerX69 <skywalkerX69@outlook.com>
2022-10-21 11:21:53 +0800

    fix: rename to collector_competitive_lock and use new index

--- packages/watchmen-storage-mongodb/meta-scripts/16.3.6/00001-collector_competitive_lock.ddl.sql
+++ packages/watchmen-storage-mongodb/meta-scripts/16.3.6/00001-collector_competitive_lock.ddl.sql
@@ -1,1 +1,3 @@
+db.oss_collector_competitive_lock.renameCollection("collector_competitive_lock")
+db.collector_competitive_lock.dropIndex({model_name:1, object_id:1})
 db.collector_competitive_lock.createIndex({resource_id:1}, {unique:true})
