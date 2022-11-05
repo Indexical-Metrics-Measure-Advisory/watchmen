e9cd909f5ec1022897b8c40761fa860fbc4fdb55
skywalkerX69 <skywalkerX69@outlook.com>
2022-11-06 01:13:33 +0800

    support version for data and generate deployment script.

---
+++ packages/watchmen-storage-mongodb/meta-scripts/16.4.1/00001-create_default_package_version.dml.sql
@@ -0,0 +1,4 @@
+db.package_versions.insertOne({
+    version_id: '1', previous_version: '50.0.0', current_version: '50.0.0', tenant_id: '1',
+    created_at: new Date(), created_by: '1', last_modified_at: new Date(), last_modified_by: '1'
+});
