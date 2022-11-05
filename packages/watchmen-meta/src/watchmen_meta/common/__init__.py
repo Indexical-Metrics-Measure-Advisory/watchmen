75b67c41da93571fdc62129960de7a51d08b4057
skywalkerX69 <skywalkerX69@outlook.com>
2022-11-05 09:53:30 +0800

    support version for data and generate deployment script.

--- packages/watchmen-meta/src/watchmen_meta/common/__init__.py
+++ packages/watchmen-meta/src/watchmen_meta/common/__init__.py
@@ -1,7 +1,8 @@
 from .exception import InitialMetaAppException
 from .last_visit_service import LastVisitShaper
 from .settings import ask_datasource_aes_enabled, ask_datasource_aes_params, ask_engine_index_enabled, \
-	ask_meta_storage, ask_snowflake_generator, ask_super_admin, MetaSettings, ask_meta_storage_type
+	ask_meta_storage, ask_snowflake_generator, ask_super_admin, MetaSettings, ask_meta_storage_type, \
+	ask_default_package_version
 from .storage_service import EntityService, IdentifiedStorableService, StorageService, TupleNotFoundException
 from .tuple_service import AuditableShaper, OptimisticLockShaper, TupleService, TupleShaper
 from .user_based_tuple_service import UserBasedTupleService, UserBasedTupleShaper
