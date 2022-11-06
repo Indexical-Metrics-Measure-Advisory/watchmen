a552297dfa4059bd865877d06deacee342a992f0
skywalkerX69 <skywalkerX69@outlook.com>
2022-11-07 01:25:22 +0800

    fix: change the index statements of table package_versions

--- packages/watchmen-storage-oracle/meta-scripts/16.4.1/00002-package_versions.ddl.sql
+++ packages/watchmen-storage-oracle/meta-scripts/16.4.1/00002-package_versions.ddl.sql
@@ -10,8 +10,8 @@ CREATE TABLE package_versions
     last_modified_by     VARCHAR2(50) NOT NULL,
     CONSTRAINT pk_versions PRIMARY KEY (version_id)
 );
-CREATE INDEX i_versions_1 ON versions (tenant_id);
-CREATE INDEX i_versions_2 ON versions (created_at);
-CREATE INDEX i_versions_3 ON versions (created_by);
-CREATE INDEX i_versions_4 ON versions (last_modified_at);
-CREATE INDEX i_versions_5 ON versions (last_modified_by);
+CREATE INDEX i_package_versions_1 ON package_versions (tenant_id);
+CREATE INDEX i_package_versions_2 ON package_versions (created_at);
+CREATE INDEX i_package_versions_3 ON package_versions (created_by);
+CREATE INDEX i_package_versions_4 ON package_versions (last_modified_at);
+CREATE INDEX i_package_versions_5 ON package_versions (last_modified_by);
