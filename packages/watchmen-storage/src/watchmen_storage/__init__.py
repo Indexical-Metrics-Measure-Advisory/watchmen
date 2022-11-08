5f06d3c17badbf082ea3640f1e239d2293f82a62
skywalkerX69 <skywalkerX69@outlook.com>
2022-11-08 21:39:08 +0800

    support secret for data source config

--- packages/watchmen-storage/src/watchmen_storage/__init__.py
+++ packages/watchmen-storage/src/watchmen_storage/__init__.py
@@ -1,7 +1,7 @@
 from .competitive_worker_id_generator import competitive_worker_id, CompetitiveWorker, CompetitiveWorkerIdGenerator, \
 	CompetitiveWorkerRestarter, CompetitiveWorkerShutdownListener, CompetitiveWorkerShutdownSignal, get_host_ip, \
 	WorkerCreationException, WorkerDeclarationException, WorkerFirstDeclarationException
-from .data_source_helper import DataSourceHelper
+from .data_source_helper import DataSourceHelper, ask_datasource_name
 from .free_storage_types import FreeAggregateArithmetic, FreeAggregateColumn, FreeAggregatePager, FreeAggregator, \
 	FreeColumn, FreeFinder, FreeJoin, FreeJoinType, FreePager
 from .settings import ask_decimal_fraction_digits, ask_decimal_integral_digits, ask_disable_compiled_cache, \
