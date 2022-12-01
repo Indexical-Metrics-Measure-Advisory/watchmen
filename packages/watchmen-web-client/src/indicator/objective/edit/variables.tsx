651e4e033d396b21e472fb9e1f2bd7f69b846ee8
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-12-01 19:05:18 +0800

    objective refactor

--- packages/watchmen-web-client/src/indicator/objective/edit/variables.tsx
+++ packages/watchmen-web-client/src/indicator/objective/edit/variables.tsx
@@ -1,13 +1,319 @@
+import {Bucket, BucketId} from '@/services/data/tuples/bucket-types';
+import {
+	Objective,
+	ObjectiveVariable,
+	ObjectiveVariableKind,
+	ObjectiveVariableOnBucket,
+	ObjectiveVariableOnRange,
+	ObjectiveVariableOnValue
+} from '@/services/data/tuples/objective-types';
+import {isNotBlank} from '@/services/utils';
+import {Button} from '@/widgets/basic/button';
+import {ICON_COLLAPSE_CONTENT, ICON_DELETE, ICON_EDIT} from '@/widgets/basic/constants';
+import {Dropdown} from '@/widgets/basic/dropdown';
+import {Input} from '@/widgets/basic/input';
+import {ButtonInk, DropdownOption} from '@/widgets/basic/types';
 import {Lang} from '@/widgets/langs';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import React, {ChangeEvent, MouseEvent, useState} from 'react';
 import {EditStep} from './edit-step';
 import {ObjectiveDeclarationStep} from './steps';
 import {EditObjective} from './types';
+import {useSave} from './use-save';
+import {
+	AddItemButton,
+	IncorrectOptionLabel,
+	ItemLabel,
+	ItemNo,
+	RemoveItemButton,
+	VariableContainer,
+	VariableKindButton,
+	VariableKindContainer,
+	VariableKindIcon,
+	VariablesContainer,
+	VariableValuesContainer
+} from './widgets';
+
+const isValueVariable = (variable: ObjectiveVariable): variable is ObjectiveVariableOnValue => variable.kind === ObjectiveVariableKind.SINGLE_VALUE;
+const isRangeVariable = (variable: ObjectiveVariable): variable is ObjectiveVariableOnRange => variable.kind === ObjectiveVariableKind.RANGE;
+const isBucketVariable = (variable: ObjectiveVariable): variable is ObjectiveVariableOnBucket => variable.kind === ObjectiveVariableKind.BUCKET;
+const defendVariableAndRemoveUnnecessary = (variable: ObjectiveVariable) => {
+	if (isValueVariable(variable)) {
+		variable.value = variable.value ?? (variable as unknown as ObjectiveVariableOnRange).min;
+		delete (variable as unknown as ObjectiveVariableOnRange).min;
+		delete (variable as unknown as ObjectiveVariableOnRange).max;
+		delete (variable as unknown as ObjectiveVariableOnRange).includeMin;
+		delete (variable as unknown as ObjectiveVariableOnRange).includeMax;
+		delete (variable as unknown as ObjectiveVariableOnBucket).bucketId;
+		delete (variable as unknown as ObjectiveVariableOnBucket).segmentName;
+	} else if (isRangeVariable(variable)) {
+		variable.min = variable.min ?? (variable as unknown as ObjectiveVariableOnValue).value;
+		variable.includeMin = variable.includeMin ?? true;
+		variable.includeMax = variable.includeMax ?? true;
+		delete (variable as unknown as ObjectiveVariableOnValue).value;
+		delete (variable as unknown as ObjectiveVariableOnBucket).bucketId;
+		delete (variable as unknown as ObjectiveVariableOnBucket).segmentName;
+	} else if (isBucketVariable(variable)) {
+		delete (variable as unknown as ObjectiveVariableOnValue).value;
+		delete (variable as unknown as ObjectiveVariableOnRange).min;
+		delete (variable as unknown as ObjectiveVariableOnRange).max;
+		delete (variable as unknown as ObjectiveVariableOnRange).includeMin;
+		delete (variable as unknown as ObjectiveVariableOnRange).includeMax;
+	}
+};
+
+const VariableValues = (props: {
+	objective: Objective;
+	variable: ObjectiveVariable;
+	buckets: Array<Bucket>;
+}) => {
+	const {objective, variable, buckets} = props;
+
+	const save = useSave();
+
+	const onValueChanged = (key: 'value' | 'min' | 'max') => (event: ChangeEvent<HTMLInputElement>) => {
+		(variable as any)[key] = event.target.value;
+		save(objective);
+	};
+	const onIncludeChanged = (key: 'includeMin' | 'includeMax') => () => {
+		(variable as ObjectiveVariableOnRange)[key] = !(variable as ObjectiveVariableOnRange)[key];
+		save(objective);
+	};
+	const onBucketChanged = (option: DropdownOption) => {
+		const variableOnBucket = variable as ObjectiveVariableOnBucket;
+		const bucketId = option.value as BucketId;
+		// eslint-disable-next-line
+		if (bucketId == variableOnBucket.bucketId || bucketId === '') {
+			return;
+		}
+
+		variableOnBucket.bucketId = bucketId;
+		delete variableOnBucket.segmentName;
+		save(objective);
+	};
+	const onSegmentChanged = (option: DropdownOption) => {
+		const variableOnBucket = variable as ObjectiveVariableOnBucket;
+		const segmentName = option.value as string;
+		if (segmentName === '') {
+			return;
+		}
+		variableOnBucket.segmentName = segmentName;
+		save(objective);
+	};
+
+	const {buckets: bucketOptions, segments: segmentOptions} = (() => {
+		if (!isBucketVariable(variable)) {
+			return {buckets: [], segments: []};
+		}
+
+		const bucketOptions: Array<DropdownOption> = buckets.map(bucket => {
+			return {value: bucket.bucketId, label: bucket.name};
+		}).sort((b1, b2) => {
+			return (b1.label || '').toLowerCase().localeCompare((b2.label || '').toLowerCase());
+		});
+		let segmentOptions: Array<DropdownOption> = [];
+		if (isNotBlank(variable.bucketId)) {
+			// bucket selected
+			// eslint-disable-next-line
+			const bucketFound = buckets.find(b => b.bucketId == variable.bucketId);
+			if (bucketFound == null) {
+				// selected bucket not found
+				bucketOptions.push({
+					value: variable.bucketId!, label: () => {
+						return {
+							node: <IncorrectOptionLabel>
+								{Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_INCORRECT_SELECTED}
+							</IncorrectOptionLabel>, label: ''
+						};
+					}
+				} as DropdownOption);
+				if (isNotBlank(variable.segmentName)) {
+					segmentOptions.push({
+						value: variable.segmentName, label: () => {
+							return {
+								node: <IncorrectOptionLabel>
+									{Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_SEGMENT_INCORRECT_SELECTED}
+								</IncorrectOptionLabel>, label: ''
+							};
+						}
+					});
+				}
+			} else {
+				// selected bucket found
+				segmentOptions = (() => {
+					if (isBucketVariable(variable) && variable.bucketId != null) {
+						// eslint-disable-next-line
+						return (buckets.find(bucket => bucket.bucketId == variable.bucketId)?.segments || []).map(segment => {
+							return {value: segment.name, label: segment.name};
+						}).sort((s1, s2) => {
+							return (s1.label || '').toLowerCase().localeCompare((s2.label || '').toLowerCase());
+						});
+					} else {
+						return [];
+					}
+				})();
+				if (isNotBlank(variable.segmentName)) {
+					// eslint-disable-next-line
+					const segmentFound = (bucketFound.segments || []).find(s => s.name == variable.segmentName);
+					if (segmentFound == null) {
+						segmentOptions.push({
+							value: variable.segmentName, label: () => {
+								return {
+									node: <IncorrectOptionLabel>
+										{Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_SEGMENT_INCORRECT_SELECTED}
+									</IncorrectOptionLabel>, label: ''
+								};
+							}
+						});
+					}
+				}
+			}
+
+			if (segmentOptions.length === 0) {
+				segmentOptions.push({value: '', label: Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_SEGMENT_NO_AVAILABLE});
+			}
+			return {buckets: bucketOptions, segments: segmentOptions};
+		} else {
+			// bucket not selected
+			segmentOptions = [{value: '', label: Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_SELECT_FIRST}];
+		}
+		if (bucketOptions.length === 0) {
+			// no available buckets
+			bucketOptions.push({value: '', label: Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_NO_AVAILABLE});
+		}
+		return {buckets: bucketOptions, segments: segmentOptions};
+	})();
+
+	return <VariableValuesContainer>
+		{isValueVariable(variable) ? <Input value={variable.value ?? ''} onChange={onValueChanged('value')}/> : null}
+		{isRangeVariable(variable)
+			? <>
+				<Button onClick={onIncludeChanged('includeMin')}>
+					{variable.includeMin ? Lang.INDICATOR.OBJECTIVE.VARIABLE_RANGE_INCLUDE_MIN : Lang.INDICATOR.OBJECTIVE.VARIABLE_RANGE_NOT_INCLUDE_MIN}
+				</Button>
+				<Input value={variable.min ?? ''} onChange={onValueChanged('min')}/>
+				<ItemLabel>{Lang.INDICATOR.OBJECTIVE.VARIABLE_RANGE_TO}</ItemLabel>
+				<Input value={variable.max ?? ''} onChange={onValueChanged('max')}/>
+				<Button onClick={onIncludeChanged('includeMax')}>
+					{variable.includeMax ? Lang.INDICATOR.OBJECTIVE.VARIABLE_RANGE_INCLUDE_MAX : Lang.INDICATOR.OBJECTIVE.VARIABLE_RANGE_NOT_INCLUDE_MAX}
+				</Button>
+			</> : null}
+		{isBucketVariable(variable)
+			? <>
+				<Dropdown value={variable.bucketId} options={bucketOptions} onChange={onBucketChanged}
+				          please={Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_PLACEHOLDER}/>
+				<Dropdown value={variable.segmentName} options={segmentOptions} onChange={onSegmentChanged}
+				          please={Lang.INDICATOR.OBJECTIVE.VARIABLE_BUCKET_SEGMENT_PLACEHOLDER}/>
+			</> : null}
+	</VariableValuesContainer>;
+};
+
+const Variable = (props: {
+	objective: Objective;
+	variable: ObjectiveVariable;
+	index: number;
+	onRemove: (variable: ObjectiveVariable) => void;
+	buckets: Array<Bucket>;
+}) => {
+	const {objective, variable, index, onRemove, buckets} = props;
+
+	const [editing, setEditing] = useState(false);
+	const save = useSave();
+
+	const onNameChanged = (event: ChangeEvent<HTMLInputElement>) => {
+		const {value} = event.target;
+		variable.name = value;
+		save(objective);
+	};
+	const onStartEditing = () => setEditing(true);
+	const onBlur = () => setEditing(false);
+	const onKindChanged = (kind: ObjectiveVariableKind) => (event: MouseEvent<HTMLDivElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+		if (kind === variable.kind) {
+			// do nothing, discard or start editing
+			setEditing(!editing);
+		} else {
+			variable.kind = kind;
+			defendVariableAndRemoveUnnecessary(variable);
+			setEditing(false);
+			save(objective);
+		}
+	};
+	const onIconClicked = (event: MouseEvent<HTMLDivElement>) => {
+		event.preventDefault();
+		event.stopPropagation();
+		setEditing(!editing);
+	};
+	const onRemoveClicked = () => onRemove(variable);
+
+	const kinds = {
+		[ObjectiveVariableKind.SINGLE_VALUE]: Lang.INDICATOR.OBJECTIVE.VARIABLE_KIND_SINGLE_VALUE,
+		[ObjectiveVariableKind.RANGE]: Lang.INDICATOR.OBJECTIVE.VARIABLE_KIND_RANGE,
+		[ObjectiveVariableKind.BUCKET]: Lang.INDICATOR.OBJECTIVE.VARIABLE_KIND_BUCKET
+	};
+	const kindCandidates = [
+		ObjectiveVariableKind.SINGLE_VALUE, ObjectiveVariableKind.RANGE, ObjectiveVariableKind.BUCKET
+	].filter(kind => kind !== variable.kind);
+
+	return <VariableContainer>
+		<ItemNo>{index === -1 ? '' : `#${index}`}</ItemNo>
+		<Input value={variable.name || ''} onChange={onNameChanged}
+		       placeholder={Lang.PLAIN.OBJECTIVE_VARIABLE_NAME_PLACEHOLDER}/>
+		<ItemLabel>{Lang.INDICATOR.OBJECTIVE.VARIABLE_IS}</ItemLabel>
+		<VariableKindContainer onClick={onStartEditing} tabIndex={0} onBlur={onBlur}>
+			<VariableKindButton active={true} edit={editing} onClick={onKindChanged(variable.kind)}>
+				{kinds[variable.kind]}
+			</VariableKindButton>
+			{kindCandidates.map(candidate => {
+				return <VariableKindButton active={false} edit={editing} onClick={onKindChanged(candidate)}
+				                           key={candidate}>
+					{kinds[candidate]}
+				</VariableKindButton>;
+			})}
+			<VariableKindIcon onClick={onIconClicked} data-expanded={editing}>
+				{editing ? <FontAwesomeIcon icon={ICON_COLLAPSE_CONTENT}/> : <FontAwesomeIcon icon={ICON_EDIT}/>}
+			</VariableKindIcon>
+		</VariableKindContainer>
+		<VariableValues objective={objective} variable={variable} buckets={buckets}/>
+		<RemoveItemButton ink={ButtonInk.DANGER} onClick={onRemoveClicked}>
+			<FontAwesomeIcon icon={ICON_DELETE}/>
+		</RemoveItemButton>
+	</VariableContainer>;
+};
 
 export const Variables = (props: { data: EditObjective }) => {
 	const {data} = props;
 
-	console.log(data);
+	const [buckets] = useState<Array<Bucket>>([]);
+	const save = useSave();
+
+	if (data.objective.variables == null) {
+		data.objective.variables = [];
+	}
+
+	const onRemove = (variable: ObjectiveVariable) => {
+		data.objective.variables!.splice(data.objective.variables!.indexOf(variable), 1);
+		save(data.objective);
+	};
+	const onAddClicked = () => {
+		data.objective.variables!.push({kind: ObjectiveVariableKind.SINGLE_VALUE} as ObjectiveVariable);
+		save(data.objective);
+	};
+
+	const variables = data.objective.variables;
 
 	return <EditStep index={ObjectiveDeclarationStep.VARIABLES} title={Lang.INDICATOR.OBJECTIVE.VARIABLES_TITLE}>
+		<VariablesContainer>
+			{variables.map((variable, index) => {
+				return <Variable objective={data.objective} variable={variable} index={index + 1}
+				                 onRemove={onRemove}
+				                 buckets={buckets}
+				                 key={`${variable.name || ''}-${index}`}/>;
+			})}
+			<AddItemButton ink={ButtonInk.PRIMARY} onClick={onAddClicked}>
+				{Lang.INDICATOR.OBJECTIVE.ADD_VARIABLE}
+			</AddItemButton>
+		</VariablesContainer>
 	</EditStep>;
 };
--- packages/watchmen-web-client/src/indicator/objective/edit/widgets.ts
+++ packages/watchmen-web-client/src/indicator/objective/edit/widgets.ts
@@ -14,76 +14,6 @@ export const BackToListButtonContainer = styled.div.attrs({'data-widget': 'objec
 	display     : flex;
 	margin-left : calc(var(--margin) / 2);
 `;
-export const TargetsContainer = styled.div.attrs({'data-widget': 'objective-targets'})`
-	display               : grid;
-	position              : relative;
-	grid-template-columns : 1fr;
-	grid-column-gap       : calc(var(--margin) / 2);
-`;
-export const TargetContainer = styled.div.attrs({'data-widget': 'objective-target'})`
-	display               : grid;
-	position              : relative;
-	grid-template-columns : 40px repeat(6, auto) 1fr;
-	grid-column-gap       : calc(var(--margin) / 2);
-	grid-row-gap          : calc(var(--margin) / 4);
-	margin                : 0 calc(var(--margin) / -2);
-	padding               : calc(var(--margin) / 2);
-	&:before, &:after {
-		content          : '';
-		display          : block;
-		position         : absolute;
-		top              : 0;
-		left             : 0;
-		width            : calc(100% - var(--margin));
-		height           : 100%;
-		background-color : transparent;
-	}
-	&:before {
-		opacity    : 0.05;
-		transition : background-color 300ms ease-in-out;
-		z-index    : -2;
-	}
-	&:after {
-		border-radius : calc(var(--border-radius) * 2);
-		border-width  : 2px;
-		border-color  : transparent;
-		border-style  : dashed;
-		opacity       : 0;
-		transition    : all 300ms ease-in-out;
-		z-index       : -1;
-	}
-	&:hover:after {
-		border-color : var(--info-color);
-		opacity      : 0.3;
-	}
-	&:nth-child(2n + 1) {
-		&:before {
-			background-color : var(--info-color);
-		}
-	}
-	> input:nth-child(14n + 3) {
-		max-width   : 600px;
-		grid-column : 3 / span 6;
-	}
-	> span:nth-child(14n + 4),
-	> span:nth-child(14n + 7),
-	> span:nth-child(14n + 9) {
-		grid-column : 2;
-	}
-	> input:nth-child(14n + 5) {
-		max-width : 150px;
-	}
-	> span:nth-child(14n + 6) {
-		grid-column : 4 / span 5;
-		opacity     : 0.7;
-	}
-	> div:nth-child(14n + 12) {
-		margin-right : var(--margin);
-	}
-	> div[data-widget=checkbox] {
-		align-self : center;
-	}
-`;
 export const ItemNo = styled.span.attrs({'data-widget': 'objective-item-no'})`
 	display      : flex;
 	align-items  : center;
@@ -106,15 +36,115 @@ export const ItemValue = styled.span.attrs({'data-widget': 'objective-item-value
 	height      : var(--height);
 	font-size   : 1.1em;
 `;
-export const SetTargetAsIsButton = styled(Button).attrs({'data-widget': 'objective-set-asis-target'})`
-	justify-self : start;
-`;
-export const AddTargetButton = styled(Button).attrs({'data-widget': 'objective-add-target'})`
-	margin-left  : calc(40px + var(--margin) / 2);
-	justify-self : start;
+export const AddItemButton = styled(Button).attrs({'data-widget': 'objective-add-item'})`
+	margin-left   : calc(40px + var(--margin) / 2);
+	justify-self  : start;
+	border-radius : calc(var(--height) / 2);
+	height        : var(--height);
 	&:not(:first-child) {
 		margin-top : calc(var(--margin) / 2);
 	}
+`;
+export const RemoveItemButton = styled(Button).attrs({'data-widget': 'objective-remove-item'})`
+	justify-self  : end;
+	margin-right  : var(--margin);
+	border-radius : calc(var(--height) / 2);
+	height        : var(--height);
+`;
+const ItemContainer = styled.div`
+	display         : grid;
+	position        : relative;
+	grid-column-gap : calc(var(--margin) / 2);
+	grid-row-gap    : calc(var(--margin) / 4);
+	margin-left     : calc(var(--margin) / -2);
+	padding         : 0 calc(var(--margin) / 2);
+	> button:last-child {
+		opacity        : 0;
+		pointer-events : none;
+	}
+	&:hover {
+		> button:last-child {
+			opacity        : 1;
+			pointer-events : auto;
+		}
+	}
+`;
+export const TargetsContainer = styled.div.attrs({'data-widget': 'objective-targets'})`
+	display               : grid;
+	position              : relative;
+	grid-template-columns : 1fr;
+	grid-column-gap       : calc(var(--margin) / 2);
+`;
+export const TargetContainer = styled(ItemContainer).attrs({'data-widget': 'objective-target'})`
+	grid-template-columns : 40px repeat(6, auto) 1fr;
+	padding               : calc(var(--margin) / 2);
+	&:before, &:after {
+		content          : '';
+		display          : block;
+		position         : absolute;
+		top              : 0;
+		left             : 0;
+		width            : calc(100% - calc(var(--margin) / 2));
+		height           : 100%;
+		background-color : transparent;
+	}
+	&:before {
+		opacity    : 0.05;
+		transition : background-color 300ms ease-in-out;
+		z-index    : -2;
+	}
+	&:after {
+		border-radius : calc(var(--border-radius) * 2);
+		border-width  : 2px;
+		border-color  : transparent;
+		border-style  : dashed;
+		opacity       : 0;
+		transition    : all 300ms ease-in-out;
+		z-index       : -1;
+	}
+	&:hover {
+		&:after {
+			border-color : var(--info-color);
+			opacity      : 0.3;
+		}
+		> button:last-child {
+			opacity        : 1;
+			pointer-events : auto;
+		}
+	}
+	&:nth-child(2n + 1):before {
+		background-color : var(--info-color);
+	}
+	> input:nth-child(3) {
+		max-width   : 600px;
+		grid-column : 3 / span 6;
+	}
+	> span:nth-child(4),
+	> span:nth-child(7),
+	> span:nth-child(9) {
+		grid-column : 2;
+	}
+	> input:nth-child(5) {
+		max-width : 150px;
+	}
+	> span:nth-child(6) {
+		grid-column : 4 / span 5;
+		opacity     : 0.7;
+	}
+	> div:nth-child(12) {
+		margin-right : var(--margin);
+	}
+	> div[data-widget=checkbox] {
+		align-self : center;
+	}
+	> button:last-child {
+		opacity        : 0;
+		pointer-events : none;
+	}
+`;
+export const SetTargetAsIsButton = styled(Button).attrs({'data-widget': 'objective-set-asis-target'})`
+	justify-self  : start;
+	border-radius : calc(var(--height) / 2);
 `;
 export const TimeFrameContainer = styled.div.attrs<{
 	timeRelated: boolean; lastN: boolean; specifiedTill: boolean
@@ -166,10 +196,164 @@ export const TimeFrameContainer = styled.div.attrs<{
 	> span:nth-child(10) {
 		grid-column : 1;
 	}
-	> span:nth-child(11),
-	> span:nth-child(13),
-	> span:nth-child(15) {
-		//font-weight: var(--font-bold);
+`;
+export const VariablesContainer = styled.div.attrs({'data-widget': 'objective-variables'})`
+	display               : grid;
+	position              : relative;
+	grid-template-columns : 1fr;
+	grid-column-gap       : calc(var(--margin) / 2);
+	grid-row-gap          : calc(var(--margin) / 4);
+`;
+export const VariableContainer = styled(ItemContainer).attrs({'data-widget': 'objective-variable'})`
+	grid-template-columns : 40px auto auto auto 1fr auto;
+	> input:nth-child(2) {
+		width : 200px;
+	}
+	> button:last-child {
+		padding       : 0;
+		width         : var(--height);
+		border-radius : 100%;
+		&:before {
+			content          : '';
+			display          : block;
+			position         : absolute;
+			height           : 1px;
+			width            : calc(var(--margin) / 2);
+			top              : calc(var(--height) / 2);
+			left             : calc(var(--margin) / -2);
+			background-color : var(--border-color);
+		}
+	}
+`;
+export const VariableKindContainer = styled.div.attrs({'data-widget': 'objective-variable-kind'})`
+	display          : flex;
+	position         : relative;
+	align-items      : center;
+	align-self       : center;
+	justify-self     : start;
+	font-variant     : petite-caps;
+	font-weight      : var(--font-demi-bold);
+	height           : calc(var(--height) - 2px);
+	background-color : var(--param-bg-color);
+	border-radius    : calc(var(--height) / 2 - 1px);
+	box-shadow       : var(--param-border);
+	cursor           : pointer;
+	outline          : none;
+	transition       : box-shadow 300ms ease-in-out;
+	&:hover {
+		box-shadow : var(--primary-hover-shadow);
+	}
+`;
+export const VariableKindButton = styled.div.attrs<{ active: boolean, edit: boolean }>(({active, edit}) => {
+	return {
+		'data-widget': 'variable-kind-button',
+		style: {
+			display: (edit || active) ? (void 0) : 'none',
+			backgroundColor: active ? (void 0) : 'var(--bg-color)',
+			boxShadow: active ? (void 0) : 'var(--param-left-border)'
+		}
+	};
+})<{ active: boolean, edit: boolean }>`
+	display      : flex;
+	align-items  : center;
+	font-variant : petite-caps;
+	font-weight  : var(--font-demi-bold);
+	height       : calc(var(--height) - 2px);
+	padding      : 0 calc(var(--margin) / 2);
+	white-space  : nowrap;
+	transition   : color 300ms ease-in-out;
+	&:hover {
+		color : ${({active}) => active ? (void 0) : 'var(--warn-color)'};
+	}
+`;
+export const VariableKindIcon = styled.div.attrs({'data-widget': 'variable-kind-icon'})`
+	display         : flex;
+	position        : relative;
+	align-items     : center;
+	justify-content : center;
+	padding         : 0 calc(var(--margin) / 2);
+	width           : 20px;
+	height          : 20px;
+	&[data-expanded=true] {
+		&:before {
+			display : none;
+		}
+		> svg {
+			transform  : rotateZ(180deg);
+			transition : transform 300ms ease-in-out;
+		}
+	}
+	&:before {
+		content          : '';
+		display          : block;
+		position         : absolute;
+		top              : 25%;
+		left             : 0;
+		width            : 1px;
+		height           : 50%;
+		background-color : var(--invert-color);
+		opacity          : 0.5;
+	}
+	> svg {
+		font-size : 0.8em;
+	}
+`;
+export const VariableValuesContainer = styled.div.attrs({'data-widget': 'objective-variable-values'})`
+	display  : flex;
+	position : relative;
+	&:before {
+		content          : '';
+		display          : block;
+		position         : absolute;
+		height           : 1px;
+		width            : calc(var(--margin) / 2);
+		top              : calc(var(--height) / 2);
+		left             : calc(var(--margin) / -2);
+		background-color : var(--border-color);
+	}
+	> input:first-child:last-child {
+		border-radius : calc(var(--height) / 2);
+		padding       : 0 calc(var(--margin) / 2);
+		width         : 100%;
+	}
+	> span:nth-child(3) {
+		display          : flex;
+		padding          : 0 calc(var(--margin) / 2);
+		height           : var(--height);
+		align-items      : center;
+		font-variant     : petite-caps;
+		font-weight      : var(--font-demi-bold);
+		background-color : var(--border-color);
+	}
+	> button {
+		background-color : var(--border-color);
+		font-weight      : var(--font-bold);
+		font-variant     : petite-caps;
+	}
+	> button:first-child {
+		border-radius : calc(var(--height) / 2) 0 0 calc(var(--height) / 2);
+	}
+	> button:last-child {
+		border-radius : 0 calc(var(--height) / 2) calc(var(--height) / 2) 0;
+	}
+	> input:not(:first-child) {
+		border-radius : 0;
+		border-left   : 0;
+		border-right  : 0;
+		flex-grow     : 1;
+		~ input {
+			border-left   : 0;
+			border-right  : 0;
+			padding-right : calc(var(--margin) / 2);
+			flex-grow     : 1;
+		}
+	}
+	> div[data-widget=dropdown]:first-child {
+		border-radius : calc(var(--height) / 2) 0 0 calc(var(--height) / 2);
+		border-right  : 0;
+		+ div[data-widget=dropdown] {
+			border-radius : 0 calc(var(--height) / 2) calc(var(--height) / 2) 0;
+		}
 	}
 `;
 export const NameInput = styled(Input)`
@@ -182,4 +366,8 @@ export const DescriptionText = styled(InputLines)`
 	width     : calc(100% - var(--margin) / 2);
 	height    : calc(var(--height) * 5);
 	font-size : 1.1em;
+`;
+export const IncorrectOptionLabel = styled.span.attrs({'data-widget': 'incorrect-option'})`
+	color           : var(--danger-color);
+	text-decoration : line-through;
 `;
