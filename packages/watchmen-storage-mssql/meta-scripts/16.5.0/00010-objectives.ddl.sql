d95c6c5173b245684637360b3a968bf84e3e199c
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-12-23 01:39:58 +0800

    objective refactor

---
+++ packages/watchmen-storage-mssql/meta-scripts/16.5.0/00010-objectives.ddl.sql
@@ -0,0 +1,23 @@
+CREATE TABLE objectives
+(
+    objective_id     NVARCHAR(50) NOT NULL,
+    name             NVARCHAR(100),
+    description      NVARCHAR(1024),
+    time_frame       NVARCHAR(MAX),
+    targets          NVARCHAR(MAX),
+    variables        NVARCHAR(MAX),
+    factors          NVARCHAR(MAX),
+    group_ids        NVARCHAR(2048),
+    tenant_id        NVARCHAR(50) NOT NULL,
+    created_at       DATETIME     NOT NULL,
+    created_by       NVARCHAR(50) NOT NULL,
+    last_modified_at DATETIME     NOT NULL,
+    last_modified_by NVARCHAR(50) NOT NULL,
+    CONSTRAINT pk_objectives PRIMARY KEY (objective_id)
+);
+CREATE INDEX i_objectives_1 ON objectives (name);
+CREATE INDEX i_objectives_3 ON objectives (tenant_id);
+CREATE INDEX i_objectives_4 ON objectives (created_at);
+CREATE INDEX i_objectives_5 ON objectives (created_by);
+CREATE INDEX i_objectives_6 ON objectives (last_modified_at);
+CREATE INDEX i_objectives_7 ON objectives (last_modified_by);
---
+++ packages/watchmen-storage-oracle/meta-scripts/16.5.0/00010-objectives.ddl.sql
@@ -0,0 +1,23 @@
+CREATE TABLE objectives
+(
+    objective_id     VARCHAR2(50) NOT NULL,
+    name             VARCHAR2(100),
+    description      VARCHAR2(1024),
+    time_frame       CLOB,
+    targets          CLOB,
+    variables        CLOB,
+    factors          CLOB,
+    group_ids        VARCHAR2(2048),
+    tenant_id        VARCHAR2(50) NOT NULL,
+    created_at       DATE         NOT NULL,
+    created_by       VARCHAR2(50) NOT NULL,
+    last_modified_at DATE         NOT NULL,
+    last_modified_by VARCHAR2(50) NOT NULL,
+    CONSTRAINT pk_objectives PRIMARY KEY (objective_id)
+);
+CREATE INDEX i_objectives_1 ON objectives (name);
+CREATE INDEX i_objectives_3 ON objectives (tenant_id);
+CREATE INDEX i_objectives_4 ON objectives (created_at);
+CREATE INDEX i_objectives_5 ON objectives (created_by);
+CREATE INDEX i_objectives_6 ON objectives (last_modified_at);
+CREATE INDEX i_objectives_7 ON objectives (last_modified_by);
