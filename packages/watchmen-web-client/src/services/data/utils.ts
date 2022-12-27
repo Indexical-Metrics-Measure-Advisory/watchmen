0d33ede791989618969b30df5d854a9d7cac57c9
botlikes <75356972+botlikes456@users.noreply.github.com>
2022-12-27 10:59:37 +0800

    add fix request headers on env, when fire fetch

--- packages/watchmen-web-client/src/services/data/utils.ts
+++ packages/watchmen-web-client/src/services/data/utils.ts
@@ -1,6 +1,14 @@
 import dayjs, {Dayjs} from 'dayjs';
 
 export const isMockService = (): boolean => process.env.REACT_APP_SERVICE_MOCK_FLAG === 'true';
+const DECLARED_REQUEST_HEADERS: Record<string, string> = (() => {
+	try {
+		return JSON.parse(process.env.REACT_APP_REQUEST_HEADER || '{}');
+	} catch {
+		return {};
+	}
+})();
+// console.log(DECLARED_REQUEST_HEADERS);
 export const getServiceHost = (): string => {
 	if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
 		return process.env.REACT_APP_SERVICE_URL!;
@@ -25,6 +33,19 @@ export const formatTime = (time: Dayjs) => time.format('YYYY-MM-DD HH:mm:ss');
 export const getCurrentTime = () => dayjs().format('YYYY-MM-DD HH:mm:ss');
 
 export const doFetch = async (input: RequestInfo, init?: RequestInit): Promise<Response> => {
+	if (Object.keys(DECLARED_REQUEST_HEADERS).length !== 0) {
+		if (init == null) {
+			init = {};
+		}
+		if (init.headers == null) {
+			init.headers = {};
+		}
+		Object.keys(DECLARED_REQUEST_HEADERS).forEach(key => {
+			// @ts-ignore
+			init!.headers![key] = DECLARED_REQUEST_HEADERS[key];
+		});
+	}
+
 	const response = await fetch(input, init);
 
 	const {ok, status, statusText} = response;
