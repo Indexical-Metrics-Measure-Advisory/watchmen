33fe4335e3994e9aae63d24c8ceffb9fff9bf692
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-01-10 14:28:32 +0800

    objective refactor

--- packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/data/__init__.py
+++ packages/watchmen-indicator-kernel/src/watchmen_indicator_kernel/data/__init__.py
@@ -1,2 +1,2 @@
-from .inspection import get_inspection_data_service
 from .objective_factor import get_objective_factor_data_service
+from .utils import compute_chain_frame, compute_previous_frame, compute_time_frame
