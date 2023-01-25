4bf14d7197b8c1436bd415f3a83cd1bfaeceeaa5
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-01-25 17:53:18 +0800

    consanguinity

--- packages/watchmen-web-client/src/consanguinity/nodes/topic.tsx
+++ packages/watchmen-web-client/src/consanguinity/nodes/topic.tsx
@@ -1,8 +1,9 @@
 import {ConsanguinityUniqueId} from '@/services/data/tuples/consanguinity';
 import {Lang} from '@/widgets/langs';
+import React from 'react';
 import {DiagramRelation, DiagramTopic, DiagramTopicFactor} from '../types';
 import {useNodeClick} from './use-node-click';
-import {NodeContainer, NodeItem, NodeItems, NodeTitle} from './widgets';
+import {NodeContainer, NodeItem, NodeItems, NodeTitle, NodeWrapper} from './widgets';
 
 const TopicFactorNode = (props: { topic: DiagramTopic, factor: DiagramTopicFactor }) => {
 	const {factor} = props;
@@ -25,14 +26,16 @@ export const TopicNode = (props: { data: DiagramTopic; relations: Array<DiagramR
 	}, {} as Record<ConsanguinityUniqueId, boolean>);
 	const factors = (data.factors || []).filter(factor => map[factor['@cid']] != null);
 
-	return <NodeContainer data-node-type="topic" data-node-id={data.topicId}>
-		<NodeTitle>
-			{(data.name || '').trim() || Lang.CONSANGUINITY.NONAME_TOPIC}
-		</NodeTitle>
-		<NodeItems>
-			{factors.map(factor => {
-				return <TopicFactorNode topic={data} factor={factor} key={factor.factorId}/>;
-			})}
-		</NodeItems>
-	</NodeContainer>;
+	return <NodeWrapper>
+		<NodeContainer data-node-type="topic" data-node-id={data.topicId}>
+			<NodeTitle>
+				{(data.name || '').trim() || Lang.CONSANGUINITY.NONAME_TOPIC}
+			</NodeTitle>
+			<NodeItems>
+				{factors.map(factor => {
+					return <TopicFactorNode topic={data} factor={factor} key={factor.factorId}/>;
+				})}
+			</NodeItems>
+		</NodeContainer>
+	</NodeWrapper>;
 };
