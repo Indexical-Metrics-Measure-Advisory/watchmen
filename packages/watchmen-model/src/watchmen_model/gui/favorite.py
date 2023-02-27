70515babbf390ffc8ed1fde1e5085f9c545c2e1a
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-02-27 16:37:30 +0800

    add derived objective ids into favorites

--- packages/watchmen-model/src/watchmen_model/gui/favorite.py
+++ packages/watchmen-model/src/watchmen_model/gui/favorite.py
@@ -2,9 +2,10 @@ from typing import List
 
 from pydantic import BaseModel
 
-from watchmen_model.common import ConnectedSpaceId, DashboardId, LastVisit, UserBasedTuple
+from watchmen_model.common import ConnectedSpaceId, DashboardId, DerivedObjectiveId, LastVisit, UserBasedTuple
 
 
 class Favorite(UserBasedTuple, LastVisit, BaseModel):
 	connectedSpaceIds: List[ConnectedSpaceId] = []
 	dashboardIds: List[DashboardId] = []
+	derivedObjectiveIds: List[DerivedObjectiveId] = []
