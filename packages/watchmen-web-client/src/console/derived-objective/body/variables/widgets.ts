b2401eac3d2ffa36ab57281f929b03dd36984ae1
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-01 17:06:08 +0800

    console derived objective

--- packages/watchmen-web-client/src/console/derived-objective/body/variables/widgets.ts
+++ packages/watchmen-web-client/src/console/derived-objective/body/variables/widgets.ts
@@ -3,20 +3,32 @@ import styled from 'styled-components';
 export const VariablesContainer = styled.div.attrs({'data-widget': 'derived-objective-variables'})`
 	display               : grid;
 	position              : relative;
-	grid-template-columns : repeat(3, 120px 1fr);
+	grid-template-columns : repeat(3, 150px 1fr);
 	grid-column-gap       : var(--margin);
 	grid-row-gap          : calc(var(--margin) / 2);
+	margin-bottom         : var(--margin);
+	transition            : height 300ms ease-in-out;
+	&[data-visible=false] {
+		height        : 0;
+		margin-bottom : 0;
+		overflow      : hidden;
+	}
 `;
-export const VariableName = styled.div.attrs({'data-widget': 'derived-objective-variable-name'})`
+export const VariablesTitle = styled.div.attrs({'data-widget': 'derived-objective-variables-title'})`
 	display       : flex;
 	position      : relative;
-	grid-column   : span 3;
+	grid-column   : span 6;
 	align-items   : center;
 	font-size     : 1.4em;
 	font-weight   : var(--font-bold);
 	font-variant  : petite-caps;
 	min-height    : var(--tall-height);
-	margin-bottom : calc(var(--margin) / 2);
 	opacity       : 0.8;
 `;
-
+export const VariableName = styled.div.attrs({'data-widget': 'derived-objective-variable-name'})`
+	display      : flex;
+	position     : relative;
+	align-items  : center;
+	font-variant : petite-caps;
+	opacity      : 0.8;
+`;
---
+++ packages/watchmen-web-client/src/console/derived-objective/objective-state-holder.tsx
@@ -0,0 +1,34 @@
+import {SAVE_TIMEOUT} from '@/services/constants';
+import {saveDerivedObjective} from '@/services/data/tuples/derived-objective';
+import {DerivedObjective} from '@/services/data/tuples/derived-objective-types';
+import {ObjectiveValues} from '@/services/data/tuples/objective-types';
+import {useEventBus} from '@/widgets/events/event-bus';
+import {EventTypes} from '@/widgets/events/types';
+import {useThrottler} from '@/widgets/throttler';
+import {Fragment, useEffect} from 'react';
+import {useObjectiveEventBus} from './objective-event-bus';
+import {ObjectiveEventTypes} from './objective-event-bus-types';
+
+export const ObjectiveStateHandler = (props: { derivedObjective: DerivedObjective }) => {
+	const {derivedObjective} = props;
+
+	const {fire: fireGlobal} = useEventBus();
+	const {on, off, fire} = useObjectiveEventBus();
+	const saveQueue = useThrottler();
+	useEffect(() => saveQueue.clear(true), [derivedObjective, saveQueue]);
+	useEffect(() => {
+		const onAskValues = () => {
+			saveQueue.replace(() => {
+				fireGlobal(EventTypes.INVOKE_REMOTE_REQUEST,
+					async () => await saveDerivedObjective(derivedObjective),
+					(values: ObjectiveValues) => fire(ObjectiveEventTypes.SAVED));
+			}, SAVE_TIMEOUT);
+		};
+		on(ObjectiveEventTypes.SAVE, onAskValues);
+		return () => {
+			off(ObjectiveEventTypes.SAVE, onAskValues);
+		};
+	}, [fireGlobal, on, off, fire, derivedObjective, saveQueue]);
+
+	return <Fragment/>;
+};
