65d6ab60d3c35bb4acc54933e075e3ad77e2a2cd
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-02 20:41:24 +0800

    console derived objective

--- packages/watchmen-web-client/src/services/data/mock/tuples/mock-data-derived-objectives.ts
+++ packages/watchmen-web-client/src/services/data/mock/tuples/mock-data-derived-objectives.ts
@@ -8,6 +8,7 @@ export const DemoDerivedObjectives: Array<DerivedObjective> = [
 		objectiveId: OBJECTIVE_MONTHLY_SALES_ID,
 		name: MonthlySalesObjective.name,
 		definition: MonthlySalesObjective,
+		breakdownTargets: [],
 		lastVisitTime: '2020/10/31 14:23:07',
 		createdAt: getCurrentTime(),
 		lastModifiedAt: getCurrentTime()
