b9ddcd5ecb224d39127c5833ba515c46b6e882c7
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-02 20:41:24 +0800

    console derived objective

--- packages/watchmen-web-client/src/services/data/tuples/derived-objective-types.ts
+++ packages/watchmen-web-client/src/services/data/tuples/derived-objective-types.ts
@@ -1,8 +1,36 @@
 import {DateTime} from '../types';
-import {Objective, ObjectiveId} from './objective-types';
+import {BucketId} from './bucket-types';
+import {FactorId} from './factor-types';
+import {MeasureMethod} from './indicator-types';
+import {Objective, ObjectiveId, ObjectiveTargetId} from './objective-types';
+import {SubjectDataSetColumnId} from './subject-types';
 import {Tuple} from './tuple-types';
 
 export type DerivedObjectiveId = string;
+
+export enum BreakdownDimensionType {
+	VALUE = 'value',
+	BUCKET = 'bucket',
+	TIME_RELATED = 'time'
+}
+
+export interface BreakdownDimension {
+	// when type is VALUE, which means no bucket, no time measure method. use the original value as dimension
+	type: BreakdownDimensionType;
+	// if measure on factor, factor id must be given
+	factorOrColumnId: FactorId | SubjectDataSetColumnId;
+	// bucket for any measure on type
+	bucketId?: BucketId;
+	// only when factor/column is date, and adaptable time measure method could be applied
+	// for example, if factor is date, then YEAR/QUARTER/MONTH/etc. could be applied to it
+	// if factor is year, then only YEAR could be applied to it.
+	timeMeasureMethod?: MeasureMethod;
+}
+
+export interface BreakdownTarget {
+	targetId: ObjectiveTargetId;
+	dimensions: Array<BreakdownDimension>;
+}
 
 export interface DerivedObjective extends Tuple {
 	derivedObjectiveId: DerivedObjectiveId;
@@ -10,5 +38,6 @@ export interface DerivedObjective extends Tuple {
 	description?: string;
 	objectiveId: ObjectiveId;
 	definition: Objective;
+	breakdownTargets: Array<BreakdownTarget>;
 	lastVisitTime: DateTime;
 }
