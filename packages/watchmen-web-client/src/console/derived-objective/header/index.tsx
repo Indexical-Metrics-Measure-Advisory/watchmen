2af0941f840444462205520d550ef3bbf44aba06
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-04 18:24:45 +0800

    console derived objective

--- packages/watchmen-web-client/src/console/derived-objective/header/index.tsx
+++ packages/watchmen-web-client/src/console/derived-objective/header/index.tsx
@@ -1,14 +1,22 @@
 import {DerivedObjective} from '@/services/data/tuples/derived-objective-types';
 import {PageHeaderHolder} from '@/widgets/basic/page-header';
 import React from 'react';
+import styled from 'styled-components';
 import {HeaderButtons} from './header-buttons';
 import {HeaderNameEditor} from './header-name-editor';
 
+const HeaderContainer = styled(PageHeaderHolder)`
+	@media print {
+		> div[data-widget=page-header-buttons] {
+			display : none;
+		}
+	}
+`;
 export const Header = (props: { derivedObjective: DerivedObjective }) => {
 	const {derivedObjective} = props;
 
-	return <PageHeaderHolder>
+	return <HeaderContainer>
 		<HeaderNameEditor derivedObjective={derivedObjective}/>
 		<HeaderButtons derivedObjective={derivedObjective}/>
-	</PageHeaderHolder>;
+	</HeaderContainer>;
 };
