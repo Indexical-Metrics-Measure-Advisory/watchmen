e5aad78afe85d8e93adec99915f847f497bce8ed
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-04 20:52:41 +0800

    console derived objective

--- packages/watchmen-web-client/src/services/data/mock/tuples/mock-data-derived-objectives.ts
+++ packages/watchmen-web-client/src/services/data/mock/tuples/mock-data-derived-objectives.ts
@@ -1,4 +1,6 @@
-import {DerivedObjective} from '../../tuples/derived-objective-types';
+import {BreakdownDimensionType, DerivedObjective} from '../../tuples/derived-objective-types';
+import {MeasureMethod} from '../../tuples/indicator-types';
+import {generateUuid} from '../../tuples/utils';
 import {getCurrentTime} from '../../utils';
 import {MonthlySalesObjective, OBJECTIVE_MONTHLY_SALES_ID} from './mock-data-objectives';
 
@@ -8,7 +10,19 @@ export const DemoDerivedObjectives: Array<DerivedObjective> = [
 		objectiveId: OBJECTIVE_MONTHLY_SALES_ID,
 		name: MonthlySalesObjective.name,
 		definition: MonthlySalesObjective,
-		breakdownTargets: [],
+		breakdownTargets: [
+			{
+				uuid: generateUuid(),
+				targetId: MonthlySalesObjective.targets![0].uuid,
+				name: 'Yearly Revenue', dimensions: [
+					{
+						factorOrColumnId: '601',
+						timeMeasureMethod: MeasureMethod.YEAR,
+						type: BreakdownDimensionType.TIME_RELATED
+					}
+				]
+			}
+		],
 		lastVisitTime: '2020/10/31 14:23:07',
 		createdAt: getCurrentTime(),
 		lastModifiedAt: getCurrentTime()
