718d77a55080529cca58d49ae19cb1d6382a1e72
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-03-04 20:52:58 +0800

    console derived objective

--- packages/watchmen-web-client/src/console/derived-objective/objective-state-holder.tsx
+++ packages/watchmen-web-client/src/console/derived-objective/objective-state-holder.tsx
@@ -1,7 +1,6 @@
 import {SAVE_TIMEOUT} from '@/services/constants';
 import {saveDerivedObjective} from '@/services/data/tuples/derived-objective';
 import {DerivedObjective} from '@/services/data/tuples/derived-objective-types';
-import {ObjectiveValues} from '@/services/data/tuples/objective-types';
 import {useEventBus} from '@/widgets/events/event-bus';
 import {EventTypes} from '@/widgets/events/types';
 import {useThrottler} from '@/widgets/throttler';
@@ -18,11 +17,11 @@ export const ObjectiveStateHandler = (props: { derivedObjective: DerivedObjectiv
 	useEffect(() => saveQueue.clear(true), [derivedObjective, saveQueue]);
 	useEffect(() => {
 		const onAskValues = () => {
-			console.log(derivedObjective)
+			console.log(derivedObjective);
 			saveQueue.replace(() => {
 				fireGlobal(EventTypes.INVOKE_REMOTE_REQUEST,
 					async () => await saveDerivedObjective(derivedObjective),
-					(values: ObjectiveValues) => fire(ObjectiveEventTypes.SAVED));
+					() => fire(ObjectiveEventTypes.SAVED));
 			}, SAVE_TIMEOUT);
 		};
 		on(ObjectiveEventTypes.SAVE, onAskValues);
