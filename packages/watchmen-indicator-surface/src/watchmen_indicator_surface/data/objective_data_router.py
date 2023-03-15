8c0d428e3eaaaeac0556723fbeaff3127b896a85
luke0623 <luke0623@outlook.com>
2023-03-15 18:47:31 +0800

    add merge dimension feature for previous and chain data

--- packages/watchmen-indicator-surface/src/watchmen_indicator_surface/data/objective_data_router.py
+++ packages/watchmen-indicator-surface/src/watchmen_indicator_surface/data/objective_data_router.py
@@ -1,6 +1,7 @@
 from decimal import Decimal
+from enum import Enum
 from logging import getLogger
-from typing import Optional, List
+from typing import Optional, List, Dict, Any
 
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
@@ -19,7 +20,7 @@ from watchmen_model.indicator import Indicator, Objective, ObjectiveFactorOnIndi
 from watchmen_model.indicator.derived_objective import BreakdownTarget
 from watchmen_rest import get_admin_principal
 from watchmen_rest.util import raise_400, raise_403
-from watchmen_utilities import is_blank
+from watchmen_utilities import is_blank, ArrayHelper
 
 logger = getLogger(__name__)
 router = APIRouter()
@@ -31,7 +32,10 @@ class ObjectiveBreakdownRequest(BaseModel):
 	target:ObjectiveTarget = None
 
 
-
+class BreakdownValueType(str,Enum):
+	Current = 'current',
+	PreviousCycle = 'previousCycle',
+	ChainCycle = 'chainCycle'
 
 
 def get_indicator_service(principal_service: PrincipalService) -> IndicatorService:
@@ -113,39 +117,92 @@ def load_objective_target_breakdown_values(breakdown_request :ObjectiveBreakdown
 
 		dataset:DataResult =  objective_factor_data_service.ask_breakdown_values(as_time_frame(time_frame),breakdown_target)
 
-		breakdown_values:ObjectiveTargetBreakdownValues = build_breakdown_result(dataset)
+		breakdown_values:ObjectiveTargetBreakdownValues = build_breakdown_result(dataset,BreakdownValueType.Current)
+
+		dimensions_dict: Dict[str, ObjectiveTargetBreakdownValueRow] = {}
+		for breakdown_row in breakdown_values.data:
+			key: str = build_key_from_list(breakdown_row.dimensions)
+			if key in dimensions_dict:
+				raise Exception("data is not correct")
+			else:
+				dimensions_dict[key] = breakdown_row
+
+
 		if objective_target.askPreviousCycle:
 			previous_time_frame = as_time_frame(compute_previous_frame(objective.timeFrame, time_frame))
 			dataset_previous: DataResult = objective_factor_data_service.ask_breakdown_values(previous_time_frame, breakdown_target)
-
+			dimensions_dict = merge_dimension(dimensions_dict,dataset_previous,BreakdownValueType.PreviousCycle)
 
 
 		elif objective_target.askChainCycle:
 			chain_time_frame = as_time_frame(compute_chain_frame(objective.timeFrame, time_frame))
+			dataset_chain: DataResult = objective_factor_data_service.ask_breakdown_values(chain_time_frame,
+			                                                                                  breakdown_target)
+			dimensions_dict = merge_dimension(dimensions_dict, dataset_chain,BreakdownValueType.ChainCycle)
 
-
+		breakdown_values.data = list(dimensions_dict.values())
 		return breakdown_values
 
 
-def merge_previous(breakdown_values:ObjectiveTargetBreakdownValues,dataset_previous: DataResult):
-	##find merge index for breakdown  {key: demintionkeys ,value:index }
+def build_key_from_list(values:List[Any]):
+	# Convert each value to a string, handling empty or None values
+	key_parts = []
+	for value in values:
+		if value is None:
+			key_parts.append('')
+		elif isinstance(value, str):
+			key_parts.append(value)
+		else:
+			key_parts.append(str(value))
+
+	# Join the key parts with a separator
+	key = '|'.join(key_parts)
+	return key
 
 
+def build_dimensions_dict(breakdown_values:ObjectiveTargetBreakdownValues)->Dict[str,ObjectiveTargetBreakdownValueRow]:
+	dimensions_dict:Dict[str,ObjectiveTargetBreakdownValueRow] ={}
+	for breakdown_row in breakdown_values.data:
+		key:str = build_key_from_list(breakdown_row.dimensions)
+		if key in dimensions_dict:
+			raise Exception("data is not correct")
+		else:
+			dimensions_dict[key] = breakdown_row
+	return dimensions_dict
+
+def merge_dimension(dimension_dict:Dict[str,ObjectiveTargetBreakdownValueRow],dataset_new: DataResult,value_type:BreakdownValueType)->Dict[str,ObjectiveTargetBreakdownValueRow]:
+	##find merge index for breakdown  {key: dimensionskeys ,value:index }
+	new_dimension_dict = build_dimensions_dict(build_breakdown_result(dataset_new,value_type))
+	dimension_dict_after_merge = merge_dicts(dimension_dict,new_dimension_dict)
+
+	return dimension_dict_after_merge
 
 
+def merge_dicts(dimension_dict:Dict[str,ObjectiveTargetBreakdownValueRow], dimension_dict_new:Dict[str,ObjectiveTargetBreakdownValueRow]):
+    merged_dict = dimension_dict.copy()  # Make a copy of dict1 to avoid modifying it
+    for key, value in dimension_dict_new.items():
+        if key in merged_dict:
+            # If the key exists in both dictionaries, combine the values
+            if value.previousValue:
+	            merged_dict[key].previousValue = value.previousValue
+            elif value.chainValue:
+	            merged_dict[key].chainValue = value.chainValue
+        else:
+            # If the key only exists in dict2, add it to the merged dictionary
+            merged_dict[key] = value
+    return merged_dict
 
-
-
-
-
-	pass
-
-def build_breakdown_result(dataset)->ObjectiveTargetBreakdownValues:
+def build_breakdown_result(dataset,value_type:BreakdownValueType)->ObjectiveTargetBreakdownValues:
 	objective_target_breakdown = ObjectiveTargetBreakdownValues()
 	for data in dataset.data:
 		row = ObjectiveTargetBreakdownValueRow()
 		row.dimensions = data[1:]
-		row.currentValue = data[0]
+		if value_type == BreakdownValueType.Current:
+			row.currentValue = data[0]
+		elif value_type ==BreakdownValueType.PreviousCycle:
+			row.previousValue = data[0]
+		elif value_type ==BreakdownValueType.ChainCycle:
+			row.chainValue = data[0]
 		objective_target_breakdown.data.append(row)
 	return objective_target_breakdown
 
