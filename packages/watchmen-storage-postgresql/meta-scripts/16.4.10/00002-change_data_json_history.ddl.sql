05d7489a07e80752250e9922dab905bdb373c7d4
skywalkerX69 <skywalkerX69@outlook.com>
2023-04-06 15:59:23 +0800

    Update 00002-change_data_json_history.ddl.sql

--- packages/watchmen-storage-postgresql/meta-scripts/16.4.10/00002-change_data_json_history.ddl.sql
+++ packages/watchmen-storage-postgresql/meta-scripts/16.4.10/00002-change_data_json_history.ddl.sql
@@ -22,12 +22,12 @@ CREATE TABLE change_data_json_history
     last_modified_by    VARCHAR(50)      NOT NULL,
     CONSTRAINT pk_change_data_json_history PRIMARY KEY (change_json_id)
 );
-CREATE UNIQUE INDEX u_scheduled_task_history_1 ON scheduled_task_history (resource_id);
-CREATE INDEX i_scheduled_task_history_1 ON scheduled_task_history (tenant_id);
-CREATE INDEX i_scheduled_task_history_2 ON scheduled_task_history (created_at);
-CREATE INDEX i_scheduled_task_history_3 ON scheduled_task_history (created_by);
-CREATE INDEX i_scheduled_task_history_4 ON scheduled_task_history (last_modified_at);
-CREATE INDEX i_scheduled_task_history_5 ON scheduled_task_history (last_modified_by);
-CREATE INDEX i_scheduled_task_history_6 ON scheduled_task_history (table_trigger_id);
-CREATE INDEX i_scheduled_task_history_7 ON scheduled_task_history (model_trigger_id);
-CREATE INDEX i_scheduled_task_history_8 ON scheduled_task_history (event_trigger_id);
+CREATE UNIQUE INDEX u_change_data_json_history_1 ON change_data_json_history (resource_id);
+CREATE INDEX i_change_data_json_history_1 ON change_data_json_history (tenant_id);
+CREATE INDEX i_change_data_json_history_2 ON change_data_json_history (created_at);
+CREATE INDEX i_change_data_json_history_3 ON change_data_json_history (created_by);
+CREATE INDEX i_change_data_json_history_4 ON change_data_json_history (last_modified_at);
+CREATE INDEX i_change_data_json_history_5 ON change_data_json_history (last_modified_by);
+CREATE INDEX i_change_data_json_history_6 ON change_data_json_history (table_trigger_id);
+CREATE INDEX i_change_data_json_history_7 ON change_data_json_history (model_trigger_id);
+CREATE INDEX i_change_data_json_history_8 ON change_data_json_history (event_trigger_id);
