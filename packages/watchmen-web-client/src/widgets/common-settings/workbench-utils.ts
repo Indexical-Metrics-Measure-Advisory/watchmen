de6cd6259c226ae7ae135682d7e4777f72608c6e
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-04-28 10:57:20 +0800

    add admin check on indicator workbench

--- packages/watchmen-web-client/src/widgets/common-settings/workbench-utils.ts
+++ packages/watchmen-web-client/src/widgets/common-settings/workbench-utils.ts
@@ -4,4 +4,4 @@ import {isAdmin, isSuperAdmin} from '@/services/data/account';
 export const isConsoleAvailable = () => !isSuperAdmin();
 export const isAdminAvailable = () => isAdmin() || isSuperAdmin();
 export const isDataQualityAvailable = () => isAdmin() && !isSuperAdmin() && isDataQualityCenterEnabled();
-export const isIndicatorAvailable = () => !isSuperAdmin();
+export const isIndicatorAvailable = () => isAdmin() && !isSuperAdmin();
