7c39162c680e29954d8efa2fea13c36406870c85
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-05-26 11:01:29 +0800

    ask sql only

--- packages/watchmen-storage-rds/src/watchmen_storage_rds/topic_data_storage_rds.py
+++ packages/watchmen-storage-rds/src/watchmen_storage_rds/topic_data_storage_rds.py
@@ -15,7 +15,7 @@ from watchmen_storage import as_table_name, EntityHelper, FreeAggregateArithmeti
 	FreeAggregatePager, FreeAggregator, FreeColumn, FreeFinder, FreeJoin, FreeJoinType, FreePager, Literal, \
 	NoFreeJoinException, TopicDataStorageSPI, UnexpectedStorageException
 from watchmen_storage.settings import ask_sql_analyzer_on
-from watchmen_storage.sql_analysis.ast_vister import QueryPerformance
+from watchmen_storage.sql_analysis.ast_visitor import QueryPerformance
 from watchmen_storage.sql_analysis.parse_sql import SqlParser
 from watchmen_utilities import ArrayHelper, is_not_blank
 from .storage_rds import StorageRDS
@@ -415,10 +415,16 @@ class TopicDataStorageRDS(StorageRDS, TopicDataStorageSPI):
 	def free_find(self, finder: FreeFinder) -> List[Dict[str, Any]]:
 		data_statement = self.build_free_find_statement(finder)
 		# print("----------")
-		# noinspection DuplicatedCode
 		sql = data_statement.compile(dialect=self.connection.dialect, compile_kwargs={"literal_binds": True})
 		query_performance = self.extract_sql(sql)
+		if finder.commandOnly:
+			query_performance.execution_time = 0
+			query_performance.data_volume = 0
+			if finder.queryPfmMonitor:
+				finder.queryPfmMonitor(query_performance, True)
+			return []
 
+		# noinspection DuplicatedCode
 		start = timer()
 		# noinspection DuplicatedCode
 		results = self.connection.execute(data_statement).mappings().all()
@@ -466,6 +472,12 @@ class TopicDataStorageRDS(StorageRDS, TopicDataStorageSPI):
 		sql = data_statement.compile(dialect=self.connection.dialect, compile_kwargs={"literal_binds": True})
 		query_performance = self.extract_sql(sql)
 		# print("----------")
+		if pager.commandOnly:
+			query_performance.execution_time = 0
+			query_performance.data_volume = 0
+			if pager.queryPfmMonitor:
+				pager.queryPfmMonitor(query_performance, True)
+			return self.create_empty_page(page_size)
 
 		start = timer()
 		results = self.connection.execute(data_statement).mappings().all()
@@ -498,6 +510,13 @@ class TopicDataStorageRDS(StorageRDS, TopicDataStorageSPI):
 		# noinspection DuplicatedCode
 		sql = data_statement.compile(dialect=self.connection.dialect, compile_kwargs={"literal_binds": True})
 		query_performance = self.extract_sql(sql)
+		if aggregator.commandOnly:
+			query_performance.execution_time = 0
+			query_performance.data_volume = 0
+			if aggregator.queryPfmMonitor:
+				aggregator.queryPfmMonitor(query_performance, True)
+			return []
+
 		start = timer()
 		# print("-----adad-----")
 		results = self.connection.execute(data_statement).mappings().all()
@@ -537,6 +556,13 @@ class TopicDataStorageRDS(StorageRDS, TopicDataStorageSPI):
 		# print("----------")
 		sql = data_statement.compile(dialect=self.connection.dialect, compile_kwargs={"literal_binds": True})
 		query_performance = self.extract_sql(sql)
+		if pager.commandOnly:
+			query_performance.execution_time = 0
+			query_performance.data_volume = 0
+			if pager.queryPfmMonitor:
+				pager.queryPfmMonitor(query_performance, True)
+			return self.create_empty_page(page_size)
+
 		# print("----------")
 		start = timer()
 		results = self.connection.execute(data_statement).mappings().all()
--- packages/watchmen-storage/src/watchmen_storage/__init__.py
+++ packages/watchmen-storage/src/watchmen_storage/__init__.py
@@ -1,13 +1,14 @@
 from .competitive_worker_id_generator import competitive_worker_id, CompetitiveWorker, CompetitiveWorkerIdGenerator, \
 	CompetitiveWorkerRestarter, CompetitiveWorkerShutdownListener, CompetitiveWorkerShutdownSignal, get_host_ip, \
 	WorkerCreationException, WorkerDeclarationException, WorkerFirstDeclarationException
-from .data_source_helper import DataSourceHelper, ask_datasource_name
+from .data_source_helper import ask_datasource_name, DataSourceHelper
 from .free_storage_types import FreeAggregateArithmetic, FreeAggregateColumn, FreeAggregatePager, FreeAggregator, \
 	FreeColumn, FreeFinder, FreeJoin, FreeJoinType, FreePager
 from .settings import ask_decimal_fraction_digits, ask_decimal_integral_digits, ask_disable_compiled_cache, \
 	ask_object_storage_need_date_directory, ask_store_json_in_clob
 from .snowflake import InvalidSystemClockException, SnowflakeGenerator
 from .snowflake_worker_id_generator import immutable_worker_id, WorkerIdGenerator
+from .sql_analysis import QueryPerformance
 from .storage_based_worker_id_generator import COMPETITIVE_WORKER_SHAPER, CompetitiveWorkerShaper, \
 	SNOWFLAKE_WORKER_ID_TABLE, StorageBasedWorkerIdGenerator
 from .storage_exception import EntityNotFoundException, InsertConflictException, NoCriteriaForUpdateException, \
@@ -16,11 +17,10 @@ from .storage_exception import EntityNotFoundException, InsertConflictException,
 	UnsupportedStraightColumnException
 from .storage_spi import StorageSPI, TopicDataStorageSPI, TransactionalStorageSPI
 from .storage_types import ColumnNameLiteral, ComputedLiteral, ComputedLiteralOperator, Entity, \
-	EntityColumnAggregateArithmetic, EntityColumnName, EntityColumnValue, EntityCriteria, EntityCriteriaExpression, \
-	EntityCriteriaJoint, EntityCriteriaJointConjunction, EntityCriteriaOperator, EntityCriteriaStatement, \
-	EntityDeleter, EntityDistinctValuesFinder, EntityFinder, EntityHelper, EntityId, EntityIdHelper, EntityList, \
-	EntityName, EntityPager, EntityRow, EntityShaper, EntitySort, EntitySortColumn, EntitySortMethod, \
-	EntityStraightAggregateColumn, EntityStraightColumn, EntityStraightValuesFinder, EntityUpdate, EntityUpdater, \
-	Literal, EntityColumnType
+	EntityColumnAggregateArithmetic, EntityColumnName, EntityColumnType, EntityColumnValue, EntityCriteria, \
+	EntityCriteriaExpression, EntityCriteriaJoint, EntityCriteriaJointConjunction, EntityCriteriaOperator, \
+	EntityCriteriaStatement, EntityDeleter, EntityDistinctValuesFinder, EntityFinder, EntityHelper, EntityId, \
+	EntityIdHelper, EntityList, EntityName, EntityPager, EntityRow, EntityShaper, EntitySort, EntitySortColumn, \
+	EntitySortMethod, EntityStraightAggregateColumn, EntityStraightColumn, EntityStraightValuesFinder, EntityUpdate, \
+	EntityUpdater, Literal
 from .topic_utils import as_table_name
-
