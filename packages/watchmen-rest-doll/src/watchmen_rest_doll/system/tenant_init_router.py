717f3a7ca72cfe48d062a99bd301f1aab40357f7
luke0623 <luke0623@outlook.com>
2023-06-15 12:21:51 +0800

    update

--- packages/watchmen-rest-doll/src/watchmen_rest_doll/system/tenant_init_router.py
+++ packages/watchmen-rest-doll/src/watchmen_rest_doll/system/tenant_init_router.py
@@ -17,7 +17,7 @@ from watchmen_rest import get_any_admin_principal
 from watchmen_rest.util import raise_400, raise_404
 from watchmen_rest_doll.admin import ask_save_pipeline_action, ask_save_topic_action
 from watchmen_rest_doll.util import trans
-from watchmen_storage.sql_analysis.topic_generator import ask_query_performance_topics, ask_query_performance_pipelines
+from watchmen_storage.sql_analysis import ask_query_performance_topics, ask_query_performance_pipelines
 from watchmen_utilities import ArrayHelper, is_blank
 
 router = APIRouter()
--- packages/watchmen-storage/src/watchmen_storage/sql_analysis/__init__.py
+++ packages/watchmen-storage/src/watchmen_storage/sql_analysis/__init__.py
@@ -1,1 +1,4 @@
 # from ast_visitor import QueryPerformance
+
+from .topic_generator import ask_query_performance_topics
+from .topic_generator import ask_query_performance_pipelines
