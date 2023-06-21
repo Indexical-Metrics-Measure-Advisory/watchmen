0d8f3b9c77c709c8e135aff38760fa33b18278f9
luke0623 <luke0623@outlook.com>
2023-06-21 19:14:40 +0800

    remove log and add task schedule
    remove log and add task schedule

--- packages/watchmen-collector-surface/src/watchmen_collector_surface/task/task_listener.py
+++ packages/watchmen-collector-surface/src/watchmen_collector_surface/task/task_listener.py
@@ -1,17 +1,21 @@
+import logging
 from logging import getLogger
 from threading import Thread
+
+from apscheduler.schedulers.background import BackgroundScheduler
 from time import sleep
 
-from watchmen_collector_kernel.common import TENANT_ID
 from watchmen_collector_kernel.model import ScheduledTask
-from .handler import pipeline_data
 from watchmen_collector_kernel.service import try_lock_nowait, unlock, get_task_service
 from watchmen_collector_kernel.service.lock_helper import get_resource_lock
 from watchmen_collector_kernel.storage import get_competitive_lock_service, get_scheduled_task_service
 from watchmen_meta.common import ask_snowflake_generator, ask_super_admin, ask_meta_storage
+from .handler import pipeline_data
+from ..settings import ask_fastapi_job, ask_task_listener
 
-
-logger = getLogger(__name__)
+logger = logging.getLogger('apscheduler')
+logger.setLevel(logging.ERROR)
+scheduler = BackgroundScheduler(logger=None)
 
 
 def init_task_listener():
@@ -34,6 +38,11 @@ class TaskListener:
 
 	def create_thread(self) -> None:
 		Thread(target=TaskListener.run, args=(self,), daemon=True).start()
+		if ask_fastapi_job():
+			scheduler.add_job(TaskListener.run, 'interval', seconds=ask_task_listener(),args=(self,))
+			scheduler.start()
+		else:
+			Thread(target=TaskListener.run, args=(self,), daemon=True).start()
 
 	def run(self):
 		try:
