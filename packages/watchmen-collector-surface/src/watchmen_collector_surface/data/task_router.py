3582cc37b82530cf10b1273a7f07e9c26ad99cf7
luke0623 <luke0623@outlook.com>
2023-06-26 13:51:50 +0800

    fix job schedule issue
    fix job schedule issue

--- packages/watchmen-collector-surface/src/watchmen_collector_surface/data/task_router.py
+++ packages/watchmen-collector-surface/src/watchmen_collector_surface/data/task_router.py
@@ -1,16 +1,25 @@
 from typing import Callable, Optional
 
+from apscheduler.schedulers.background import BackgroundScheduler
 from fastapi import APIRouter, Depends
 
 from watchmen_auth import PrincipalService
 from watchmen_collector_kernel.model import ScheduledTask
 from watchmen_collector_kernel.storage import get_scheduled_task_service, ScheduledTaskService
+from watchmen_collector_surface.cdc.monitor_event import CollectorEventListener
+from watchmen_collector_surface.cdc.post_json import PostJsonService
+from watchmen_collector_surface.cdc.record_to_json import RecordToJsonService
+from watchmen_collector_surface.cdc.table_extractor import TableExtractor
+from watchmen_collector_surface.settings import ask_table_wait
+from watchmen_collector_surface.task.task_listener import TaskListener
 from watchmen_meta.common import ask_snowflake_generator, ask_meta_storage
 from watchmen_model.admin import UserRole
 from watchmen_rest import get_any_admin_principal
 from watchmen_rest.util import validate_tenant_id, raise_403
 
 router = APIRouter()
+
+scheduler= BackgroundScheduler()
 
 
 @router.post('/collector/task', tags=[UserRole.ADMIN, UserRole.SUPER_ADMIN], response_model=ScheduledTask)
@@ -24,6 +33,18 @@ async def save_task(
 	action = ask_save_scheduled_task_action(scheduled_task_service, principal_service)
 	return action(task)
 
+@router.on_event("shutdown")
+def shutdown_event():
+    scheduler.shutdown()
+
+
+def add_collector_job():
+	TableExtractor().create_thread(scheduler)
+	RecordToJsonService().create_thread(scheduler)
+	PostJsonService().create_thread(scheduler)
+	CollectorEventListener().create_thread(scheduler)
+	TaskListener().create_thread(scheduler)
+	scheduler.start()
 
 # noinspection PyUnusedLocal
 def ask_save_scheduled_task_action(
@@ -47,3 +68,5 @@ def ask_save_scheduled_task_action(
 		return scheduled_task
 
 	return action
+
+
