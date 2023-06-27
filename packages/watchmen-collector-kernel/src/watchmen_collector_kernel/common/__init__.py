292f2875c4dfe0598a541ce10e12a33e735194cb
skywalkerX69 <skywalkerX69@outlook.com>
2023-06-28 00:20:58 +0800

    collector config cache supported

--- packages/watchmen-collector-kernel/src/watchmen_collector_kernel/common/__init__.py
+++ packages/watchmen-collector-kernel/src/watchmen_collector_kernel/common/__init__.py
@@ -1,3 +1,5 @@
 from .constants import CHANGE_RECORD_ID, TENANT_ID, IS_MERGED, LEFT_BRACE, RIGHT_BRACE, COMMA, \
 	IS_POSTED, CHANGE_JSON_ID, WAVE, IS_FINISHED, IS_EXTRACTED, MODEL_TRIGGER_ID
-from .settings import ask_lock_clean_interval, ask_lock_clean_timeout, ask_partial_size
+from .settings import ask_lock_clean_interval, ask_lock_clean_timeout, ask_partial_size, \
+	ask_collector_config_cache_enabled
+from .exception import CollectorKernelException
