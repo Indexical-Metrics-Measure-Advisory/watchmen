f85221659c94c551c6f3dda3101cc40ef6eb34f7
luke0623 <luke0623@outlook.com>
2023-06-29 00:06:33 +0800

    change dqc loop to uvloop
    change dqc loop to uvloop

--- packages/watchmen-rest-dqc/src/app.py
+++ packages/watchmen-rest-dqc/src/app.py
@@ -1,4 +1,4 @@
 from uvicorn import run
 
 if __name__ == "__main__":
-	run("watchmen_rest_dqc.main:app", host="127.0.0.1", port=8800, log_level="info", workers=1)
+	run("watchmen_rest_dqc.main:app", host="127.0.0.1", port=8800, log_level="info", workers=1,loop="uvloop")
