9aeffc4db9864b6dd6301c5a1e3741466ea5431e
skywalkerX69 <skywalkerX69@outlook.com>
2023-07-11 10:28:49 +0800

    Update table_defs.py

--- packages/watchmen-storage-rds/src/watchmen_storage_rds/table_defs.py
+++ packages/watchmen-storage-rds/src/watchmen_storage_rds/table_defs.py
@@ -77,6 +77,7 @@ table_user_groups = Table(
 	create_pk('user_group_id'),
 	create_str('name', 50, False), create_description(),
 	create_json('user_ids'), create_json('space_ids'), create_json("objective_ids"),
+	create_json("convergence_ids"),
 	create_tenant_id(), *create_tuple_audit_columns(), create_optimistic_lock()
 )
 table_spaces = Table(
