b6e62edfa8b1178f0b21de454ee46c8b96a1d810
skywalkerX69 <iceberg_G@outlook.com>
2023-09-24 16:43:59 +0800

    fix dbscripts

--- packages/watchmen-storage-postgresql/meta-scripts/16.5.24/00002-collector_model_config.ddl.sql
+++ packages/watchmen-storage-postgresql/meta-scripts/16.5.24/00002-collector_model_config.ddl.sql
@@ -1,3 +1,2 @@
-ALTER TABLE collector_model_config DROP CONSTRAINT u_collector_model_config_1;
 DROP INDEX u_collector_model_config_1;
 CREATE UNIQUE INDEX u_collector_model_config_1 ON collector_model_config (model_name, tenant_id);
--- packages/watchmen-storage-postgresql/meta-scripts/16.5.24/00003-collector_table_config.ddl.sql
+++ packages/watchmen-storage-postgresql/meta-scripts/16.5.24/00003-collector_table_config.ddl.sql
@@ -1,4 +1,3 @@
-ALTER TABLE collector_table_config DROP CONSTRAINT u_collector_table_config_1;
 DROP INDEX u_collector_table_config_1;
 CREATE UNIQUE INDEX u_collector_table_config_1 ON collector_table_config (name, tenant_id);
 CREATE UNIQUE INDEX u_collector_table_config_2 ON collector_table_config (table_name, tenant_id);
