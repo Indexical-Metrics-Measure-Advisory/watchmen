ec72e38e573ffccdf08efb57bfc54c090f99d76a
botlikes <75356972+botlikes456@users.noreply.github.com>
2023-10-08 15:07:05 +0800

    fix: connection leaked since db engine is created on every time ask storage, which holds connections by pool

--- packages/watchmen-storage-postgresql/src/watchmen_storage_postgresql/storage_postgresql_configuration.py
+++ packages/watchmen-storage-postgresql/src/watchmen_storage_postgresql/storage_postgresql_configuration.py
@@ -45,10 +45,10 @@ class Configuration:
 		return self.storageHolder
 
 	def build(self) -> StoragePostgreSQL:
-		return self.storageHolder.create_storage()
+		return self.get_or_create_storage_holder().create_storage()
 
 	def build_topic_data(self) -> TopicDataStoragePostgreSQL:
-		return self.storageHolder.create_topic_data_storage()
+		return self.get_or_create_storage_holder().create_topic_data_storage()
 
 
 class StoragePostgreSQLConfiguration:
