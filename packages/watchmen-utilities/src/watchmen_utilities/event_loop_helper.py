e7bbab5b9320a44ee93f39e44cf126d759921c42
skywalkerX69 <skywalkerX69@outlook.com>
2024-02-21 18:01:36 +0800

    use loop run instead of asyncio run

--- packages/watchmen-utilities/src/watchmen_utilities/event_loop_helper.py
+++ packages/watchmen-utilities/src/watchmen_utilities/event_loop_helper.py
@@ -1,26 +1,18 @@
 import asyncio
-from typing import Any
 
 
-def run(async_function) -> Any:
+def run(async_func):
 	try:
-		return run_with_running_loop(async_function)
+		run_with_running_loop(async_func)
 	except RuntimeError:
-		return run_with_new_loop(async_function)
+		run_with_new_loop(async_func)
 
 
-def run_with_running_loop(async_function) -> Any:
+def run_with_running_loop(async_func):
 	loop = asyncio.get_running_loop()
-	task = loop.create_task(async_function)
-	return task
+	task = loop.create_task(async_func)
+	asyncio.gather(task)
 
 
-def run_with_new_loop(async_function) -> Any:
-	loop = asyncio.new_event_loop()
-	try:
-		asyncio.set_event_loop(loop)
-		result = loop.run_until_complete(async_function)
-		return result
-	finally:
-		asyncio.set_event_loop(None)
-		loop.close()
+def run_with_new_loop(async_func):
+	asyncio.run(async_func)
