31a86b34e930fbf769725a4ce29f93d1ec7e413a
skywalker <iceberg_G@outlook.com>
2024-08-01 12:17:28 +0800

    Optimize: Modify the mechanism between the task and change json, and do not redundantly apply the change json content to the task model

--- packages/watchmen-collector-surface/src/watchmen_collector_surface/task/task_listener.py
+++ packages/watchmen-collector-surface/src/watchmen_collector_surface/task/task_listener.py
@@ -1,4 +1,5 @@
 import logging
+import copy
 from traceback import format_exc
 from typing import List, Dict, Optional
 from abc import ABC, abstractmethod
@@ -121,7 +122,8 @@ class TaskListener:
 					change_json = self.get_change_data_json(change_json_id)
 					if change_json:
 						try:
-							self.process_sub_tasks(unfinished_task, change_json)
+							sub_task = copy.deepcopy(unfinished_task)
+							self.process_sub_tasks(sub_task, change_json)
 						finally:
 							self.update_change_json_result(change_json)
 							finished_json.append(change_json.changeJsonId)
