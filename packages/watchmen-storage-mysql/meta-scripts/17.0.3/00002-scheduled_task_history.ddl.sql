17167217b4be94b0ae467343e7b43821b78e300d
skywalker <iceberg_G@outlook.com>
2024-08-13 13:36:14 +0800

    Optimize add event trigger id to task table

--- packages/watchmen-storage-mysql/meta-scripts/17.0.3/00002-scheduled_task_history.ddl.sql
+++ packages/watchmen-storage-mysql/meta-scripts/17.0.3/00002-scheduled_task_history.ddl.sql
@@ -1,3 +1,1 @@
 ALTER TABLE scheduled_task_history ADD event_trigger_id BIGINT NOT NULL;
-ALTER TABLE scheduled_task_history DROP INDEX event_id;
-ALTER TABLE scheduled_task_history ADD INDEX event_trigger_id(event_trigger_id);
