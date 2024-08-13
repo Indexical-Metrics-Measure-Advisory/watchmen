446fc4bd182bc624a395bf8044b2e8c4dda79857
skywalker <iceberg_G@outlook.com>
2024-08-13 13:21:22 +0800

    Optimize add event trigger id to task table

---
+++ packages/watchmen-storage-mysql/meta-scripts/17.0.3/00001-scheduled_task.ddl.sql
@@ -0,0 +1,9 @@
+ALTER TABLE scheduled_task ADD event_trigger_id BIGINT NOT NULL;
+
+ALTER TABLE scheduled_task DROP INDEX idx_task_event_id;
+ALTER TABLE scheduled_task DROP INDEX idx_task_model_name_event_id;
+ALTER TABLE scheduled_task DROP INDEX idx_task_model_name_object_id_event_id_tenant_id;
+
+ALTER TABLE scheduled_task ADD INDEX idx_task_event_id(event_trigger_id);
+ALTER TABLE scheduled_task ADD INDEX idx_task_model_name_event_id(model_name, event_trigger_id);
+ALTER TABLE scheduled_task ADD INDEX idx_task_model_name_object_id_event_id_tenant_id(model_name, object_id, event_trigger_id, tenant_id);
