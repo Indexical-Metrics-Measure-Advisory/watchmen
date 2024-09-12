84d02180d0768904d17d4da80c485f242a55b68d
skywalker <iceberg_G@outlook.com>
2024-09-12 10:20:14 +0800

    upgrade pydantic from 1.x to 2.x

--- packages/watchmen-model/src/watchmen_model/common/parameter_and_condition.py
+++ packages/watchmen-model/src/watchmen_model/common/parameter_and_condition.py
@@ -95,7 +95,7 @@ class ParameterJointType(str, Enum):
 
 class ParameterJoint(ParameterCondition):
 	jointType: ParameterJointType = ParameterJointType.AND
-	filters: Optional[List[ParameterCondition]] = None
+	filters: Optional[List[ParameterCondition]] = []
 
 	def __setattr__(self, name, value):
 		if name == 'filters':
