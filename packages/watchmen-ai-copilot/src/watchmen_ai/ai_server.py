5d81dbb1922cb3f4a669c93436938bd26647a88a
skywalkerX69 <iceberg_G@outlook.com>
2025-02-13 12:53:37 +0800

    fix: support env config file for dspy configuration

--- packages/watchmen-ai-copilot/src/watchmen_ai/ai_server.py
+++ packages/watchmen-ai-copilot/src/watchmen_ai/ai_server.py
@@ -2,13 +2,12 @@ from typing import Callable, Optional
 
 
 from fastapi import FastAPI
-
 from watchmen_meta.auth import build_find_user_by_name, build_find_user_by_pat
 from watchmen_model.admin import User
 from watchmen_rest import RestApp
 #
 # from .dspy.test import lancedb_retriever
-from .settings import AISettings
+from .settings import AISettings, ask_azure_api_key, ask_azure_api_base, ask_azure_api_version
 
 
 class AIApp(RestApp):
@@ -26,7 +25,10 @@ class AIApp(RestApp):
     def init_llm_dspy(self):
         import dspy
         # load markdown upload_file
-        lm = dspy.LM('azure/gpt_4o')
+        api_key = ask_azure_api_key()
+        api_base = ask_azure_api_base()
+        api_version = ask_azure_api_version()
+        lm = dspy.LM('azure/gpt_4o', api_key=api_key, api_base=api_base, api_version=api_version)
         # lm = dspy.LM('azure/gpt_4o_mini')
         dspy.settings.configure(lm=lm)
 
--- packages/watchmen-ai-copilot/src/watchmen_ai/settings.py
+++ packages/watchmen-ai-copilot/src/watchmen_ai/settings.py
@@ -3,3 +3,20 @@ from watchmen_rest import RestSettings
 
 class AISettings(RestSettings):
     APP_NAME: str = 'Watchmen AI'
+    AZURE_API_KEY: str = ""
+    AZURE_API_BASE: str = ""
+    AZURE_API_VERSION: str = ""
+
+
+ai_settings = AISettings()
+
+def ask_azure_api_key() -> str:
+    return ai_settings.AZURE_API_KEY
+
+
+def ask_azure_api_base() -> str:
+    return ai_settings.AZURE_API_BASE
+
+
+def ask_azure_api_version() -> str:
+    return ai_settings.AZURE_API_VERSION
