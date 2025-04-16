f2fb948273f7a8597efcadada2c9546e85228874
skywalkerX69 <iceberg_G@outlook.com>
2025-04-16 16:28:09 +0800

    upgrade mssql driver from 17 to 18

--- packages/watchmen-storage-mssql/meta-scripts/16.4.1/00003-create_default_package_version.dml.sql
+++ packages/watchmen-storage-mssql/meta-scripts/16.4.1/00003-create_default_package_version.dml.sql
@@ -1,4 +1,4 @@
 # noinspection SpellCheckingInspectionForFile
 INSERT INTO package_versions(version_id, previous_version, current_version, tenant_id, created_at, created_by,
-                  last_modified_at, last_modified_by, version)
+                  last_modified_at, last_modified_by)
 VALUES (1, '50.0.0', '50.0.0', 1, GETDATE(), 1, GETDATE(), 1);
