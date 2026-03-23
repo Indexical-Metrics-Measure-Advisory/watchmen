28f737a8858d60f70f4ae26ebf023a8f34f350fb
skywalkerX69 <70319919+skywalkerX69@users.noreply.github.com>
2026-03-23 11:56:44 +0800

    clean the test file and ignored file

--- test_sa.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine, text
-engine = create_engine("postgresql+psycopg2://watchmen:watchmen@localhost:5432/watchmen")
-conn = engine.connect()
-trans = conn.begin()
-conn.execute(text("SELECT 1"))
-conn.close()
-print("done")
