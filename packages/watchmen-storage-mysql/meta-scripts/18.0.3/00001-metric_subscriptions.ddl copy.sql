06e69aacdaa047b3dff5fbb31e17641c438ae564
skywalkerX69 <70319919+skywalkerX69@users.noreply.github.com>
2026-04-29 17:08:46 +0800

    fix column type from bool to int

--- packages/watchmen-storage-mysql/meta-scripts/18.0.3/00001-metric_subscriptions.ddl copy.sql
+++
@@ -1,1 +0,0 @@
-ALTER TABLE metric_subscriptions ADD COLUMN only_on_alert_triggered BOOLEAN DEFAULT FALSE;
--- packages/watchmen-storage-postgresql/meta-scripts/18.0.3/00001-metric_subscriptions.ddl.sql
+++ packages/watchmen-storage-postgresql/meta-scripts/18.0.3/00001-metric_subscriptions.ddl.sql
@@ -1,1 +1,1 @@
-ALTER TABLE metric_subscriptions ADD COLUMN only_on_alert_triggered BOOLEAN DEFAULT FALSE;
+ALTER TABLE metric_subscriptions ADD COLUMN only_on_alert_triggered SMALLINT;
